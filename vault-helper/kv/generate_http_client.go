0247e5b61c33feecc35f3f1a7301f1d080d05f3c
VIEWPOINT\Sara.wille <sara.wille@viewpoint.com>
2018-06-08 11:52:04 -0700

    Added http client generator

---
+++ vault-helper/kv/generate_http_client.go
@@ -0,0 +1,44 @@
+package kv
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"net/http"
+)
+
+//CreateHTTPClient reads from several .pem files to get the necessary keys and certs to configure the http client and returns the client.
+func CreateHTTPClient() (client *http.Client, err error) {
+	rootCertPEM, err := ioutil.ReadFile("certs/cert_files/root_cert.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	servCertPEM, err := ioutil.ReadFile("certs/cert_files/serv_cert.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	servKeyPEM, err := ioutil.ReadFile("certs/cert_files/serv_key.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	servTLSCert, err := tls.X509KeyPair(servCertPEM, servKeyPEM)
+	if err != nil {
+		return nil, err
+	}
+	// create a pool of trusted certs
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(rootCertPEM)
+
+	// create another test server and use the certificate
+	// configure a client to use trust those certificates
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{Certificates: []tls.Certificate{servTLSCert}, RootCAs: certPool},
+			//InsecureSkipVerify: true
+		},
+	}
+	return httpClient, nil
+}
