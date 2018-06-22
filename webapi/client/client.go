5aa446a8e2fb6e2c8e74b74a839c9e17041fa127
VIEWPOINT\Sara.wille <sara.wille@viewpoint.com>
2018-06-22 09:17:14 -0700

    Added functionality for getting values from vault through twirp.

--- webapi/client/client.go
+++ webapi/client/client.go
@@ -2,35 +2,110 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"net/http"
 
 	"bitbucket.org/dexterchaney/whoville/utils"
-	pb "bitbucket.org/dexterchaney/whoville/webapi/rpc/twirpapi"
+	pb "bitbucket.org/dexterchaney/whoville/webapi/rpc/apinator"
 )
 
-// Small twirp tmpClient made to test the twirp server endpoints
-
 func main() {
-	// tmpClient := tmp.NewTemplatesProtobufClient("http://localhost:8080", &http.Client{})
-	apiClient := pb.NewTwirpAPIProtobufClient("http://localhost:8080", &http.Client{})
+	addrPtr := flag.String("addr", "http://127.0.0.1:8080", "API endpoint for the vault")
+	apiClient := pb.NewEnterpriseServiceBrokerProtobufClient(*addrPtr, &http.Client{})
 
-	templateReq := &pb.TemplateReq{
-		Service: "ST",
-		File:    "hibernate",
-	}
+	//templates := getTemplates("ST")
+	// templateReq := &pb.TemplateReq{
+	// 	Service: "ST",
+	// 	File:    "hibernate",
+	// }
 
-	validReq := &pb.ValidationReq{
-		Service: "ServiceTechDB",
-		Env:     "QA",
-	}
+	// validReq := &pb.ValidationReq{
+	// 	Service: "ServiceTechDB",
+	// 	Env:     "QA",
+	// }
 
-	templateRes, err := apiClient.GetTemplate(context.Background(), templateReq)
+	// listReq := &pb.ListReq{
+	// 	Service: "ST",
+	// }
+
+	// servicePathReq := &pb.ServicePathReq{
+	// 	Service: "values",
+	// 	Env:     "dev",
+	// }
+
+	makeVaultReq := &pb.MakeVaultReq{}
+
+	vault, err := apiClient.MakeVault(context.Background(), makeVaultReq)
 	utils.CheckError(err)
 
-	validRes, err := apiClient.Validate(context.Background(), validReq)
-	utils.CheckError(err)
+	// templateRes, err := apiClient.GetTemplate(context.Background(), templateReq)
+	// utils.CheckError(err)
 
-	fmt.Printf("Template: \nService:\t%s\n File:\t%s\n Data:\n%s\nExt:\t%s\n\n", templateReq.Service, templateReq.File, templateRes.Data, templateRes.Ext)
-	fmt.Printf("Validity: \nService:\t%s\n Env:\t%s\n Valid:\t%v\n", validReq.Service, validReq.Env, validRes.IsValid)
+	// validRes, err := apiClient.Validate(context.Background(), validReq)
+	// utils.CheckError(err)
+
+	// listRes, err := apiClient.ListServiceTemplates(context.Background(), listReq)
+	// utils.CheckError(err)
+
+	// servicePathRes, err := apiClient.GetServicePaths(context.Background(), servicePathReq)
+	// utils.CheckError(err)
+
+	// for _, path := range {
+	// 	pathDataReq := &pb.PathDataReq{
+	// 		Paths: servicePathRes.Paths,
+	// 		Env:   "dev",
+	// 	}
+	// }
+
+	//pathDataRes, err := apiClient.GetPathData(context.Background(), pathDataReq)
+	//utils.CheckError(err)
+
+	//fmt.Printf("Template: \nService:\t%s\n File:\t%s\n Data:\n%s\nExt:\t%s\n\n", templateReq.Service, templateReq.File, templateRes.Data, templateRes.Ext)
+	//fmt.Println("")
+	//fmt.Printf("Validity: \nService:\t%s\n Env:\t%s\n Valid:\t%v\n", validReq.Service, validReq.Env, validRes.IsValid)
+	//fmt.Println("")
+	//fmt.Printf("List: \nService:\t%s\nResult:\t%v\n", listReq.Service, listRes.Templates)
+	//fmt.Println("")
+	//fmt.Printf("List service paths: \nService:\t%s\nEnv:\t%s\nResult:\t%v\n", servicePathReq.Service, servicePathReq.Env, servicePathRes.Paths)
+	//fmt.Println("")
+	//fmt.Printf("List path data: \nPaths:\t%s\nEnv:\t%s\nResult:\t%v\n", pathDataReq.Paths, pathDataReq.Env, pathDataRes.Data)
+	fmt.Printf("Vault: \n")
+	for _, env := range vault.Envs {
+		fmt.Printf("Env: %s\n", env.Name)
+		for _, service := range env.Services {
+			fmt.Printf("\tService: %s\n", service.Name)
+			for _, file := range service.Files {
+				fmt.Printf("\t\tFile: %s\n", file.Name)
+				for _, val := range file.Values {
+					fmt.Printf("\t\t\tkey: %s\tvalue: %s\n", val.Key, val.Value)
+				}
+			}
+		}
+	}
 }
+
+// func getTemplates(service string) []string {
+// 	templates := []string{}
+// 	listReq := &pb.ListReq{
+// 		Service: service,
+// 	}
+// 	listRes, err := apiClient.ListServiceTemplates(context.Background(), listReq)
+// 	utils.CheckError(err)
+// 	for _, file := range listRes.Templates {
+// 		templateReq := &pb.TemplateReq{
+// 			Service: service,
+// 			File:    file,
+// 		}
+// 		templateRes, err := apiClient.GetTemplate(context.Background(), templateReq)
+// 		utils.CheckError(err)
+// 		templateString := string(b64.Encoding.DecodeString(templateRes.Data))
+// 		templates = append(templates, templateString)
+// 		//turn template back to a string
+// 	}
+// 	return templates
+// }
+
+// func getServiceData(env ){
+
+// }
--- webapi/server/server.go
+++ webapi/server/server.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
+
+	"bitbucket.org/dexterchaney/whoville/webapi/rpc/apinator"
 
 	"bitbucket.org/dexterchaney/whoville/vault-helper/kv"
 	pb "bitbucket.org/dexterchaney/whoville/webapi/rpc/apinator"
@@ -16,6 +19,7 @@ type Server struct {
 	CertPath   string
 }
 
+//ListServiceTemplates lists the templates under the requested service
 func (s *Server) ListServiceTemplates(ctx context.Context, req *pb.ListReq) (*pb.ListResp, error) {
 	mod, err := kv.NewModifier(s.VaultToken, s.VaultAddr, s.CertPath)
 	if err != nil {
@@ -94,4 +98,156 @@ func (s *Server) Validate(ctx context.Context, req *pb.ValidationReq) (*pb.Valid
 	}
 
 	return &pb.ValidationResp{IsValid: data["verified"].(bool)}, nil
+}
+
+func (s *Server) GetServicePaths(ctx context.Context, req *pb.ServicePathReq) (*pb.ServicePathResp, error) {
+	// Connect to the vault
+	mod, err := kv.NewModifier(s.VaultToken, s.VaultAddr, s.CertPath)
+	mod.Env = req.Env
+	//fmt.Println("mod: " + mod.Env)
+	if err != nil {
+		return nil, err
+	}
+	//data, err := getPathsFromService(mod, req.Service)
+	paths := []string{}
+	secret, err := mod.List(req.Service + "/")
+	if err != nil {
+		//fmt.Println("error")
+		return nil, err
+	}
+	if secret != nil {
+		//fmt.Println("yes secrets")
+		pathsI := secret.Data["keys"].([]interface{})
+		for _, pathI := range pathsI {
+			p := pathI.(string)
+			path := req.Service + "/" + p
+			//fmt.Println(path)
+			paths = getPaths(mod, path)
+			//data = append(data, d)
+		}
+	} else {
+		fmt.Println("no paths")
+	}
+	return &pb.ServicePathResp{
+		Paths: paths,
+	}, nil
+}
+func (s *Server) GetPathData(ctx context.Context, req *pb.PathDataReq) (*pb.PathDataResp, error) {
+	// Connect to the vault
+	mod, err := kv.NewModifier(s.VaultToken, s.VaultAddr, s.CertPath)
+	mod.Env = req.Env
+	//fmt.Println("mod: " + mod.Env)
+	if err != nil {
+		return nil, err
+	}
+	//data, err := getPathsFromService(mod, req.Service)
+	data := []string{}
+	//paths := req.Paths
+
+	if len(req.Paths) > 0 {
+		for _, path := range req.Paths {
+			//for each path, read the secrets there
+			secrets, err := mod.ReadData(path)
+			if err != nil {
+				panic(err)
+			}
+			if secrets != nil {
+				//fmt.Println("data at path " + path)
+				for key := range secrets {
+					data = append(data, key)
+					//data = append(data, value.(string))
+				}
+			} //else {
+			//fmt.Println("no data at path " + path)
+			//}
+		}
+	} else {
+		fmt.Println("no paths")
+	}
+	return &pb.PathDataResp{
+		Data: data,
+	}, nil
+}
+
+func (s *Server) MakeVault(ctx context.Context, req *pb.MakeVaultReq) (*pb.Vault, error) {
+	// Connect to the vault
+	//mod, err := kv.NewModifier(s.VaultToken, s.VaultAddr, s.CertPath)
+	//mod.Env = req.Env
+	//fmt.Println("mod: " + mod.Env)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	environments := []*apinator.Vault_Env{}
+	envStrings := []string{"dev", "QA", "local", "secrets"}
+	for _, environment := range envStrings {
+		mod, err := kv.NewModifier(s.VaultToken, s.VaultAddr, s.CertPath)
+		mod.Env = environment
+		if err != nil {
+			return nil, err
+		}
+		services := []*apinator.Vault_Env_Service{}
+		//get a list of services under values
+		servicePaths := getPaths(mod, "values/")
+		for _, servicePath := range servicePaths {
+			files := []*apinator.Vault_Env_Service_File{}
+			//get a list of files under service
+			filePaths := getPaths(mod, servicePath)
+			for _, filePath := range filePaths {
+				vals := []*apinator.Vault_Env_Service_File_Value{}
+				//get a list of values
+				valueMap, err := mod.ReadData(filePath)
+				if err != nil {
+					panic(err)
+				}
+				if valueMap != nil {
+					//fmt.Println("data at path " + path)
+					for key, value := range valueMap {
+						kv := &apinator.Vault_Env_Service_File_Value{Key: key, Value: value.(string)}
+						vals = append(vals, kv)
+						//data = append(data, value.(string))
+					}
+
+				}
+				file := &apinator.Vault_Env_Service_File{Name: getPathEnd(filePath), Values: vals}
+				files = append(files, file)
+			}
+			service := &apinator.Vault_Env_Service{Name: getPathEnd(servicePath), Files: files}
+			services = append(services, service)
+		}
+		env := &apinator.Vault_Env{Name: environment, Services: services}
+		environments = append(environments, env)
+	}
+	//vault := Vault{Envs: environments}
+	//from each environment(dev, QA, secrets, local)
+	//get the services
+	//get the values
+	return &pb.Vault{
+		Envs: environments,
+	}, nil
+}
+func getPaths(mod *kv.Modifier, pathName string) []string {
+	secrets, err := mod.List(pathName)
+	pathList := []string{}
+	if err != nil {
+		panic(err)
+	} else if secrets != nil {
+		//add paths
+		slicey := secrets.Data["keys"].([]interface{})
+		//fmt.Println("secrets are")
+		//fmt.Println(slicey)
+		for _, pathEnd := range slicey {
+			//List is returning both pathEnd and pathEnd/
+			path := pathName + pathEnd.(string)
+			pathList = append(pathList, path)
+		}
+		return pathList
+	}
+	return pathList
+}
+func getPathEnd(path string) string {
+	strs := strings.Split(path, "/")
+	for strs[len(strs)-1] == "" {
+		strs = strs[:len(strs)-1]
+	}
+	return strs[len(strs)-1]
 }
