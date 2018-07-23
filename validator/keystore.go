6b1041dfeb47b7f3caeabfa0864b05e6f760e0fd
VIEWPOINT\Sara.wille <sara.wille@viewpoint.com>
2018-07-23 12:52:00 -0700

    Added keystore validation

---
+++ validator/keystore.go
@@ -0,0 +1,32 @@
+package validator
+
+import (
+	"crypto/rsa"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	pkcs "bitbucket.org/crypto/pkcs12"
+)
+
+//ValidateSendGrid validates the sendgrid API key.
+func ValidateKeyStore(filename string, pass string) (bool, error) {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return false, err
+	}
+	keys, certs, err := pkcs.DecodeAll(file, pass)
+
+	fmt.Println(len(keys))
+	fmt.Println(len(certs))
+	if err != nil {
+		return false, errors.New("failed to parse: " + err.Error())
+	}
+
+	if err := keys[0].(*rsa.PrivateKey).Validate(); err != nil {
+		fmt.Println("key validation didn't work")
+	}
+
+	isValid, err := VerifyCertificate(certs[0])
+	return isValid, err
+}
