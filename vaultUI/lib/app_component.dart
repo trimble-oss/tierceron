818ccc206bc6517cdbc06f75545b989d1bbbb28e
Max McKee <max.mckee@viewpoint.com>
2018-08-06 09:31:21 -0700

    Updated dart to 2.0.0.

--- vaultUI/lib/app_component.dart
+++ vaultUI/lib/app_component.dart
@@ -1,7 +1,6 @@
 import 'dart:html';
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/browser_client.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
