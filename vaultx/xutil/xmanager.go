d7819f9e7a06adf451f520a1b9d30b3c48c3561a
Robert Simons <robert.simons@viewpoint.com>
2019-06-17 15:10:25 -0700

    Initial xmanager and xparser
    --HG--
    branch : vault-x

---
+++ vaultx/xutil/xmanager.go
@@ -0,0 +1,8 @@
+package xutil
+
+import "log"
+
+// Manage configures the templates in vault_templates and writes them to vaultx
+func Manage(startDir string, endDir string, seed string, logger *log.Logger) {
+
+}
