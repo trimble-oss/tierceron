55cc134846d4675c14a0baa3b1ca3f28996f2625
Robert Simons <robert.simons@viewpoint.com>
2019-07-24 17:00:57 -0700

    Combine generated seed file(s) into one
    --HG--
    branch : vault-x

--- vaultx/xutil/xmanager.go
+++ vaultx/xutil/xmanager.go
@@ -6,78 +6,77 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"bitbucket.org/dexterchaney/whoville/utils"
 	"gopkg.in/yaml.v2"
 )
 
-//TODO
+// Declare global variables
+var templateCombinedSection interface{}
+
 // Manage configures the templates in vault_templates and writes them to vaultx
 func Manage(startDir string, endDir string, seed string, logger *log.Logger) {
-	//get files from directory
+
+	// TODO - possibly delete later
+	//sliceSections := []interface{}{[]interface{}{}, []map[string]map[string]map[string]string{}, []map[string]map[string]map[string]string{}, []int{}}
+
+	// Initialize global variables
+	valueCombinedSection := map[string]map[string]map[string]string{}
+	valueCombinedSection["values"] = map[string]map[string]string{}
+
+	secretCombinedSection := map[string]map[string]map[string]string{}
+	secretCombinedSection["super-secrets"] = map[string]map[string]string{}
+
+	// Declare local variables
+	sliceTemplateSection := []interface{}{}
+	sliceValueSection := []map[string]map[string]map[string]string{}
+	sliceSecretSection := []map[string]map[string]map[string]string{}
+	sliceTemplateDepth := []int{}
+
+	// Get files from directory
 	templatePaths, endPaths := getDirFiles(startDir, endDir)
-	//configure each template in directory
-	for i, templatePath := range templatePaths {
-		//check for template_files directory here
-		s := strings.Split(templatePath, "/")
-		//figure out which path is vault_templates
-		dirIndex := -1
-		for j, piece := range s {
-			if piece == "vault_templates" {
-				dirIndex = j
-			}
-		}
-		if dirIndex != -1 {
-			interfaceTemplateSection, valueSection, secretSection := ToSeed(templatePath, logger)
 
-			template, errT := yaml.Marshal(interfaceTemplateSection)
-			value, errV := yaml.Marshal(valueSection)
-			secret, errS := yaml.Marshal(secretSection)
+	// Configure each template in directory
+	for _, templatePath := range templatePaths {
+		interfaceTemplateSection, valueSection, secretSection, templateDepth := ToSeed(templatePath, logger)
 
-			if errT != nil {
-				fmt.Println(errT)
-			}
-
-			if errV != nil {
-				fmt.Println(errV)
-			}
-
-			if errS != nil {
-				fmt.Println(errS)
-			}
-
-			seedFile := string(template) + "\n\n\n" + string(value) + "\n\n\n" + string(secret)
-
-			writeToFile(seedFile, endPaths[i])
-		} else {
-			//assume the starting directory was vault_templates
-			interfaceTemplateSection, valueSection, secretSection := ToSeed(templatePath, logger)
-
-			template, errT := yaml.Marshal(interfaceTemplateSection)
-			value, errV := yaml.Marshal(valueSection)
-			secret, errS := yaml.Marshal(secretSection)
-
-			if errT != nil {
-				fmt.Println(errT)
-			}
-
-			if errV != nil {
-				fmt.Println(errV)
-			}
-
-			if errS != nil {
-				fmt.Println(errS)
-			}
-
-			seedFile := string(template) + "\n\n\n" + string(value) + "\n\n\n" + string(secret)
-
-			writeToFile(seedFile, endPaths[i])
-		}
+		// Append new sections to propper slices
+		sliceTemplateSection = append(sliceTemplateSection, interfaceTemplateSection)
+		sliceValueSection = append(sliceValueSection, valueSection)
+		sliceSecretSection = append(sliceSecretSection, secretSection)
+		sliceTemplateDepth = append(sliceTemplateDepth, templateDepth)
 	}
-	//print that we're done
+
+	// Combine values of slice
+	maxDepth := getMaxDepth(sliceTemplateDepth)
+	combineSection(nil, sliceTemplateSection, maxDepth, nil)
+	combineSection(sliceValueSection, nil, -1, valueCombinedSection)
+	combineSection(sliceSecretSection, nil, -1, secretCombinedSection)
+
+	// Create seed file structure
+	template, errT := yaml.Marshal(sliceTemplateSection)
+	value, errV := yaml.Marshal(valueCombinedSection)
+	secret, errS := yaml.Marshal(secretCombinedSection)
+
+	if errT != nil {
+		fmt.Println(errT)
+	}
+
+	if errV != nil {
+		fmt.Println(errV)
+	}
+
+	if errS != nil {
+		fmt.Println(errS)
+	}
+
+	seedFile := string(template) + "\n\n\n" + string(value) + "\n\n\n" + string(secret)
+	writeToFile(seedFile, endPaths[1]) // TODO: change this later
+
+	// Print that we're done
 	fmt.Println("seed created and written to ", endDir)
 }
+
 func writeToFile(data string, path string) {
 	byteData := []byte(data)
 	//Ensure directory has been created
@@ -125,4 +124,45 @@ func getDirFiles(dir string, endDir string) ([]string, []string) {
 		//add endings of path names
 	}
 	return filePaths, endPaths
+}
+
+// Get max depth of template
+func getMaxDepth(sliceTemplateDepth []int) int {
+	max := -1
+	for _, v := range sliceTemplateDepth {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
+// Combines the values in a slice, creating a singular map from multiple
+// Input:
+//	- slice to combine
+//	- template slice to combine
+//	- depth of map (-1 for value/secret sections)
+func combineSection(sliceSection []map[string]map[string]map[string]string, sliceTemplateSection []interface{}, maxDepth int, combinedSection map[string]map[string]map[string]string) map[string]map[string]map[string]string {
+
+	// Value/secret slice section
+	if maxDepth < 0 {
+
+		for _, v := range sliceSection {
+			for k2, v2 := range v {
+				for k3, v3 := range v2 {
+					combinedSection[k2][k3] = map[string]string{}
+					for k4, v4 := range v3 {
+						combinedSection[k2][k3][k4] = v4
+					}
+				}
+			}
+		}
+
+		// template slice section
+	} else {
+
+	}
+
+	return combinedSection
 }
