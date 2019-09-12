27e89feafd13e2f81ba66d8a6edf2a64a6f551be
Joel Rieke <joel.rieke@viewpoint.com>
2019-09-12 14:49:02 -0700

    Lost too many things in client setup.

--- vaulthelper/kv/generate_http_client.go
+++ vaulthelper/kv/generate_http_client.go
@@ -3,7 +3,9 @@ package kv
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"net"
 	"net/http"
+	"time"
 )
 
 //CreateHTTPClient reads from several .pem files to get the necessary keys and certs to configure the http client and returns the client.
@@ -22,8 +24,17 @@ func CreateHTTPClient() (client *http.Client, err error) {
 	// configure a client to use trust those certificates
 	httpClient := &http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig:   &tls.Config{RootCAs: certPool},
-			DisableKeepAlives: false,
+			TLSClientConfig: &tls.Config{RootCAs: certPool},
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			DisableKeepAlives:     false,
 		},
 	}
 	return httpClient, nil
