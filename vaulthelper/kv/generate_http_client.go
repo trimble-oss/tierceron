9c871b8798452c9449ca6254d09deb1a1f52ddb5
Joel Rieke <joel.rieke@viewpoint.com>
2019-09-13 16:11:22 +0000

    Merged PR 16910: Update master to match develop.
    Update master to match develop.

--- vaulthelper/kv/generate_http_client.go
+++ vaulthelper/kv/generate_http_client.go
@@ -3,7 +3,9 @@ package kv
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"net"
 	"net/http"
+	"time"
 )
 
 //CreateHTTPClient reads from several .pem files to get the necessary keys and certs to configure the http client and returns the client.
@@ -23,6 +25,17 @@ func CreateHTTPClient() (client *http.Client, err error) {
 	httpClient := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{RootCAs: certPool},
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			DisableKeepAlives:     false,
+			MaxConnsPerHost:       10,
 		},
 	}
 	return httpClient, nil
