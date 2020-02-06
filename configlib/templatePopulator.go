f828cee9421203c8cf3b888dea36d32aaf1fe96a
Joel Rieke <joel.rieke@viewpoint.com>
2020-02-05 17:53:01 -0800

    Actually return the result.

--- configlib/templatePopulator.go
+++ configlib/templatePopulator.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"C"
-	"fmt"
 
 	"bitbucket.org/dexterchaney/whoville/vaultconfig/utils"
 	"bitbucket.org/dexterchaney/whoville/vaulthelper/kv"
 )
 
 //export ConfigTemplateLib
-func ConfigTemplateLib(token string, address string, env string, templatePath string, configuredFilePath string, project string, service string) string {
-	fmt.Println(templatePath)
-
+func ConfigTemplateLib(token string, address string, env string, templatePath string, configuredFilePath string, project string, service string) *C.char {
 	mod, err := kv.NewModifier(token, address, env, nil)
 	mod.Env = env
 	if err != nil {
@@ -19,6 +16,7 @@ func ConfigTemplateLib(token string, address string, env string, templatePath st
 	}
 
 	configuredTemplate, _ := utils.ConfigTemplate(mod, templatePath, configuredFilePath, true, project, service, false)
-	return configuredTemplate
+
+	return C.CString(configuredTemplate)
 }
 func main() {}
