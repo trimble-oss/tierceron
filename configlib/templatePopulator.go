c9de69a92e7f7dabce38d02b74a4cb67e13a16dd
Joel Rieke <joel.rieke@viewpoint.com>
2020-02-12 14:49:44 -0800

    Adding config certs.

--- configlib/templatePopulator.go
+++ configlib/templatePopulator.go
@@ -3,12 +3,16 @@ package main
 import (
 	"C"
 
+	"encoding/base64"
+	"fmt"
+
 	"bitbucket.org/dexterchaney/whoville/vaultconfig/utils"
 	"bitbucket.org/dexterchaney/whoville/vaulthelper/kv"
 )
 
 //export ConfigTemplateLib
 func ConfigTemplateLib(token string, address string, env string, templatePath string, configuredFilePath string, project string, service string) *C.char {
+	fmt.Println("NCLib Version: " + "1.11")
 	mod, err := kv.NewModifier(token, address, env, nil)
 	mod.Env = env
 	if err != nil {
@@ -19,4 +23,21 @@ func ConfigTemplateLib(token string, address string, env string, templatePath st
 
 	return C.CString(configuredTemplate)
 }
+
+//export ConfigCertLib
+func ConfigCertLib(token string, address string, env string, templatePath string, configuredFilePath string, project string, service string) *C.char {
+	fmt.Println("NCLib Version: " + "1.11")
+	mod, err := kv.NewModifier(token, address, env, nil)
+	mod.Env = env
+	if err != nil {
+		panic(err)
+	}
+
+	_, configuredCert := utils.ConfigTemplate(mod, templatePath, configuredFilePath, true, project, service, true)
+
+	certBase64 := base64.StdEncoding.EncodeToString([]byte(configuredCert[1]))
+
+	return C.CString(certBase64)
+}
+
 func main() {}
