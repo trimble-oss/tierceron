095cb6b6a34eae1e55d5d994e1384f7801ec08a4
Joel Rieke <joel.rieke@viewpoint.com>
2020-10-30 12:39:14 -0700

    Adding better zc support.

--- configlib/templatePopulator.go
+++ configlib/templatePopulator.go
@@ -19,7 +19,7 @@ func ConfigTemplateLib(token string, address string, env string, templatePath st
 		panic(err)
 	}
 
-	configuredTemplate, _ := utils.ConfigTemplate(mod, templatePath, configuredFilePath, true, project, service, false)
+	configuredTemplate, _ := utils.ConfigTemplate(mod, templatePath, configuredFilePath, true, project, service, false, true)
 
 	mod.Close()
 
@@ -35,7 +35,7 @@ func ConfigCertLib(token string, address string, env string, templatePath string
 		panic(err)
 	}
 
-	_, configuredCert := utils.ConfigTemplate(mod, templatePath, configuredFilePath, true, project, service, true)
+	_, configuredCert := utils.ConfigTemplate(mod, templatePath, configuredFilePath, true, project, service, true, true)
 
 	mod.Close()
 
