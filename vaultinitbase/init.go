f01d9c7108e05ff40ced1b74075905a773c759f8
Joel Rieke <joel_rieke@trimble.com>
2021-08-30 14:25:50 -0700

    Fix problem found in new vault creation.

--- vaultinitbase/init.go
+++ vaultinitbase/init.go
@@ -324,7 +324,7 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 		// Upload policies from the given policy directory
 		il.UploadPolicies(namespacePolicyConfigs, v, false, logger)
 		// Upload tokens from the given token directory
-		tokens := il.UploadTokens(namespacePolicyConfigs, v, logger)
+		tokens := il.UploadTokens(namespaceTokenConfigs, v, logger)
 		if !*prodPtr {
 			tokenMap := map[string]interface{}{}
 			for _, token := range tokens {
