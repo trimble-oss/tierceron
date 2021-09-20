51b4f02d617ab1a0cb1d7d5035a6da32a957ae54
Karnveer Gill <karnveerSG@KGILL-US-LE01.am.trimblecorp.net>
2021-09-20 14:58:54 -0700

    Added -diff flag

--- vaultxbase/vaultxbase.go
+++ vaultxbase/vaultxbase.go
@@ -5,12 +5,130 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 
 	eUtils "Vault.Whoville/utils"
 	"Vault.Whoville/vaultx/xutil"
 )
+
+type ResultData struct {
+	inData *string
+	inPath string
+}
+
+var resultMap = make(map[string]*string)
+var envSlice = make([]string, 0)
+var resultChannel = make(chan *ResultData, 5)
+var envLength int
+var mutex = &sync.Mutex{}
+
+func messenger(inData *string, inPath string) {
+	var data ResultData
+	data.inData = inData
+	data.inPath = inPath
+	resultChannel <- &data
+}
+
+func reciever() {
+	for {
+		select {
+		case data := <-resultChannel:
+			if data != nil && data.inData != nil && data.inPath != "" {
+				mutex.Lock()
+				resultMap[data.inPath] = data.inData
+				mutex.Unlock()
+			}
+		default:
+		}
+	}
+}
+
+func diffHelper() {
+	keys := []string{}
+
+	//Arranges keys for ordered output
+	for _, env := range envSlice {
+		keys = append(keys, env+"||"+env+"_seed.yml")
+	}
+
+	Reset := "\033[0m"
+	Red := "\033[31m"
+	Green := "\033[32m"
+	Yellow := "\033[0;33m"
+
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Red = ""
+		Green = ""
+		Yellow = ""
+	}
+
+	keyA := keys[0]
+	keyB := keys[1]
+	keySplitA := strings.Split(keyA, "||")
+	keySplitB := strings.Split(keyB, "||")
+	mutex.Lock()
+	envFileKeyA := resultMap[keyA]
+	envFileKeyB := resultMap[keyB]
+	mutex.Unlock()
+
+	//Seperator
+	if runtime.GOOS == "windows" {
+		fmt.Printf("\n======================================================================================")
+	} else {
+		fmt.Printf("\n\033[1;35m======================================================================================\033[0m")
+	}
+	switch envLength {
+	case 4:
+		keyC := keys[2]
+		keyD := keys[3]
+		keySplitC := strings.Split(keyC, "||")
+		keySplitD := strings.Split(keyD, "||")
+		mutex.Lock()
+		envFileKeyC := resultMap[keyC]
+		envFileKeyD := resultMap[keyD]
+		mutex.Unlock()
+
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitA[0] + Reset + Green + " +Env-" + keySplitB[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyB, envFileKeyA))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitA[0] + Reset + Green + " +Env-" + keySplitC[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyC, envFileKeyA))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitA[0] + Reset + Green + " +Env-" + keySplitD[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyD, envFileKeyA))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitB[0] + Reset + Green + " +Env-" + keySplitC[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyC, envFileKeyB))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitB[0] + Reset + Green + " +Env-" + keySplitD[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyD, envFileKeyB))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitC[0] + Reset + Green + " +Env-" + keySplitD[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyD, envFileKeyC))
+	case 3:
+		keyC := keys[2]
+		keySplitC := strings.Split(keyC, "||")
+		mutex.Lock()
+		envFileKeyC := resultMap[keyC]
+		mutex.Unlock()
+
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitA[0] + Reset + Green + " +Env-" + keySplitB[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyB, envFileKeyA))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitA[0] + Reset + Green + " +Env-" + keySplitC[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyC, envFileKeyA))
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitB[0] + Reset + Green + " +Env-" + keySplitC[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyC, envFileKeyB))
+	default:
+		fmt.Print("\n" + Yellow + " (" + Reset + Red + "-Env-" + keySplitA[0] + Reset + Green + " +Env-" + keySplitB[0] + Reset + Yellow + ")" + Reset + "\n")
+		fmt.Println(eUtils.LineByLineDiff(envFileKeyB, envFileKeyA))
+	}
+
+	//Seperator
+	if runtime.GOOS == "windows" {
+		fmt.Println("======================================================================================")
+	} else {
+		fmt.Println("\033[1;35m======================================================================================\033[0m")
+	}
+	keys = keys[:0] //Cleans keys for next file
+}
 
 // CommonMain This executable automates the creation of seed files from template file(s).
 // New seed files are written (or overwrite current seed files) to the specified directory.
@@ -34,6 +152,7 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	noVaultPtr := flag.Bool("novault", false, "Don't pull configuration data from vault.")
 	pingPtr := flag.Bool("ping", false, "Ping vault.")
 	insecurePtr := flag.Bool("insecure", false, "By default, every ssl connection is secure.  Allows to continue with server connections considered insecure.")
+	diffPtr := flag.Bool("diff", false, "Diff files")
 
 	// Checks for proper flag input
 	args := os.Args[1:]
@@ -64,6 +183,34 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 		os.Exit(1)
 	}
 
+	//Diff flag parsing check
+	if *diffPtr {
+		if strings.ContainsAny(*envPtr, ",") { //Multiple environments
+			envSlice = strings.Split(*envPtr, ",")
+			envLength = len(envSlice)
+			if len(envSlice) > 4 {
+				fmt.Println("Unsupported number of environments - Maximum: 4")
+				os.Exit(1)
+			}
+			for _, env := range envSlice {
+				if env == "local" {
+					fmt.Println("Unsupported env: local not available with diff flag")
+					os.Exit(1)
+				}
+			}
+		} else {
+			fmt.Println("Incorrect format for diff: -env=env1,env2,...")
+			os.Exit(1)
+		}
+	} else { //non diff
+		if strings.ContainsAny(*envPtr, ",") { //Multiple environments
+			envSlice = strings.Split(*envPtr, ",")
+			envLength = len(envSlice)
+		} else {
+			envSlice = append(envSlice, *envPtr) //For single env
+		}
+	}
+
 	// Prints usage if no flags are specified
 	if *helpPtr {
 		flag.Usage()
@@ -85,13 +232,7 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 
 	regions := []string{}
 
-	//Split multiple environments into slice
-	envSlice := make([]string, 0)
-	if strings.ContainsAny(*envPtr, ",") {
-		envSlice = strings.Split(*envPtr, ",")
-	}
-
-	if len(envSlice) == 0 && !*noVaultPtr {
+	if len(envSlice) == 1 && !*noVaultPtr {
 		if *envPtr == "staging" || *envPtr == "prod" {
 			secretIDPtr = nil
 			appRoleIDPtr = nil
@@ -100,7 +241,7 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 		eUtils.AutoAuth(*insecurePtr, secretIDPtr, appRoleIDPtr, tokenPtr, tokenNamePtr, envPtr, addrPtr, *pingPtr)
 	}
 
-	if tokenPtr == nil || *tokenPtr == "" && !*noVaultPtr && len(envSlice) == 0 {
+	if tokenPtr == nil || *tokenPtr == "" && !*noVaultPtr && len(envSlice) == 1 {
 		fmt.Println("Missing required auth token.")
 		os.Exit(1)
 	}
@@ -121,12 +262,8 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	logger.SetPrefix("[vaultx]")
 	logger.Printf("Looking for template(s) in directory: %s\n", *startDirPtr)
 
-	//If 1 env only
-	if len(envSlice) == 0 {
-		envSlice = append(envSlice, *envPtr)
-	}
-
 	var waitg sync.WaitGroup
+	go reciever() //Channel reciever
 	for _, env := range envSlice {
 		*envPtr = env
 		if secretIDPtr != nil && *secretIDPtr != "" && appRoleIDPtr != nil && *appRoleIDPtr != "" {
@@ -150,7 +287,9 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 			WantCert:       false,
 			GenAuth:        *genAuth,
 			Log:            logger,
-			Diff:           *cleanPtr,
+			Clean:          *cleanPtr,
+			Diff:           *diffPtr,
+			Update:         messenger,
 		}
 		waitg.Add(1)
 		go func() {
@@ -159,6 +298,15 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 		}()
 	}
 	waitg.Wait()
+	close(resultChannel)
+	if *diffPtr { //Diff if needed
+		waitg.Add(1)
+		go func() {
+			defer waitg.Done()
+			diffHelper()
+		}()
+	}
+	waitg.Wait() //Wait for diff
 
 	logger.SetPrefix("[vaultx]")
 	logger.Println("=============== Terminating Seed Generator ===============")
