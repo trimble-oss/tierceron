ae4592855e459226aab37c9c1e9e5603427935f5
Joel Rieke <joel_rieke@trimble.com>
2021-09-22 16:11:34 -0700

    A few more rename cleanup.

--- trcUI/web/main.dart
+++ trcUI/web/main.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:vaultUI/app_component.template.dart' as ng;
+import 'package:trcUI/app_component.template.dart' as ng;
 
 import 'main.template.dart' as self;
 
