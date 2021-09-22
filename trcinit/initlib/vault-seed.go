25e1e96a86ebc9b51afacccd801b3502c62e4380
Joel Rieke <joel_rieke@trimble.com>
2021-09-22 15:44:26 -0700

    Code renaming to remove vault from tool naming.

---
+++ trcinit/initlib/vault-seed.go
@@ -0,0 +1,354 @@
+package initlib
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"tierceron/trcx/xutil"
+	"tierceron/utils"
+	"tierceron/validator"
+	"tierceron/vaulthelper/kv"
+
+	configcore "VaultConfig.Bootstrap/configcore"
+	"gopkg.in/yaml.v2"
+)
+
+// Used in the decomposition of the seed
+type seedCollection struct {
+	path string
+	data map[interface{}]interface{}
+}
+
+// Used for containing the actual paths and values to vault
+type writeCollection struct {
+	path string
+	data map[string]interface{}
+}
+
+// SeedVault seeds the vault with seed files in the given directory
+func SeedVault(insecure bool, dir string, addr string, token string, env string, logger *log.Logger, service string, uploadCert bool) {
+	logger.SetPrefix("[SEED]")
+	logger.Printf("Seeding vault from seeds in: %s\n", dir)
+
+	files, err := ioutil.ReadDir(dir)
+	utils.LogErrorObject(err, logger, true)
+
+	if len(files) == 1 && files[0].Name() == "certs" && uploadCert {
+		// Cert rotation support without templates
+		logger.Printf("No templates available, Common service requested.: %s\n", dir)
+
+		var templatePaths = configcore.GetSupportedTemplates()
+		regions := []string{}
+
+		if env == "staging" || env == "prod" {
+			regions = utils.GetSupportedProdRegions()
+		}
+
+		config := utils.DriverConfig{
+			Insecure:       insecure,
+			Token:          token,
+			VaultAddress:   addr,
+			Env:            env,
+			Regions:        regions,
+			SecretMode:     true, //  "Only override secret values in templates?"
+			ServicesWanted: []string{service},
+			StartDir:       append([]string{}, ""),
+			EndDir:         "",
+			WantCert:       false,
+			GenAuth:        false,
+			Log:            logger,
+		}
+
+		_, _, _, seedData := xutil.GenerateSeedsFromVaultRaw(config, true, templatePaths)
+
+		seedData = strings.ReplaceAll(seedData, "<Enter Secret Here>", "")
+
+		SeedVaultFromData(config.Insecure, []byte(seedData), addr, token, env, logger, service, true)
+		return
+	}
+
+	seeded := false
+	for _, file := range files {
+		if file.Name() == env || (strings.HasPrefix(env, "local") && file.Name() == "local") {
+			logger.Println("\tStepping into: " + file.Name())
+
+			filesSteppedInto, err := ioutil.ReadDir(dir + "/" + env)
+			utils.LogErrorObject(err, logger, true)
+
+			if len(filesSteppedInto) > 1 {
+				utils.CheckWarning(fmt.Sprintf("Multiple potentially conflicting configuration files found for evironment: %s", file.Name()), true)
+			}
+			for _, fileSteppedInto := range filesSteppedInto {
+				ext := filepath.Ext(fileSteppedInto.Name())
+				if ext == ".yaml" || ext == ".yml" { // Only read YAML config files
+					logger.Println("\t\t" + fileSteppedInto.Name())
+					logger.Printf("\tFound seed file: %s\n", fileSteppedInto.Name())
+					path := dir + "/" + env + "/" + fileSteppedInto.Name()
+					logger.Println("\tSeeding vault with: " + fileSteppedInto.Name())
+
+					SeedVaultFromFile(insecure, path, addr, token, env, logger, service, uploadCert)
+					seeded = true
+				}
+			}
+		}
+	}
+	if !seeded {
+		fmt.Println("Environment is not valid - Environment:", env)
+	}
+}
+
+//SeedVaultFromFile takes a file path and seeds the vault with the seeds found in an individual file
+func SeedVaultFromFile(insecure bool, filepath string, vaultAddr string, token string, env string, logger *log.Logger, service string, uploadCert bool) {
+	rawFile, err := ioutil.ReadFile(filepath)
+	// Open file
+	utils.LogErrorObject(err, logger, true)
+	SeedVaultFromData(insecure, rawFile, vaultAddr, token, env, logger, service, uploadCert)
+}
+
+//SeedVaultFromData takes file bytes and seeds the vault with contained data
+func SeedVaultFromData(insecure bool, fData []byte, vaultAddr string, token string, env string, logger *log.Logger, service string, uploadCert bool) {
+	logger.SetPrefix("[SEED]")
+	logger.Println("=========New File==========")
+	var verificationData map[interface{}]interface{} // Create a reference for verification. Can't run until other secrets written
+	// Unmarshal
+	var rawYaml interface{}
+	if bytes.Contains(fData, []byte("<Enter Secret Here>")) {
+		fmt.Println("Incomplete configuration of seed data.  Found default secret data: '<Enter Secret Here>'.  Refusing to continue.")
+		os.Exit(1)
+	}
+	err := yaml.Unmarshal(fData, &rawYaml)
+	utils.LogErrorObject(err, logger, true)
+	seed, ok := rawYaml.(map[interface{}]interface{})
+	if ok == false {
+		fmt.Println("Invalid yaml file.  Refusing to continue.")
+		os.Exit(1)
+	}
+
+	mapStack := []seedCollection{seedCollection{"", seed}} // Begin with root of yaml file
+	writeStack := make([]writeCollection, 0)               // List of all values to write to the vault with p
+
+	// While the stack is not empty
+	for len(mapStack) > 0 {
+		current := mapStack[0]
+		mapStack = mapStack[1:] // Pop the top value
+		writeVals := writeCollection{path: current.path, data: map[string]interface{}{}}
+		hasLeafNodes := false // Flag to signify this map had values to write
+
+		// Convert nested maps into vault writable data
+		for k, v := range current.data {
+			if v == nil { // Don't write empty valus, Vault does not handle them
+				if !uploadCert {
+					logger.Printf("Key with no value will not be written: %s\n", current.path+": "+k.(string))
+				}
+			} else if current.path == "" && k.(string) == "verification" { // Found verification on top level, store for later
+				verificationData = v.(map[interface{}]interface{})
+			} else if newData, ok := v.(map[interface{}]interface{}); ok { // Decompose into submaps, update path
+				decomp := seedCollection{
+					data: newData}
+				if len(current.path) == 0 {
+					decomp.path = k.(string)
+				} else {
+					decomp.path = current.path + "/" + k.(string)
+				}
+				mapStack = append([]seedCollection{decomp}, mapStack...)
+			} else { // Found a key value pair, add to working writeVal
+				writeVals.data[k.(string)] = v
+				hasLeafNodes = true
+			}
+		}
+		if hasLeafNodes { // Save all writable values in the current path
+			writeStack = append(writeStack, writeVals)
+		}
+	}
+
+	// Write values to vault
+	logger.Println("Seeding configuration data for the following templates:")
+	logger.Println("Please verify that these templates exist in each service")
+
+	mod, err := kv.NewModifier(insecure, token, vaultAddr, env, nil) // Connect to vault
+	utils.LogErrorObject(err, logger, true)
+	mod.Env = env
+	for _, entry := range writeStack {
+		// Output data being written
+		// Write data and ouput any errors
+		if strings.HasPrefix(entry.path, "values/") {
+			if certPathData, certPathOk := entry.data["certSourcePath"]; certPathOk {
+				if !uploadCert {
+					continue
+				}
+				certPath := fmt.Sprintf("%s", certPathData)
+				fmt.Println("Inspecting certificate: " + certPath + ".")
+
+				if strings.Contains(certPath, "ENV") {
+					if len(env) >= 5 && (env)[:5] == "local" {
+						envParts := strings.SplitN(env, "/", 3)
+						certPath = strings.Replace(certPath, "ENV", envParts[1], 1)
+					} else {
+						certPath = strings.Replace(certPath, "ENV", env, 1)
+					}
+				}
+				certPath = "trc_seeds/" + certPath
+				cert, err := ioutil.ReadFile(certPath)
+				utils.LogErrorObject(err, logger, false)
+				if err == nil {
+					//if pfx file size greater than 25 KB, print warning
+					if len(cert) > 32000 {
+						fmt.Println("Unreasonable size for certificate type file. Not written to vault")
+						continue
+					}
+
+					isValidCert := false
+					var certValidationErr error
+					if strings.HasSuffix(certPath, ".pfx") {
+						fmt.Println("Inspecting pfx: " + certPath + ".")
+						isValidCert, certValidationErr = validator.IsPfxRfc7292(cert)
+					} else if strings.HasSuffix(certPath, ".cer") {
+						fmt.Println("Inspecting cer: " + certPath + ".")
+						cert, certValidationErr := x509.ParseCertificate(cert)
+						if certValidationErr == nil {
+							isValidCert = true
+						} else {
+							fmt.Println("failed to parse and verify certificate: " + certValidationErr.Error())
+						}
+						var certHost string
+						if certHostData, certHostOk := entry.data["certHost"]; certHostOk {
+							certHost = fmt.Sprintf("%s", certHostData)
+						} else {
+							fmt.Println("Missing certHost, cannot validate cert.  Not written to vault")
+							continue
+						}
+						switch env {
+						case "dev":
+							certHost = strings.Replace(certHost, "*", "develop", 1)
+							break
+						case "QA":
+							certHost = strings.Replace(certHost, "*", "qa", 1)
+							break
+						case "performance":
+							certHost = strings.Replace(certHost, "*", "performance", 1)
+							break
+						}
+
+						opts := x509.VerifyOptions{
+							DNSName: certHost,
+						}
+
+						if _, err := cert.Verify(opts); err != nil {
+							if _, isUnknownAuthority := err.(x509.UnknownAuthorityError); !isUnknownAuthority {
+								fmt.Println("Unknown authority: failed to verify certificate: " + err.Error())
+								continue
+							}
+						}
+					} else if strings.HasSuffix(certPath, ".pem") {
+						fmt.Println("Inspecting pem: " + certPath + ".")
+						pemBlock, _ := pem.Decode(cert)
+						if pemBlock == nil {
+							fmt.Println("failed to verify certificate PEM.")
+						} else {
+							isValidCert = true
+						}
+					} else if strings.HasSuffix(certPath, ".jks") {
+						isValidCert = true
+					}
+					if isValidCert {
+						fmt.Println("Certificate passed validation: " + certPath + ".")
+						certBase64 := base64.StdEncoding.EncodeToString(cert)
+						if _, ok := entry.data["certData"]; ok {
+							// insecure value entry.
+							entry.data["certData"] = certBase64
+							fmt.Println("Public cert updated: " + certPath + ".")
+						} else {
+							entryPathParts := strings.Split(entry.path, "/")
+							if len(entryPathParts) == 2 {
+								secretPath := "super-secrets/" + entryPathParts[1]
+								done := false
+								// Look up in private entry.
+								for _, secretEntry := range writeStack {
+									if secretPath == secretEntry.path {
+										if _, ok := secretEntry.data["certData"]; ok {
+											secretEntry.data["certData"] = certBase64
+											WriteData(secretEntry.path, secretEntry.data, mod, logger)
+											WriteData(entry.path, entry.data, mod, logger)
+											done = true
+											break
+										}
+									}
+								}
+								fmt.Println("Cert loaded from: " + certPath + ".")
+
+								if done {
+									continue
+								}
+							}
+						}
+					} else {
+						fmt.Println("Cert validation failure.  Cert will not be loaded.", certValidationErr)
+						delete(entry.data, "certData")
+						delete(entry.data, "certHost")
+						delete(entry.data, "certSourcePath")
+						delete(entry.data, "certDestPath")
+						continue
+					}
+				} else {
+					fmt.Println("Missing expected cert at: " + certPath + ".  Cert will not be loaded.")
+					continue
+				}
+			}
+		} else {
+			if _, certPathOk := entry.data["certSourcePath"]; certPathOk {
+				if !uploadCert {
+					continue
+				}
+			}
+			if _, certDataOK := entry.data["certData"]; certDataOK {
+				if !uploadCert {
+					continue
+				}
+			}
+		}
+
+		if service != "" {
+			if strings.HasSuffix(entry.path, service) || strings.Contains(entry.path, "Common") {
+				WriteData(entry.path, entry.data, mod, logger)
+			}
+		} else {
+			WriteData(entry.path, entry.data, mod, logger)
+		}
+	}
+
+	// Run verification after seeds have been written
+	warn, err := verify(mod, verificationData, logger)
+	utils.LogErrorObject(err, logger, false)
+	utils.LogWarningsObject(warn, logger, false)
+	fmt.Printf("\nInitialization complete.\n")
+}
+
+//WriteData takes entry path and date from each iteration of writeStack in SeedVaultFromData and writes to vault
+func WriteData(path string, data map[string]interface{}, mod *kv.Modifier, logger *log.Logger) {
+	warn, err := mod.Write(path, data)
+
+	utils.LogWarningsObject(warn, logger, false)
+	utils.LogErrorObject(err, logger, false)
+	// Update value metrics to reflect credential use
+	root := strings.Split(path, "/")[0]
+	if root == "templates" {
+		//Printing out path of each entry so that users can verify that folder structure in seed files are correct
+
+		logger.Println("vault_" + path + ".*.tmpl")
+		for _, v := range data {
+			if templateKey, ok := v.([]interface{}); ok {
+				metricsKey := templateKey[0].(string) + "." + templateKey[1].(string)
+				mod.AdjustValue("value-metrics/credentials", metricsKey, 1)
+			}
+		}
+	}
+
+}
---
+++ trcx/xutil/xmanager.go
@@ -0,0 +1,409 @@
+package xutil
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	vcutils "tierceron/trcconfig/utils"
+	"tierceron/utils"
+	eUtils "tierceron/utils"
+	"tierceron/vaulthelper/kv"
+
+	"gopkg.in/yaml.v2"
+)
+
+var wg sync.WaitGroup
+var wg2 sync.WaitGroup
+
+type TemplateResultData struct {
+	interfaceTemplateSection interface{}
+	valueSection             map[string]map[string]map[string]string
+	secretSection            map[string]map[string]map[string]string
+	templateDepth            int
+	env                      string
+}
+
+var templateResultChan = make(chan *TemplateResultData, 5)
+
+// GenerateSeedsFromVaultRaw configures the templates in vault_templates and writes them to vaultx
+func GenerateSeedsFromVaultRaw(config eUtils.DriverConfig, fromVault bool, templatePaths []string) (string, string, bool, string) {
+	// Initialize global variables
+	valueCombinedSection := map[string]map[string]map[string]string{}
+	valueCombinedSection["values"] = map[string]map[string]string{}
+
+	secretCombinedSection := map[string]map[string]map[string]string{}
+	secretCombinedSection["super-secrets"] = map[string]map[string]string{}
+
+	// Declare local variables
+	templateCombinedSection := map[string]interface{}{}
+	sliceTemplateSection := []interface{}{}
+	sliceValueSection := []map[string]map[string]map[string]string{}
+	sliceSecretSection := []map[string]map[string]map[string]string{}
+	maxDepth := -1
+
+	project := ""
+	endPath := ""
+	multiService := false
+	service := ""
+	var mod *kv.Modifier
+	noVault := false
+
+	if config.Token != "" {
+		var err error
+		mod, err = kv.NewModifier(config.Insecure, config.Token, config.VaultAddress, config.Env, config.Regions)
+		if err != nil {
+			panic(err)
+		}
+		mod.Env = config.Env
+		if config.Token == "novault" {
+			noVault = true
+		}
+	}
+
+	if config.GenAuth && mod != nil {
+		_, err := mod.ReadData("apiLogins/meta")
+		if err != nil {
+			fmt.Println("Cannot genAuth with provided token.")
+			os.Exit(1)
+		}
+	}
+
+	//Reciever for configs
+	go func(c eUtils.DriverConfig) {
+		for {
+			select {
+			case tResult := <-templateResultChan:
+				if config.Env == tResult.env {
+					sliceTemplateSection = append(sliceTemplateSection, tResult.interfaceTemplateSection)
+					sliceValueSection = append(sliceValueSection, tResult.valueSection)
+					sliceSecretSection = append(sliceSecretSection, tResult.secretSection)
+					if tResult.templateDepth > maxDepth {
+						maxDepth = tResult.templateDepth
+						//templateCombinedSection = interfaceTemplateSection
+					}
+					wg.Done()
+				} else {
+					go func(tResult *TemplateResultData) {
+						templateResultChan <- tResult
+					}(tResult)
+				}
+			default:
+			}
+		}
+	}(config)
+
+	// Configure each template in directory
+	for _, templatePath := range templatePaths {
+		wg.Add(1)
+		go func(templatePath string, project string, service string, multiService bool, c eUtils.DriverConfig, noVault bool) {
+			// Map Subsections
+			var templateResult TemplateResultData
+
+			templateResult.valueSection = map[string]map[string]map[string]string{}
+			templateResult.valueSection["values"] = map[string]map[string]string{}
+
+			templateResult.secretSection = map[string]map[string]map[string]string{}
+			templateResult.secretSection["super-secrets"] = map[string]map[string]string{}
+
+			var goMod *kv.Modifier
+
+			if c.Token != "" {
+				var err error
+				goMod, err = kv.NewModifier(c.Insecure, c.Token, c.VaultAddress, c.Env, c.Regions)
+				if err != nil {
+					panic(err)
+				}
+				goMod.Env = c.Env
+			}
+
+			if c.GenAuth && goMod != nil {
+				_, err := mod.ReadData("apiLogins/meta")
+				if err != nil {
+					fmt.Println("Cannot genAuth with provided token.")
+					os.Exit(1)
+				}
+			}
+
+			//check for template_files directory here
+			s := strings.Split(templatePath, "/")
+			//figure out which path is vault_templates
+			dirIndex := -1
+			for j, piece := range s {
+				if piece == "vault_templates" {
+					dirIndex = j
+				}
+			}
+			if dirIndex != -1 {
+				project = s[dirIndex+1]
+				if service != s[dirIndex+2] {
+					multiService = true
+				}
+				service = s[dirIndex+2]
+			}
+
+			// Clean up service naming (Everything after '.' removed)
+			dotIndex := strings.Index(service, ".")
+			if dotIndex > 0 && dotIndex <= len(service) {
+				service = service[0:dotIndex]
+			}
+
+			var cds *vcutils.ConfigDataStore
+			if goMod != nil && !noVault {
+				cds = new(vcutils.ConfigDataStore)
+				cds.Init(goMod, c.SecretMode, true, project, service)
+			}
+
+			_, _, _, templateResult.templateDepth = ToSeed(goMod,
+				cds,
+				templatePath,
+				config.Log,
+				project,
+				service,
+				fromVault,
+				&(templateResult.interfaceTemplateSection),
+				&(templateResult.valueSection),
+				&(templateResult.secretSection),
+			)
+			templateResult.env = goMod.Env
+			templateResultChan <- &templateResult
+		}(templatePath, project, service, multiService, config, noVault)
+	}
+	wg.Wait()
+
+	// Combine values of slice
+	combineSection(sliceTemplateSection, maxDepth, templateCombinedSection)
+	combineSection(sliceValueSection, -1, valueCombinedSection)
+	combineSection(sliceSecretSection, -1, secretCombinedSection)
+
+	var authYaml []byte
+	var errA error
+
+	// Add special auth section.
+	if config.GenAuth {
+		if mod != nil {
+			connInfo, err := mod.ReadData("apiLogins/meta")
+			if err == nil {
+				authSection := map[string]interface{}{}
+				authSection["apiLogins"] = map[string]interface{}{}
+				authSection["apiLogins"].(map[string]interface{})["meta"] = connInfo
+				authYaml, errA = yaml.Marshal(authSection)
+				if errA != nil {
+					fmt.Println(errA)
+				}
+			} else {
+				fmt.Println("Attempt to gen auth for reduced privilege token failed.  No permissions to gen auth.")
+				os.Exit(1)
+			}
+		} else {
+			authConfigurations := map[string]interface{}{}
+			authConfigurations["authEndpoint"] = "<Enter Secret Here>"
+			authConfigurations["pass"] = "<Enter Secret Here>"
+			authConfigurations["sessionDB"] = "<Enter Secret Here>"
+			authConfigurations["user"] = "<Enter Secret Here>"
+			authConfigurations["vaultApiTokenSecret"] = "<Enter Secret Here>"
+
+			authSection := map[string]interface{}{}
+			authSection["apiLogins"] = map[string]interface{}{}
+			authSection["apiLogins"].(map[string]interface{})["meta"] = authConfigurations
+			authYaml, errA = yaml.Marshal(authSection)
+			if errA != nil {
+				fmt.Println(errA)
+			}
+		}
+	}
+
+	// Create seed file structure
+	template, errT := yaml.Marshal(templateCombinedSection)
+	value, errV := yaml.Marshal(valueCombinedSection)
+	secret, errS := yaml.Marshal(secretCombinedSection)
+
+	if errT != nil {
+		fmt.Println(errT)
+	}
+
+	if errV != nil {
+		fmt.Println(errV)
+	}
+
+	if errS != nil {
+		fmt.Println(errS)
+	}
+	templateData := string(template)
+	// Remove single quotes generated by Marshal
+	templateData = strings.ReplaceAll(templateData, "'", "")
+	seedData := templateData + "\n\n\n" + string(value) + "\n\n\n" + string(secret) + "\n\n\n" + string(authYaml)
+
+	return service, endPath, multiService, seedData
+}
+
+// GenerateSeedsFromVault configures the templates in vault_templates and writes them to vaultx
+func GenerateSeedsFromVault(config eUtils.DriverConfig) {
+	if config.Clean { //Clean flag in vaultX
+		_, err1 := os.Stat(config.EndDir + config.Env)
+		err := os.RemoveAll(config.EndDir + config.Env)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if err1 == nil {
+			fmt.Println("Seed removed from", config.EndDir+config.Env)
+		}
+		return
+	}
+
+	// Get files from directory
+	tempTemplatePaths := []string{}
+	for _, startDir := range config.StartDir {
+		//get files from directory
+		tp := getDirFiles(startDir)
+		tempTemplatePaths = append(tempTemplatePaths, tp...)
+	}
+
+	//Duplicate path remover
+	keys := make(map[string]bool)
+	templatePaths := []string{}
+	for _, path := range tempTemplatePaths {
+		if _, value := keys[path]; !value {
+			keys[path] = true
+			templatePaths = append(templatePaths, path)
+		}
+	}
+
+	_, endPath, multiService, seedData := GenerateSeedsFromVaultRaw(config, false, templatePaths)
+
+	if multiService {
+		if strings.HasPrefix(config.Env, "local") {
+			endPath = config.EndDir + "local/local_seed.yml"
+		} else {
+			endPath = config.EndDir + config.Env + "/" + config.Env + "_seed.yml"
+		}
+	} else {
+		endPath = config.EndDir + config.Env + "/" + config.Env + "_seed.yml"
+	}
+
+	if config.Diff {
+		config.Update(&seedData, config.Env+"||"+config.Env+"_seed.yml")
+	} else {
+		writeToFile(seedData, endPath)
+		// Print that we're done
+		fmt.Println("Seed created and written to " + strings.Replace(config.EndDir, "\\", "/", -1) + config.Env)
+	}
+
+}
+
+func writeToFile(data string, path string) {
+	byteData := []byte(data)
+	//Ensure directory has been created
+	dirPath := filepath.Dir(path)
+	err := os.MkdirAll(dirPath, os.ModePerm)
+	utils.CheckError(err, true)
+	//create new file
+	newFile, err := os.Create(path)
+	utils.CheckError(err, true)
+	//write to file
+	_, err = newFile.Write(byteData)
+	utils.CheckError(err, true)
+	err = newFile.Sync()
+	utils.CheckError(err, true)
+	newFile.Close()
+}
+
+func getDirFiles(dir string) []string {
+	files, err := ioutil.ReadDir(dir)
+	filePaths := []string{}
+	//endPaths := []string{}
+	if err != nil {
+		//this is a file
+		return []string{dir}
+	}
+	for _, file := range files {
+		//add this directory to path names
+		filename := file.Name()
+		if strings.HasSuffix(filename, ".DS_Store") {
+			continue
+		}
+		extension := filepath.Ext(filename)
+		filePath := dir + file.Name()
+		if !strings.HasSuffix(dir, "/") {
+			filePath = dir + "/" + file.Name()
+		}
+		if extension == "" {
+			//if subfolder add /
+			filePath += "/"
+		}
+		//recurse to next level
+		newPaths := getDirFiles(filePath)
+		filePaths = append(filePaths, newPaths...)
+	}
+	return filePaths
+}
+
+// MergeMaps - merges 2 maps recursively.
+func MergeMaps(x1, x2 interface{}) interface{} {
+	switch x1 := x1.(type) {
+	case map[string]interface{}:
+		x2, ok := x2.(map[string]interface{})
+		if !ok {
+			return x1
+		}
+		for k, v2 := range x2 {
+			if v1, ok := x1[k]; ok {
+				x1[k] = MergeMaps(v1, v2)
+			} else {
+				x1[k] = v2
+			}
+		}
+	case nil:
+		x2, ok := x2.(map[string]interface{})
+		if ok {
+			return x2
+		}
+	}
+	return x1
+}
+
+// Combines the values in a slice, creating a singular map from multiple
+// Input:
+//	- slice to combine
+//	- template slice to combine
+//	- depth of map (-1 for value/secret sections)
+func combineSection(sliceSectionInterface interface{}, maxDepth int, combinedSectionInterface interface{}) {
+	_, okMap := sliceSectionInterface.([]map[string]map[string]map[string]string)
+
+	// Value/secret slice section
+	if maxDepth < 0 && okMap {
+		sliceSection := sliceSectionInterface.([]map[string]map[string]map[string]string)
+		combinedSectionImpl := combinedSectionInterface.(map[string]map[string]map[string]string)
+		for _, v := range sliceSection {
+			for k2, v2 := range v {
+				for k3, v3 := range v2 {
+					if _, ok := combinedSectionImpl[k2][k3]; !ok {
+						combinedSectionImpl[k2][k3] = map[string]string{}
+					}
+					for k4, v4 := range v3 {
+						combinedSectionImpl[k2][k3][k4] = v4
+					}
+				}
+			}
+		}
+
+		combinedSectionInterface = combinedSectionImpl
+
+		// template slice section
+	} else {
+		if maxDepth < 0 && !okMap {
+			fmt.Printf("Env failed to gen.  MaxDepth: %d, okMap: %t\n", maxDepth, okMap)
+		}
+		sliceSection := sliceSectionInterface.([]interface{})
+
+		for _, v := range sliceSection {
+			MergeMaps(combinedSectionInterface, v)
+		}
+	}
+}
