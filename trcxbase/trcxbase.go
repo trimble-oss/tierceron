f20010e45df635bfb82b28da33b25b416975e51e
Karnveer Gill <karnveerSG@KGILL-US-LE01.am.trimblecorp.net>
2021-10-05 15:24:04 -0700

    Added -version flag and support for version diffs

--- trcxbase/trcxbase.go
+++ trcxbase/trcxbase.go
@@ -6,11 +6,15 @@ import (
 	"log"
 	"os"
 	"runtime"
+	"sort"
+	"strconv"
 	"strings"
 	"sync"
 
 	"tierceron/trcx/xutil"
 	eUtils "tierceron/utils"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 type ResultData struct {
@@ -130,6 +134,68 @@ func diffHelper() {
 	keys = keys[:0] //Cleans keys for next file
 }
 
+func removeDuplicateValues(intSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range intSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+func versionHelper(versionData map[string]interface{}, templateOrValues bool, valuePath string) {
+	Reset := "\033[0m"
+	Cyan := "\033[36m"
+	Red := "\033[31m"
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Cyan = ""
+		Red = ""
+	}
+	if versionData == nil {
+		fmt.Println("No version data found for this environment")
+		os.Exit(1)
+	}
+	for _, versionMetadata := range versionData {
+		for field, data := range versionMetadata.(map[string]interface{}) {
+			if field == "destroyed" && !data.(bool) {
+				goto printOutput
+			}
+		}
+	}
+	return
+printOutput:
+	fmt.Println(Cyan + "======================================================================================" + Reset)
+	keys := make([]int, 0, len(versionData))
+	for versionNumber, _ := range versionData {
+		versionNo, _ := strconv.ParseInt(versionNumber, 10, 64)
+		keys = append(keys, int(versionNo))
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		versionNumber := key
+		versionMetadata := versionData[fmt.Sprint(key)]
+		fields := make([]string, 0)
+		fieldData := make(map[string]interface{}, 0)
+		for field, data := range versionMetadata.(map[string]interface{}) {
+			fields = append(fields, field)
+			fieldData[field] = data
+		}
+		sort.Strings(fields)
+		fmt.Println("Version " + fmt.Sprint(versionNumber) + " Metadata:")
+		for _, field := range fields {
+			fmt.Printf(field + ": ")
+			fmt.Println(fieldData[field])
+		}
+		if keys[len(keys)-1] != versionNumber {
+			fmt.Println(Red + "-------------------------------------------------------------------------------" + Reset)
+		}
+	}
+	fmt.Println(Cyan + "======================================================================================" + Reset)
+}
+
 // CommonMain This executable automates the creation of seed files from template file(s).
 // New seed files are written (or overwrite current seed files) to the specified directory.
 func CommonMain(envPtr *string, addrPtrIn *string) {
@@ -138,21 +204,22 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	if addrPtrIn != nil && *addrPtrIn != "" {
 		addrPtr = addrPtrIn
 	}
-	startDirPtr := flag.String("startDir", "trc_templates", "Pull templates from this directory")
-	endDirPtr := flag.String("endDir", "./trc_seeds/", "Write generated seed files to this directory")
-	logFilePtr := flag.String("log", "./var/log/trcx.log", "Output path for log file")
-	helpPtr := flag.Bool("h", false, "Provide options for trcx")
+	startDirPtr := flag.String("startDir", "vault_templates", "Pull templates from this directory")
+	endDirPtr := flag.String("endDir", "./vault_seeds/", "Write generated seed files to this directory")
+	logFilePtr := flag.String("log", "./var/log/vaultx.log", "Output path for log file")
+	helpPtr := flag.Bool("h", false, "Provide options for vaultx")
 	tokenPtr := flag.String("token", "", "Vault access token")
 	secretMode := flag.Bool("secretMode", true, "Only override secret values in templates?")
 	genAuth := flag.Bool("genAuth", false, "Generate auth section of seed data?")
 	cleanPtr := flag.Bool("clean", false, "Cleans seed files locally")
 	secretIDPtr := flag.String("secretID", "", "Secret app role ID")
 	appRoleIDPtr := flag.String("appRoleID", "", "Public app role ID")
-	tokenNamePtr := flag.String("tokenName", "", "Token name used by this trcx to access the vault")
+	tokenNamePtr := flag.String("tokenName", "", "Token name used by this vaultx to access the vault")
 	noVaultPtr := flag.Bool("novault", false, "Don't pull configuration data from vault.")
 	pingPtr := flag.Bool("ping", false, "Ping vault.")
 	insecurePtr := flag.Bool("insecure", false, "By default, every ssl connection is secure.  Allows to continue with server connections considered insecure.")
 	diffPtr := flag.Bool("diff", false, "Diff files")
+	versionPtr := flag.Bool("version", false, "Gets version metadata information")
 
 	// Checks for proper flag input
 	args := os.Args[1:]
@@ -165,6 +232,13 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	}
 
 	flag.Parse()
+
+	Yellow := "\033[33m"
+	Reset := "\033[0m"
+	if runtime.GOOS == "windows" {
+		Reset = ""
+		Yellow = ""
+	}
 
 	//check for clean + env flag
 	cleanPresent := false
@@ -181,36 +255,78 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	if cleanPresent && !envPresent {
 		fmt.Println("Environment must be defined with -env=env1,... for -clean usage")
 		os.Exit(1)
+	} else if *diffPtr && *versionPtr {
+		fmt.Println("-version flag cannot be used with -diff flag")
+		os.Exit(1)
+	}
+
+	//Duplicate env checker
+	if len(envSlice) > 1 {
+		removeDuplicateValuesSlice := removeDuplicateValues(envSlice)
+		if !cmp.Equal(envSlice, removeDuplicateValuesSlice) {
+			fmt.Println("There is a duplicate environment in the -env flag")
+			os.Exit(1)
+		}
+	}
+
+	if *versionPtr {
+		if strings.Contains(*envPtr, ",") {
+			fmt.Println(Yellow + "Invalid environment, please specify one environment." + Reset)
+			os.Exit(1)
+		}
+		envVersion := strings.Split(*envPtr, "_")
+		if len(envVersion) > 1 && envVersion[1] != "" && envVersion[1] != "0" {
+
+			fmt.Println(Yellow + "Specified versioning not available, using " + envVersion[0] + " as environment" + Reset)
+		}
+		envSlice = append(envSlice, *envPtr+"_versionInfo")
+		goto skipDiff
 	}
 
 	//Diff flag parsing check
 	if *diffPtr {
 		if strings.ContainsAny(*envPtr, ",") { //Multiple environments
+			*envPtr = strings.ReplaceAll(*envPtr, "latest", "0")
 			envSlice = strings.Split(*envPtr, ",")
 			envLength = len(envSlice)
 			if len(envSlice) > 4 {
 				fmt.Println("Unsupported number of environments - Maximum: 4")
 				os.Exit(1)
 			}
-			for _, env := range envSlice {
+			for i, env := range envSlice {
 				if env == "local" {
 					fmt.Println("Unsupported env: local not available with diff flag")
 					os.Exit(1)
+				}
+				if !strings.Contains(env, "_") {
+					envSlice[i] = env + "_0"
 				}
 			}
 		} else {
 			fmt.Println("Incorrect format for diff: -env=env1,env2,...")
 			os.Exit(1)
 		}
-	} else { //non diff
-		if strings.ContainsAny(*envPtr, ",") { //Multiple environments
-			envSlice = strings.Split(*envPtr, ",")
-			envLength = len(envSlice)
+	} else {
+		if strings.ContainsAny(*envPtr, ",") {
+			fmt.Println("-diff flag is required for multiple environments - env: -env=env1,env2,...")
+			os.Exit(1)
+		}
+		envSlice = append(envSlice, (*envPtr))
+		envVersion := strings.Split(*envPtr, "_") //Break apart env+version for token
+		*envPtr = envVersion[0]
+		eUtils.AutoAuth(*insecurePtr, secretIDPtr, appRoleIDPtr, tokenPtr, tokenNamePtr, envPtr, addrPtr, *pingPtr)
+		if len(envVersion) >= 2 { //Put back env+version together
+			*envPtr = envVersion[0] + "_" + envVersion[1]
+			if envVersion[1] == "" {
+				fmt.Println("Must declare desired version number after '_' : -env=env1_ver1")
+				os.Exit(1)
+			}
 		} else {
-			envSlice = append(envSlice, *envPtr) //For single env
+			*envPtr = envVersion[0] + "_0"
 		}
 	}
 
+skipDiff:
 	// Prints usage if no flags are specified
 	if *helpPtr {
 		flag.Usage()
@@ -226,8 +342,8 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	}
 
 	// If logging production directory does not exist and is selected log to local directory
-	if _, err := os.Stat("./var/log/"); os.IsNotExist(err) && *logFilePtr == "./var/log/trcx.log" {
-		*logFilePtr = "./trcx.log"
+	if _, err := os.Stat("./var/log/"); os.IsNotExist(err) && *logFilePtr == "./var/log/vaultx.log" {
+		*logFilePtr = "./vaultx.log"
 	}
 
 	regions := []string{}
@@ -256,16 +372,18 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	// Initialize logging
 	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	eUtils.CheckError(err, true)
-	logger := log.New(f, "[trcx]", log.LstdFlags)
+	logger := log.New(f, "[vaultx]", log.LstdFlags)
 	logger.Println("=============== Initializing Seed Generator ===============")
 
-	logger.SetPrefix("[trcx]")
+	logger.SetPrefix("[vaultx]")
 	logger.Printf("Looking for template(s) in directory: %s\n", *startDirPtr)
 
 	var waitg sync.WaitGroup
 	go reciever() //Channel reciever
 	for _, env := range envSlice {
-		*envPtr = env
+		envVersion := strings.Split(env, "_") //Break apart env+version for token
+		*envPtr = envVersion[0]
+		*tokenPtr = ""
 		if secretIDPtr != nil && *secretIDPtr != "" && appRoleIDPtr != nil && *appRoleIDPtr != "" {
 			*tokenPtr = ""
 		}
@@ -273,6 +391,12 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 			eUtils.AutoAuth(*insecurePtr, secretIDPtr, appRoleIDPtr, tokenPtr, tokenNamePtr, envPtr, addrPtr, *pingPtr)
 		} else {
 			*tokenPtr = "novault"
+		}
+
+		if len(envVersion) >= 2 { //Put back env+version together
+			*envPtr = envVersion[0] + "_" + envVersion[1]
+		} else {
+			*envPtr = envVersion[0] + "_0"
 		}
 		config := eUtils.DriverConfig{
 			Insecure:       *insecurePtr,
@@ -290,6 +414,7 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 			Clean:          *cleanPtr,
 			Diff:           *diffPtr,
 			Update:         messenger,
+			VersionInfo:    versionHelper,
 		}
 		waitg.Add(1)
 		go func() {
@@ -308,7 +433,7 @@ func CommonMain(envPtr *string, addrPtrIn *string) {
 	}
 	waitg.Wait() //Wait for diff
 
-	logger.SetPrefix("[trcx]")
+	logger.SetPrefix("[vaultx]")
 	logger.Println("=============== Terminating Seed Generator ===============")
 	logger.SetPrefix("[END]")
 	logger.Println()
