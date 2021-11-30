00d5adf3219f70c64d47be458dd5c0675ed6575d
Karnveer Gill <karnveerSG@KGILL-US-LE01.am.trimblecorp.net>
2021-11-29 16:25:19 -0800

    Optimized enterprise filtering & Fixed unsafe bool switch

--- trcx/xutil/xmanager.go
+++ trcx/xutil/xmanager.go
@@ -183,8 +183,35 @@ func GenerateSeedsFromVaultRaw(config eUtils.DriverConfig, fromVault bool, templ
 		}
 		commonMod.Close()
 	}
+	serviceFound := false
+	// Configure each template in directory
+	for _, templatePath := range templatePaths {
+		var service string
+		_, service, templatePath = vcutils.GetProjectService(templatePath)
+		//This checks whether a enterprise env has the relevant project otherwise env gets skipped when generating seed files.
+		if strings.Contains(mod.Env, ".") && !serviceFound {
+			listValues, err := mod.ListEnv("values/" + mod.Env + "/") //Fix values to add to project to directory
+			if err != nil {
+				fmt.Println(err)
+			}
+			serviceSlice := make([]string, 0)
+			for _, valuesPath := range listValues.Data {
+				for _, envInterface := range valuesPath.([]interface{}) {
+					env := envInterface.(string)
+					serviceSlice = append(serviceSlice, env)
+				}
+			}
+			for _, listedService := range serviceSlice {
+				if strings.Contains(listedService, service) {
+					serviceFound = true
+				}
+			}
+		}
+	}
 
-	projectFound := false
+	if !serviceFound { //Exit for irrelevant enterprises
+		return "", false, ""
+	}
 	// Configure each template in directory
 	for _, templatePath := range templatePaths {
 		wg.Add(1)
@@ -224,25 +251,6 @@ func GenerateSeedsFromVaultRaw(config eUtils.DriverConfig, fromVault bool, templ
 
 			//check for template_files directory here
 			project, service, templatePath = vcutils.GetProjectService(templatePath)
-			//This checks whether a enterprise env has the relevant project otherwise env gets skipped when generating seed files.
-			if strings.Contains(goMod.Env, ".") && !projectFound {
-				listValues, err := goMod.ListEnv("values/" + goMod.Env + "/")
-				if err != nil {
-					fmt.Println(err)
-				}
-				projectSlice := make([]string, 0)
-				for _, valuesPath := range listValues.Data {
-					for _, envInterface := range valuesPath.([]interface{}) {
-						env := envInterface.(string)
-						projectSlice = append(projectSlice, env)
-					}
-				}
-				for _, listedProject := range projectSlice {
-					if strings.Contains(listedProject, project) {
-						projectFound = true
-					}
-				}
-			}
 
 			requestedVersion := goMod.Version
 			var cds *vcutils.ConfigDataStore
@@ -279,9 +287,6 @@ func GenerateSeedsFromVaultRaw(config eUtils.DriverConfig, fromVault bool, templ
 	}
 	wg.Wait()
 
-	if !projectFound { //Exit for irrelevant enterprises
-		return "", false, ""
-	}
 	// Combine values of slice
 	combineSection(sliceTemplateSection, maxDepth, templateCombinedSection)
 	combineSection(sliceValueSection, -1, valueCombinedSection)
