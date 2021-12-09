6e76ecb0a0c89dd6e70cd3196f5c51125c89db38
Joel Rieke <joel_rieke@trimble.com>
2021-12-09 11:13:56 -0800

    Updating config to match template.

--- trcvault/factory/config.go
+++ trcvault/factory/config.go
@@ -1,7 +1,12 @@
 package factory
 
 type EnvConfig struct {
-	Env      string `yaml:"-"`
+	Env string `yaml:"-"`
+	// Mysql access.
+	MysqlDburl    string `yaml:"mysqldburl,omitempty"`
+	MysqlDbuser   string `yaml:"mysqldbuser,omitempty"`
+	MysqlDbpasswd string `yaml:"mysqldbpassword,omitempty"`
+	// Local db access.
 	Dbuser   string `yaml:"dbuser,omitempty"`
 	Dbpasswd string `yaml:"dbpassword,omitempty"`
 	Maxconn  string `yaml:"maxconn,omitempty"` // Maximum allowed db connections
