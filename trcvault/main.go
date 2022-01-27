f37ba5693fcf927e98acdce029e0533fa9c0f604
Joel Rieke <joel_rieke@trimble.com>
2022-01-27 06:52:15 -0800

    Fixing some local startup issues.

--- trcvault/main.go
+++ trcvault/main.go
@@ -4,38 +4,73 @@ import (
 	"log"
 	"os"
 	"tierceron/trcvault/factory"
+	vscutils "tierceron/trcvault/util"
 	eUtils "tierceron/utils"
+	"tierceron/utils/mlock"
 
 	"github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/sdk/plugin"
 )
 
 func main() {
-	// mLockErr := unix.Mlockall(unix.MCL_CURRENT | unix.MCL_FUTURE)
-	// if mLockErr != nil {
-	// 	fmt.Println(mLockErr)
-	// 	os.Exit(-1)
-	// }
+	mlock.Mlock()
+
 	f, logErr := os.OpenFile("trcvault.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	eUtils.CheckError(logErr, true)
 	logger := log.New(f, "[trcvault]", log.LstdFlags)
 	factory.Init(logger)
 
-	logger.Println("=============== Initializing Vault Tierceron Plugin ===============")
-
 	apiClientMeta := api.PluginAPIClientMeta{}
 	flags := apiClientMeta.FlagSet()
-	argErr := flags.Parse(os.Args[1:])
+
+	args := os.Args
+	vaultHost, lvherr := vscutils.GetLocalVaultHost(false)
+	if lvherr != nil {
+		logger.Println("Host lookup failure.")
+		os.Exit(-1)
+	}
+
+	if vaultHost == "https://vault.whoboot.org" {
+		logger.Println("Running in developer mode with self signed certs.")
+		args = append(args, "--tls-skip-verify=true")
+	} else {
+		// TODO: this may not be needed...
+		//	args = append(args, fmt.Sprintf("--ca-cert=", caPEM))
+	}
+
+	argErr := flags.Parse(args[1:])
 	eUtils.LogErrorObject(argErr, logger, true)
-	logger.Println("Vault Tierceron Plugin Args parsed")
+	logger.Print("Warming up...")
 
 	tlsConfig := apiClientMeta.GetTLSConfig()
 	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+
+	/*
+		signalChannel := make(chan os.Signal, 1)
+		signal.Notify(signalChannel,
+			syscall.SIGHUP,
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGQUIT)
+
+		go func() {
+			for {
+				select {
+				case s := <-signalChannel:
+					log.Println("Got signal:", s)
+					log.Println("Received shutdown presumably from vault.")
+					os.Exit(0)
+				}
+			}
+		}()
+	*/
 
 	err := plugin.Serve(&plugin.ServeOpts{
 		BackendFactoryFunc: factory.TrcFactory,
 		TLSProviderFunc:    tlsProviderFunc,
 	})
-	eUtils.LogErrorObject(err, logger, true)
-	logger.Println("=============== Vault Tierceron Plugin Initialization complete ===============")
+	if err != nil {
+		logger.Println("Plugin shutting down")
+		os.Exit(1)
+	}
 }
