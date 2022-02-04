403976bf8cb0543ceba3be23ed59323949369c9e
Joel Rieke <joel_rieke@trimble.com>
2022-02-04 14:03:16 -0800

    Comment out the broken bits.

--- trctest/maintest.go
+++ trctest/maintest.go
@@ -16,20 +16,20 @@ func main() {
 	eUtils.CheckError(err, true)
 	logger := log.New(f, "[trcdbplugin]", log.LstdFlags)
 
-	tokenEnvMap := map[string]interface{}{}
-	tokenEnvMap["address"] = "https://vault.whoboot.org:8200" //This should be local
-	tokenEnvMap["token"] = "s.KnIlXXqTY5QFe54JE1Oypttr"
+	pluginConfig := map[string]interface{}{}
+	pluginConfig["address"] = "https://vault.whoboot.org:8200" //This should be local
+	pluginConfig["token"] = "s.KnIlXXqTY5QFe54JE1Oypttr"
 	// TenantConfiguration, SpectrumEnterpriseConfig, Mysqlfile
-	tokenEnvMap["templatePath"] = []string{
+	pluginConfig["templatePath"] = []string{
 		"trc_templates/TenantDatabase/TenantConfiguration/TenantConfiguration.tmpl",           // implemented
 		"trc_templates/TenantDatabase/SpectrumEnterpriseConfig/SpectrumEnterpriseConfig.tmpl", // not yet implemented.
-		"trc_templates/TenantDatabase/KafkaTableConfiguration/KafkaTableConfiguration.tmpl",   // not yet implemented.
+		//		"trc_templates/TenantDatabase/KafkaTableConfiguration/KafkaTableConfiguration.tmpl",   // not yet implemented.
 		//		"trc_templates/TenantDatabase/Mysqlfile/Mysqlfile.tmpl",                               // not yet implemented.
 	}
 
 	// plugin configs here...
 	// plugin configs here...
-	tokenEnvMap["connectionPath"] = []string{
+	pluginConfig["connectionPath"] = []string{
 		"trc_templates/TrcVault/VaultDatabase/config.yml.tmpl", // implemented
 		"trc_templates/TrcVault/Database/config.yml.tmpl",      // implemented
 		"trc_templates/TrcVault/Identity/config.yml.tmpl",      // implemented
@@ -37,6 +37,9 @@ func main() {
 		//		"trc_templates/TenantDatabase/KafkaTableConfiguration/KafkaTableConfiguration.tmpl",   // not yet implemented.
 		//		"trc_templates/TenantDatabase/Mysqlfile/Mysqlfile.tmpl",                               // not yet implemented.
 	}
-	tokenEnvMap["env"] = "QA"
-	vscutils.ProcessFlows(tokenEnvMap, logger)
+	pluginConfig["env"] = "QA"
+	pluginConfig["regions"] = []string{}
+	pluginConfig["insecure"] = true
+
+	vscutils.ProcessFlows(pluginConfig, logger)
 }
