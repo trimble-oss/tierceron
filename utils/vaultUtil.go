2fc4816f09d0e490a25ead7fe08c8fe217b9cbb2
Joel Rieke <joel_rieke@trimble.com>
2022-02-14 08:48:02 -0800

    Fail fast while developing.

--- utils/vaultUtil.go
+++ utils/vaultUtil.go
@@ -8,23 +8,31 @@ import (
 
 // Helper to easiliy intialize a vault and a mod all at once.
 func InitVaultMod(config *DriverConfig) (*helperkv.Modifier, *sys.Vault, error) {
-	mod, err := helperkv.NewModifier(config.Insecure, config.Token, config.VaultAddress, config.Env, config.Regions, config.Log)
-	mod.Env = config.Env
-	mod.Version = "0"
-	mod.VersionFilter = config.VersionFilter
-
-	vault, err := sys.NewVault(true, config.VaultAddress, mod.Env, false, false, false, config.Log)
+	vault, err := sys.NewVault(true, config.VaultAddress, config.Env, false, false, config.ExitOnFailure, config.Log)
 	if err != nil {
 		LogErrorObject(err, config.Log, false)
 		return nil, nil, err
 	}
 	vault.SetToken(config.Token)
 
+	mod, err := helperkv.NewModifier(config.Insecure, config.Token, config.VaultAddress, config.Env, config.Regions, config.Log)
+	if err != nil {
+		LogErrorObject(err, config.Log, false)
+		return nil, nil, err
+	}
+	mod.Env = config.Env
+	mod.Version = "0"
+	mod.VersionFilter = config.VersionFilter
+
 	return mod, vault, nil
 }
 
 // Helper to easiliy intialize a vault and a mod all at once.
 func InitVaultModForPlugin(pluginConfig map[string]interface{}, logger *log.Logger) (*helperkv.Modifier, *sys.Vault, error) {
+	exitOnFailure := false
+	if _, ok := pluginConfig["exitOnFailure"]; ok {
+		exitOnFailure = pluginConfig["exitOnFailure"].(bool)
+	}
 	config := DriverConfig{
 		Insecure:       pluginConfig["insecure"].(bool),
 		Token:          pluginConfig["token"].(string),
@@ -37,6 +45,7 @@ func InitVaultModForPlugin(pluginConfig map[string]interface{}, logger *log.Logg
 		EndDir:         "",
 		WantCerts:      false,
 		GenAuth:        false,
+		ExitOnFailure:  exitOnFailure,
 		Log:            logger,
 	}
 
