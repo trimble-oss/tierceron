e55f362164901fd93d206f4e20831f00245ad819
Joel Rieke <joel_rieke@trimble.com>
2022-02-22 09:48:56 -0800

    Refactor more exit code.

--- trcx/xdbutil/xdbmanager.go
+++ trcx/xdbutil/xdbmanager.go
@@ -8,20 +8,20 @@ import (
 )
 
 // GenerateSeedsFromVaultToDb pulls all data from vault for each template into a database
-func GenerateSeedsFromVaultToDb(config eUtils.DriverConfig) interface{} {
+func GenerateSeedsFromVaultToDb(config eUtils.DriverConfig) (interface{}, error) {
 	if config.Diff { //Clean flag in trcx
 		_, err1 := os.Stat(config.EndDir + config.Env)
 		err := os.RemoveAll(config.EndDir + config.Env)
 
 		if err != nil {
 			eUtils.LogErrorObject(err, config.Log, false)
-			os.Exit(1)
+			return nil, err
 		}
 
 		if err1 == nil {
 			eUtils.LogInfo("Seed removed from"+config.EndDir+config.Env, config.Log)
 		}
-		return nil
+		return nil, nil
 	}
 
 	// Get files from directory
@@ -46,8 +46,8 @@ func GenerateSeedsFromVaultToDb(config eUtils.DriverConfig) interface{} {
 		templatePaths, config.Env, config.VersionFilter[0])
 	if err != nil {
 		eUtils.LogErrorObject(err, config.Log, false)
-		os.Exit(1)
+		return nil, err
 	}
 
-	return tierceronEngine
+	return tierceronEngine, nil
 }
