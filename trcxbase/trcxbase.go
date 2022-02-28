21bd1686fc197a1e929482c13b52ed49fc5b29a8
Karnveer Gill <karnveer_gill@trimble.com>
2022-02-28 11:11:34 -0800

    Making -indexFilter flag required for -indexed flag

--- trcxbase/trcxbase.go
+++ trcxbase/trcxbase.go
@@ -146,6 +146,9 @@ func CommonMain(ctx eUtils.ProcessContext, configDriver eUtils.ConfigDriver, env
 	} else if *diffPtr && *versionPtr {
 		fmt.Println("-version flag cannot be used with -diff flag")
 		os.Exit(1)
+	} else if len(*indexServiceFilterPtr) == 0 && len(*indexedPtr) != 0 {
+		fmt.Println("-indexFilter must be specificed to use -indexed flag")
+		os.Exit(1)
 	} else if (len(*filterTemplatePtr) == 0 || len(*indexServiceFilterPtr) == 0) && *diffPtr && len(*indexedPtr) != 0 {
 		fmt.Println("-templateFilter & -indexFilter must be specificed to use -indexed & -diff flag")
 		os.Exit(1)
