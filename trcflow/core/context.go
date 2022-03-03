5bb548353b3d17f8749db2fca299ce49f6f65b2a
Joel Rieke <joel_rieke@trimble.com>
2022-03-03 10:00:50 -0800

    Refactor to use flow type.

--- trcflow/core/context.go
+++ trcflow/core/context.go
@@ -12,6 +12,7 @@ import (
 )
 
 type FlowType int64
+type FlowNameType string
 
 const (
 	TableSyncFlow FlowType = iota
@@ -19,17 +20,25 @@ const (
 	TableTestFlow
 )
 
+func (fnt FlowNameType) TableName() string {
+	return string(fnt)
+}
+
+func (fnt FlowNameType) ServiceName() string {
+	return string(fnt)
+}
+
 type TrcFlowMachineContext struct {
 	Env                      string
 	TierceronEngine          *db.TierceronEngine
 	ExtensionAuthData        map[string]interface{}
-	FlowMap                  map[string]interface{}
+	FlowMap                  map[FlowNameType]interface{} // Map of flows.
 	CallGetFlowConfiguration func(trcFlowContext *TrcFlowContext, templatePath string) (map[string]interface{}, bool)
 	CallAPI                  func(apiAuthHeaders map[string]string, host string, apiEndpoint string, bodyData io.Reader, getOrPost bool) (map[string]interface{}, error)
 	CallGetDbConn            func(dbUrl string, username string, sourceDBConfig map[string]interface{}) (*sql.DB, error)
 	CallAddTableSchema       func(schema sqle.PrimaryKeySchema, tableName string)
 	CallCreateTableTriggers  func(trcFlowContext *TrcFlowContext, identityColumnName string)
-	CallDBQuery              func(trcFlowContext *TrcFlowContext, insertQuery string, changed bool, operation string, flowNotifications []string) [][]string
+	CallDBQuery              func(trcFlowContext *TrcFlowContext, insertQuery string, changed bool, operation string, flowNotifications []FlowNameType) [][]string
 	CallSyncTableCycle       func(trcFlowContext *TrcFlowContext, identityColumnName string, vaultIndexColumnName string, flowPushRemote func(map[string]interface{}, map[string]interface{}) error)
 	CallLog                  func(msg string, err error)
 }
@@ -37,10 +46,9 @@ type TrcFlowMachineContext struct {
 type TrcFlowContext struct {
 	RemoteDataSource map[string]interface{}
 	GoMod            *helperkv.Modifier
-	FlowSource       string // The name of the flow source identified by project.
-	FlowService      string // The name of the flow source identified by service.
-	FlowSourceAlias  string // May be a database name
-	FlowName         string // May be a table name.
+	FlowSource       string       // The name of the flow source identified by project.
+	FlowSourceAlias  string       // May be a database name
+	Flow             FlowNameType // May be a table name.
 	FlowPath         string
 	FlowData         interface{}
 	ChangeFlowName   string // Change flow table name.
