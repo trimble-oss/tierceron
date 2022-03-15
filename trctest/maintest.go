062a6c062dc5d32d40ce6091dea4b80917d4f00e
Karnveer Gill <karnveer_gill@trimble.com>
2022-03-15 14:44:29 -0700

    Resolving PR comments

--- trctest/maintest.go
+++ trctest/maintest.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	trcflow "tierceron/trcflow/flumen"
 	eUtils "tierceron/utils"
+
+	tcutil "VaultConfig.TenantConfig/util"
 )
 
 // This executable automates the creation of seed files from template file(s).
@@ -16,32 +18,7 @@ func main() {
 	logger := log.New(f, "[trcdbplugin]", log.LstdFlags)
 	eUtils.CheckError(&eUtils.DriverConfig{Log: logger, ExitOnFailure: true}, err, true)
 
-	pluginConfig := map[string]interface{}{}
-	pluginConfig["address"] = "https://vault.whoboot.org:8200" //This should be local
-	pluginConfig["token"] = "s.2OamEH93aN4psYH7XmtWODCy"
-
-	// TenantConfiguration, SpectrumEnterpriseConfig, Mysqlfile
-	pluginConfig["templatePath"] = []string{
-		"trc_templates/TenantDatabase/TenantConfiguration/TenantConfiguration.tmpl",           // implemented
-		"trc_templates/TenantDatabase/SpectrumEnterpriseConfig/SpectrumEnterpriseConfig.tmpl", // not yet implemented.
-		//		"trc_templates/TenantDatabase/KafkaTableConfiguration/KafkaTableConfiguration.tmpl",   // not yet implemented.
-		//		"trc_templates/TenantDatabase/Mysqlfile/Mysqlfile.tmpl",                               // not yet implemented.
-	}
-
-	// plugin configs here...
-	// plugin configs here...
-	pluginConfig["connectionPath"] = []string{
-		"trc_templates/TrcVault/VaultDatabase/config.yml.tmpl", // implemented
-		"trc_templates/TrcVault/Database/config.yml.tmpl",      // implemented
-		"trc_templates/TrcVault/Identity/config.yml.tmpl",      // implemented
-		//		"trc_templates/TenantDatabase/SpectrumEnterpriseConfig/SpectrumEnterpriseConfig.tmpl", // not yet implemented.
-		//		"trc_templates/TenantDatabase/KafkaTableConfiguration/KafkaTableConfiguration.tmpl",   // not yet implemented.
-		//		"trc_templates/TenantDatabase/Mysqlfile/Mysqlfile.tmpl",                               // not yet implemented.
-	}
-	pluginConfig["env"] = "QA"
-	pluginConfig["regions"] = []string{}
-	pluginConfig["insecure"] = true
-	pluginConfig["exitOnFailure"] = true
+	pluginConfig := tcutil.GetPluginConfig()
 
 	trcflow.ProcessFlows(pluginConfig, logger)
 }
