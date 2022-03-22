89c132ef591cf85c8cd80a5f908ebb849d7fd3d6
Joel Rieke <joel_rieke@trimble.com>
2022-03-22 10:46:11 -0700

    Refactor for better plugin support.

--- trctest/maintest.go
+++ trctest/maintest.go
@@ -14,11 +14,14 @@ import (
 // New seed files are written (or overwrite current seed files) to the specified directory.
 func main() {
 	logFilePtr := flag.String("log", "./trcdbplugin.log", "Output path for log file")
+	tokenPtr := flag.String("token", "", "Vault access token")
+	flag.Parse()
+
 	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	logger := log.New(f, "[trcdbplugin]", log.LstdFlags)
 	eUtils.CheckError(&eUtils.DriverConfig{Log: logger, ExitOnFailure: true}, err, true)
 
-	pluginConfig := tcutil.GetPluginConfig()
+	pluginConfig := tcutil.GetTestConfig(*tokenPtr)
 
 	trcflow.ProcessFlows(pluginConfig, logger)
 }
