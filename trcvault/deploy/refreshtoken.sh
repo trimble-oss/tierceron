07e7da2d0da906bdedb4c870ab4e7029d3dcd514
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2022-03-25 15:10:55 -0700

    Plugin registration scripts (#46)
    * Installation scripts to get vault plugin registered and running
    
    * Also add a refreshtoken in case plugin fails.
    
    * Adding support for staging, too.
    
    * Combine logic.
    
    * Added sha256 to artifacts
    
    * Excluded plugin as artifact
    
    * Updating comment to indicate we are verifying only.
    
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>

---
+++ trcvault/deploy/refreshtoken.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Enter vault host base url: "
+read VAULT_ADDR
+
+echo "Enter root token: "
+read VAULT_TOKEN
+
+echo "Enter environment: "
+read VAULT_ENV
+
+echo "Enter environment token with write permissions: "
+read VAULT_ENV_TOKEN
+
+vault write vaultdb/$VAULT_ENV token=$VAULT_ENV_TOKEN
+
