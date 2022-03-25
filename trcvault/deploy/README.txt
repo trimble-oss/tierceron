0365f84a26112c89e0266fa2eac1b0734968a63c
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2022-03-25 15:10:55 -0700

    Plugin registration scripts (#46)
    * Installation scripts to get vault plugin registered and running
    
    * Also add a refreshtoken in case plugin fails.
    
    * Adding support for staging, too.
    
    * Combine logic.
    
    * Added sha256 to artifacts
    
    * Excluded plugin as artifact
    
    * Updating comment to indicate we are verifying only.
    
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>

---
+++ trcvault/deploy/README.txt
@@ -0,0 +1,5 @@
+Instructions for deploying the Tierceron Vault Plugin follows.
+
+1. Download the latest sha256 artifact to this directory.
+2. Run ./deploy.sh, providing inputs so it has the right permissions etc to function.
+3. Sit back and relax.
---
+++ trcvault/deploy/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Enter vault host base url: "
+read VAULT_ADDR
+
+echo "Enter root token: "
+read VAULT_TOKEN
+
+echo "Enter environment: "
+read VAULT_ENV
+
+echo "Enter environment token with write permissions: "
+read VAULT_ENV_TOKEN
+
+if [ "$VAULT_ENV" = "prod" ] || [ "$VAULT_ENV" = "staging" ]
+then
+vault plugin register \
+          -command=trc-vault-plugin-prod \
+          -sha256=$( cat trc-vault-plugin-prod.sha256 ) \
+          -args=`backendUUID=4` \
+          trc-vault-plugin-prod
+vault secrets enable \
+          -path=vaultdb \
+          -plugin-name=trc-vault-plugin-prod \
+          -description="Tierceron Vault Plugin Prod" \
+          plugin
+else
+vault plugin register \
+          -command=trc-vault-plugin \
+          -sha256=$( cat trc-vault-plugin.sha256 ) \
+          -args=`backendUUID=4` \
+          trc-vault-plugin
+vault secrets enable \
+          -path=vaultdb \
+          -plugin-name=trc-vault-plugin \
+          -description="Tierceron Vault Plugin" \
+          plugin
+fi
+
+vault write vaultdb/$VAULT_ENV token=$VAULT_ENV_TOKEN
