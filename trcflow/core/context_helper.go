13b464e1fab69174f49abd40d44ca8fd49708228
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-03-25 14:13:28 -0700

    Added Delete Functionality (#45)
    * Adding functionality for delete writebacks
    
    * Added check for only SEC deletions

--- trcflow/core/context_helper.go
+++ trcflow/core/context_helper.go
@@ -79,7 +79,22 @@ func (tfmContext *TrcFlowMachineContext) vaultPersistPushRemoteChanges(
 			continue
 		}
 
-		if len(changedTableRowData) == 0 {
+		if len(changedTableRowData) == 0 && err == nil { //This change was a delete
+			if tfContext.Flow.TableName() != "SpectrumEnterpriseConfig" { //TODO: Add delete functionality for other tables? - logic is in SEC push remote
+				continue
+			}
+			//Check if it exists in trcdb
+			//Writeback to mysql to delete that
+			rowDataMap := map[string]interface{}{}
+			rowDataMap["Deleted"] = "true"
+			rowDataMap["changedId"] = changedId
+			for _, column := range changedTableColumns {
+				rowDataMap[column] = ""
+			}
+			pushError := flowPushRemote(tfContext.RemoteDataSource, rowDataMap)
+			if pushError != nil {
+				eUtils.LogErrorObject(tfmContext.Config, err, false)
+			}
 			continue
 		}
 
