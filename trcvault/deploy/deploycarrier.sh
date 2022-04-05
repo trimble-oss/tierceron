35dcdf6de43a216b06a9878ee2d68b768a97477a
Joel Rieke <joel_rieke@trimble.com>
2022-04-05 11:05:10 -0700

    Adding vault carrier plugin.

---
+++ trcvault/deploy/deploycarrier.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Enter vault host base url: "
+read VAULT_ADDR
+
+echo "Enter root token: "
+read VAULT_TOKEN
+
+echo "Enter environment: "
+read VAULT_ENV
+
+echo "Enter environment token with write permissions: "
+read VAULT_ENV_TOKEN
+
+# TODO: make api call to trc carrier with sha256...
+# input (plugin name)
+# output sha256 or error (if plugin has not been copied and a copy attempt failed).
+
+vault secrets disable vaultcarrier/
+vault plugin deregister trc-vault-carrier-plugin
+
+if [ "$VAULT_ENV" = "prod" ] || [ "$VAULT_ENV" = "staging" ]
+then
+vault plugin register \
+          -command=trc-vault-carrier-plugin-prod \
+          -sha256=$( cat target/trc-vault-carrier-plugin-prod.sha256 ) \
+          -args=`backendUUID=4` \
+          plugin
+vault secrets enable \
+          -path=vaultcarrier \
+          -plugin-name=trc-vault-carrier-plugin-prod \
+          -description="Tierceron Vault Carrier Plugin Prod" \
+          plugin
+else
+vault plugin register \
+          -command=trc-vault-carrier-plugin \
+          -sha256=$( cat target/trc-vault-plugin.sha256 ) \
+          -args=`backendUUID=4` \
+          trc-vault-plugin
+vault secrets enable \
+          -path=vaultcarrier \
+          -plugin-name=trc-vault-carrier-plugin \
+          -description="Tierceron Vault Carrier Plugin" \
+          plugin
+fi
+
+vault write vaultcarrier/$VAULT_ENV token=$VAULT_ENV_TOKEN
+
+# TODO: run trcplgtool -certify -deployed
