b5037bd37a183dbf4467d3d6ba64f6156c39d75f
Joel Rieke <joel_rieke@trimble.com>
2022-04-06 14:31:22 -0700

    Adding support for env variables.

--- trcvault/deploy/deploycarrier.sh
+++ trcvault/deploy/deploycarrier.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+if [[ -z "${VAULT_ADDR}" ]]; then
 echo "Enter vault host base url: "
 read VAULT_ADDR
+fi
 
+if [[ -z "${VAULT_TOKEN}" ]]; then
 echo "Enter root token: "
 read VAULT_TOKEN
+fi
 
+if [[ -z "${VAULT_ENV}" ]]; then
 echo "Enter environment: "
 read VAULT_ENV
+fi
 
+if [[ -z "${VAULT_ENV_TOKEN}" ]]; then
 echo "Enter environment token with write permissions: "
 read VAULT_ENV_TOKEN
+fi
 
 # TODO: make api call to trc carrier with sha256...
 # input (plugin name)
