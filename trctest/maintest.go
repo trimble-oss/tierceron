6a8193e0bca38c4cd34a1e43ffa6df0de94a0cda
Karnveer Gill <karnveer_gill@trimble.com>
2022-04-06 10:34:28 -0700

    Small change

--- trctest/maintest.go
+++ trctest/maintest.go
@@ -14,7 +14,7 @@ import (
 // New seed files are written (or overwrite current seed files) to the specified directory.
 func main() {
 	logFilePtr := flag.String("log", "./trcdbplugin.log", "Output path for log file")
-	tokenPtr := flag.String("token", "", "Vault access token")
+	tokenPtr := flag.String("token", "", "Vault access Token")
 	flag.Parse()
 
 	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
