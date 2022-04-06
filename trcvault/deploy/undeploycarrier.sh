ddfac7c4e6e8d28d5264a3d0fa91180f6d5a34c3
Karnveer Gill <karnveer_gill@trimble.com>
2022-04-06 09:28:18 -0700

    Added undeploy for carrier

---
+++ trcvault/deploy/undeploycarrier.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../../../Vault.Hashicorp
+
+echo "Enter vault host base url: "
+read VAULT_ADDR
+
+echo "Enter root token: "
+read VAULT_TOKEN
+
+vault secrets disable vaultcarrier/
+
+vault plugin deregister trc-vault-carrier-plugin
+#rm vault/data/core/plugin-catalog/secret/_trc-vault-carrier-plugin
+#rm plugins/*
+
