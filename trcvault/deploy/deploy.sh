4796a09e1266b9fb1b525702242434ea6fa37fef
Karnveer Gill <karnveer_gill@trimble.com>
2022-04-08 10:29:14 -0700

    Removing IFs

--- trcvault/deploy/deploy.sh
+++ trcvault/deploy/deploy.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-if [[ -z "${VAULT_ADDR}" ]]; then
 echo "Enter vault host base url: "
 read VAULT_ADDR
-fi
 
-if [[ -z "${VAULT_TOKEN}" ]]; then
 echo "Enter root token: "
 read VAULT_TOKEN
-fi
 
-if [[ -z "${VAULT_ENV}" ]]; then
 echo "Enter environment: "
 read VAULT_ENV
-fi
 
-if [[ -z "${VAULT_ENV_TOKEN}" ]]; then
 echo "Enter environment token with write permissions: "
 read VAULT_ENV_TOKEN
-fi
 
 vault secrets disable vaultdb/
 vault plugin deregister trc-vault-plugin
