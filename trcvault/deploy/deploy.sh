43a4dc464efe26e9803d648b6710fb7e8d678792
Joel Rieke <joel_rieke@trimble.com>
2022-04-14 15:33:29 -0700

    Removing dev variables.

--- trcvault/deploy/deploy.sh
+++ trcvault/deploy/deploy.sh
@@ -3,25 +3,17 @@
 echo "Enter plugin name: "
 read TRC_PLUGIN_NAME
 
-if [[ -z "${VAULT_ADDR}" ]]; then
 echo "Enter vault host base url: "
 read VAULT_ADDR
-fi
 
-if [[ -z "${VAULT_TOKEN}" ]]; then
 echo "Enter root token: "
 read VAULT_TOKEN
-fi
 
-if [[ -z "${VAULT_ENV}" ]]; then
 echo "Enter environment: "
 read VAULT_ENV
-fi
 
-if [[ -z "${VAULT_ENV_TOKEN}" ]]; then
 echo "Enter environment token with write permissions: "
 read VAULT_ENV_TOKEN
-fi
 
 echo "Precertify plugin: "
 read PRE_CERTIFY
