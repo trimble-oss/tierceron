736ed9dca2e75889716b4d958650aa140950face
Karnveer Gill <karnveer_gill@trimble.com>
2022-05-04 16:14:59 -0700

    Adding dynamic plugin name

--- trcvault/deploy/undeploy.sh
+++ trcvault/deploy/undeploy.sh
@@ -8,12 +8,16 @@ read VAULT_ADDR
 echo "Enter root token: "
 read VAULT_TOKEN
 
+echo "Enter plugin name: "
+read TRC_PLUGIN_NAME
+
 export VAULT_ADDR
 export VAULT_TOKEN
+export TRC_PLUGIN_NAME
 
-vault secrets disable trc-vault-plugin/
+vault secrets disable $TRC_PLUGIN_NAME/
 
-vault plugin deregister trc-vault-plugin
+vault plugin deregister $TRC_PLUGIN_NAME
 #rm vault/data/core/plugin-catalog/secret/_trc-vault-plugin
 #rm plugins/*
 
