f6c44598d2531667edc83af3ee7da04a789bbcef
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-05-26 14:16:24 -0700

    Merging Develop to Main (#99)
    * Fixing -novault for trcx
    
    * Update version of go. (#91)
    
    * Merging Harbinger into Develop  (#98)
    
    * Adding new harbinger build for trcdb
    
    * Adding harbinger
    
    * Merging Develop into Harbinger (#85)
    
    * Adding better support for plugin ip connections. (#81)
    
    * Adding better support for plugin ip connections.
    
    * Using insecure for plugins because always local.
    
    * Adding better support for plugin ip connections. (#81) (#82)
    
    * Adding better support for plugin ip connections.
    
    * Using insecure for plugins because always local.
    
    * Fixing crash of plugin due to missing certification.  Also remove port scanning.
    
    * Clean some logging.
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    * Adding cert template
    
    * Adding checkCopied flag
    
    * Adding cert template
    
    * Removing old cert
    
    * Fixing -novault for trcx
    
    * Fixing -novault certs
    
    * Fixing novault certs
    
    * Delete serv_cert.pem.mf.tmpl
    
    * Updating go.mod
    
    * Reverting go.mod
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>

--- trcflow/flumen/process.go
+++ trcflow/flumen/process.go
@@ -20,6 +20,7 @@ import (
 	sys "tierceron/vaulthelper/system"
 
 	tcutil "VaultConfig.TenantConfig/util"
+	"VaultConfig.TenantConfig/util/harbinger"
 	sqle "github.com/dolthub/go-mysql-server/sql"
 )
 
@@ -281,6 +282,11 @@ func ProcessFlows(pluginConfig map[string]interface{}, logger *log.Logger) error
 	// The engine is in tfmContext...  that's the one we need to make available for connecting via dbvis...
 	// be sure to enable encryption on the connection...
 
+	harbingerErr := harbinger.BuildInterface(config, goMod, tfmContext, vaultDatabaseConfig)
+	if harbingerErr != nil {
+		eUtils.LogErrorMessage(config, "Failed to start up database interface:"+harbingerErr.Error(), false)
+		return harbingerErr
+	}
 	wg.Wait()
 	logger.Println("ProcessFlows complete.")
 
