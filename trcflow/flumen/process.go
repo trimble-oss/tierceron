220e8e8474a8bec96e3c67a02759bad60ae6ae39
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-05-31 14:57:56 -0700

    Merging Develop into Main (#101)
    * Fixing -novault for trcx
    
    * Update version of go. (#91)
    
    * Merging Harbinger into Develop  (#98)
    
    * Adding new harbinger build for trcdb
    
    * Adding harbinger
    
    * Merging Develop into Harbinger (#85)
    
    * Adding better support for plugin ip connections. (#81)
    
    * Adding better support for plugin ip connections.
    
    * Using insecure for plugins because always local.
    
    * Adding better support for plugin ip connections. (#81) (#82)
    
    * Adding better support for plugin ip connections.
    
    * Using insecure for plugins because always local.
    
    * Fixing crash of plugin due to missing certification.  Also remove port scanning.
    
    * Clean some logging.
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    * Adding cert template
    
    * Adding checkCopied flag
    
    * Adding cert template
    
    * Removing old cert
    
    * Fixing -novault for trcx
    
    * Fixing -novault certs
    
    * Fixing novault certs
    
    * Delete serv_cert.pem.mf.tmpl
    
    * Updating go.mod
    
    * Reverting go.mod
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    * Adding logging for interface
    
    * Fixing empty SEC row appearing
    
    * Adding a dbport for use by trcvault.
    
    * Adding support for dynamic interface address
    
    * Fixing harbinger build
    
    * Fixing interface address
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>

--- trcflow/flumen/process.go
+++ trcflow/flumen/process.go
@@ -3,7 +3,9 @@ package flumen
 import (
 	"io"
 	"log"
+	"net/url"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -20,8 +22,7 @@ import (
 	sys "tierceron/vaulthelper/system"
 
 	tcutil "VaultConfig.TenantConfig/util"
-  "VaultConfig.TenantConfig/util/harbinger"
-
+	"VaultConfig.TenantConfig/util/harbinger"
 	sqle "github.com/dolthub/go-mysql-server/sql"
 )
 
@@ -282,13 +283,20 @@ func ProcessFlows(pluginConfig map[string]interface{}, logger *log.Logger) error
 	// Variables such as username, password, port are in vaultDatabaseConfig -- configs coming from encrypted vault.
 	// The engine is in tfmContext...  that's the one we need to make available for connecting via dbvis...
 	// be sure to enable encryption on the connection...
-
+	logger.Println("Starting Interface.")
+	wg.Add(1)
+	interfaceUrl, parseErr := url.Parse(pluginConfig["interfaceaddr"].(string))
+	if parseErr != nil {
+		eUtils.LogErrorMessage(config, "Could parse address for interface. Failing to start interface", false)
+		return parseErr
+	}
+	vaultDatabaseConfig["interfaceaddr"] = strings.Split(interfaceUrl.Host, ":")[0] + ":" + vaultDatabaseConfig["dbport"].(string)
 	harbingerErr := harbinger.BuildInterface(config, goMod, tfmContext, vaultDatabaseConfig)
 	if harbingerErr != nil {
+		wg.Done()
 		eUtils.LogErrorMessage(config, "Failed to start up database interface:"+harbingerErr.Error(), false)
 		return harbingerErr
 	}
-
 	wg.Wait()
 	logger.Println("ProcessFlows complete.")
 
