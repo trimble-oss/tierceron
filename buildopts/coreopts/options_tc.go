cceed1eceae813bb065fbbb135ccdf5976bb4beb
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-06-07 16:11:18 -0700

    Merging Develop to Main (#113)
    * Fixing -novault for trcx
    
    * Update version of go. (#91)
    
    * Merging Harbinger into Develop  (#98)
    
    * Adding new harbinger build for trcdb
    
    * Adding harbinger
    
    * Merging Develop into Harbinger (#85)
    
    * Adding better support for plugin ip connections. (#81)
    
    * Adding better support for plugin ip connections.
    
    * Using insecure for plugins because always local.
    
    * Adding better support for plugin ip connections. (#81) (#82)
    
    * Adding better support for plugin ip connections.
    
    * Using insecure for plugins because always local.
    
    * Fixing crash of plugin due to missing certification.  Also remove port scanning.
    
    * Clean some logging.
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    * Adding cert template
    
    * Adding checkCopied flag
    
    * Adding cert template
    
    * Removing old cert
    
    * Fixing -novault for trcx
    
    * Fixing -novault certs
    
    * Fixing novault certs
    
    * Delete serv_cert.pem.mf.tmpl
    
    * Updating go.mod
    
    * Reverting go.mod
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    * Adding logging for interface
    
    * Fixing empty SEC row appearing
    
    * Adding a dbport for use by trcvault.
    
    * Adding support for dynamic interface address
    
    * Fixing harbinger build
    
    * Fixing interface address
    
    * Removing testflow for harbinger build
    
    * Adding log files
    
    * Adding log files
    
    * Remove testflow from dev build as well.
    
    * Fixing problem with TrcDb not starting correctly.
    
    * Fixing non-harbinger builds
    
    * Doing some cleanup.
    
    * Standardize on vaddress.
    
    * Fix some crashing issues.
    
    * Adding Local TrcDB Build Option (#112)
    
    * Added encoding on vault writeback for mysqlfile
    
    * Added QueryWithBinding method to support more queries
    
    * Fixing writeback to vault
    
    * Improve logging.
    
    * Some cleanup to reduce logging.
    
    * Refactor most custom dependencies into buildopts folder.
    
    * Refactor most custom dependencies into buildopts folder.
    
    * Adding testrunner for command line driven test.
    
    * Adding tc flag for all builds.
    
    * Adding tc flag for all builds... additional files.
    
    * Update versions for all command line tools.
    
    * Partial refactor of configcore.
    
    * Fixing test runner build flag
    
    * Updating trcname build
    
    * Refactor interface layer for sessions.
    
    * Adding support for interface writebacks to vault
    
    * Adding support for interface writebacks to vault
    
    * Updating makefile
    
    * Update to track only inserts and updates.
    
    * Adding cidrblock variable.
    
    * Fixing ineffiencies in trcx -certs
    
    * Adding support for local trcdb
    
    * Adding back interval
    
    * Disable auto reg testing for now.
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>

---
+++ buildopts/coreopts/options_tc.go
@@ -0,0 +1,50 @@
+//go:build tc
+// +build tc
+
+package coreopts
+
+import (
+	configcore "VaultConfig.Bootstrap/configcore"
+	tcbuildopts "VaultConfig.TenantConfig/util/buildopts"
+	tvUtils "VaultConfig.TenantConfig/util/buildtrcprefix"
+	"database/sql"
+)
+
+func GetSyncedTables() []string {
+	return tcbuildopts.GetSyncedTables()
+}
+
+func GetFolderPrefix() string {
+	return tvUtils.GetFolderPrefix()
+}
+
+func DecryptSecretConfig(tenantConfiguration map[string]interface{}, config map[string]interface{}) string {
+	return configcore.DecryptSecretConfig(tenantConfiguration, config)
+}
+
+func GetSupportedTemplates() []string {
+	return configcore.GetSupportedTemplates()
+}
+
+func GetLocalHost() string {
+	return configcore.LocalHost
+}
+
+func GetVaultHost() string {
+	return configcore.VaultHost
+}
+
+func GetVaultHostPort() string {
+	return configcore.VaultHostPort
+}
+
+func ActiveSessions(db *sql.DB) ([]map[string]interface{}, error) {
+	return configcore.ActiveSessions(db)
+}
+func GetUserNameField() string {
+	return configcore.UserNameField
+}
+
+func GetUserCodeField() string {
+	return configcore.UserCodeField
+}
