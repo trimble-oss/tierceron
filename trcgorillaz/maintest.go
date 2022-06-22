31d4b611a8dc2491e0eb7d6a71a28fbd0711c2b4
Meghan Bailey <mbailey@mbailey-us-ll01.am.trimblecorp.net>
2022-06-22 13:49:34 -0700

    initial

---
+++ trcgorillaz/maintest.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	eUtils "tierceron/utils"
+)
+
+// This executable automates the creation of seed files from template file(s).
+// New seed files are written (or overwrite current seed files) to the specified directory.
+func main() {
+	// Supported build flags:
+	//    insecure harbinger tc testrunner ( mysql, testflow -- auto registration -- warning do not use!)
+	logFilePtr := flag.String("log", "./trcgorillaz.log", "Output path for log file")
+	tokenPtr := flag.String("token", "", "Vault access Token")
+	flag.Parse()
+
+	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	logger := log.New(f, "[trcgorillaz]", log.LstdFlags)
+	eUtils.CheckError(&eUtils.DriverConfig{Log: logger, ExitOnFailure: true}, err, true)
+
+	//pluginConfig := buildopts.GetTestConfig(*tokenPtr, false)
+
+	//trcflow.ProcessFlows(pluginConfig, logger)
+}
