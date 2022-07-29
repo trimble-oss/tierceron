9c169696cc5059ae804850b94876ca6c2ea6c86c
Karnveer Gill <karnveer_gill@trimble.com>
2022-07-29 16:23:27 -0700

    Adding check for deploy failures

--- trcvault/deploy/deploy.sh
+++ trcvault/deploy/deploy.sh
@@ -15,6 +15,8 @@ if [ ! -f "$FILESHA" ]; then
     exit 1
 fi
 
+FILESHAVAL=$(cat $FILESHA)
+
 echo "Enter vault host base url: "
 read VAULT_ADDR
 
@@ -99,7 +101,7 @@ if [ "$VAULT_ENV" = "prod" ] || [ "$VAULT_ENV" = "staging" ]; then
     # It should return sha256 of copied plugin on success.
     SHA256BUNDLE=$(vault write vaultcarrier/$VAULT_ENV token=$VAULT_CARRIER_DEPLOY_TOKEN plugin=$TRC_PLUGIN_NAME-prod vaddress=$VAULT_ADDR)
     SHAVAL=$(echo $SHA256BUNDLE | awk '{print $6}')
-    FILESHAVAL=$(cat target/$TRC_PLUGIN_NAME-prod.sha256)
+    
     if [ "$SHAVAL" != "$FILESHAVAL" ]; then
     echo "Carrier failed to deploy plugin.  Please try again."
     exit -1
@@ -143,8 +145,7 @@ else
         fi
     fi
 
-    FILESHAVAL=$(cat target/$TRC_PLUGIN_NAME.sha256)
-    if [ "$SHAVAL" != "$FILESHAVAL" ]; then
+    if [ "$SHAVAL" != "$FILESHAVAL"]; then
     echo "Carrier failed to deploy plugin.  Please try again."
     exit -1
     fi
