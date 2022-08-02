4f3fe6de136bfb7f3e385ff928d5e70df645445b
Karnveer Gill <karnveer_gill@trimble.com>
2022-08-02 10:57:20 -0700

    Adding stub

---
+++ trcx/trcxerutil/util.go
@@ -0,0 +1,6 @@
+package trcxerutil
+
+func PromptChanges(fields map[string]interface{}, encrypted map[string]interface{}) (map[string]interface{}, map[string]interface{}) {
+
+	return fields, encrypted
+}
