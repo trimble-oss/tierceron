3a7c7b30007108174c77a6c5decac0c6e9bcc127
Joel Rieke <joel_rieke@trimble.com>
2022-08-15 16:57:00 -0700

    Putting in more precise locking.

--- utils/mlock/mlock_unix.go
+++ utils/mlock/mlock_unix.go
@@ -5,6 +5,7 @@ package mlock
 
 import (
 	"log"
+	"reflect"
 	"syscall"
 	"unsafe"
 
@@ -21,9 +22,10 @@ var _zero uintptr
 func Mlock2(logger *log.Logger, sensitive *string) error {
 	var _p0 unsafe.Pointer
 	var err error = nil
-	sensitiveLen := len(*sensitive) + int(unsafe.Sizeof(*sensitive))
+	sensitiveHeader := (*reflect.StringHeader)(unsafe.Pointer(sensitive))
+	sensitiveLen := sensitiveHeader.Len
 	if sensitiveLen > 0 {
-		_p0 = unsafe.Pointer(sensitive)
+		_p0 = unsafe.Pointer(sensitiveHeader.Data)
 	} else {
 		_p0 = unsafe.Pointer(&_zero)
 	}
