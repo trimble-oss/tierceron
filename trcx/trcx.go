0c99b7768bdd1f4254ba3dac9d55e1b279ccc7db
Joel Rieke <joel_rieke@trimble.com>
2022-08-15 15:25:13 -0700

    Protect token init

--- trcx/trcx.go
+++ trcx/trcx.go
@@ -4,13 +4,18 @@ import (
 	"flag"
 	"fmt"
 
+	"tierceron/trcvault/opts/memonly"
 	"tierceron/trcx/xutil"
 	trcxbase "tierceron/trcxbase"
+	"tierceron/utils/mlock"
 )
 
 // This executable automates the creation of seed files from template file(s).
 // New seed files are written (or overwrite current seed files) to the specified directory.
 func main() {
+	if memonly.IsMemonly() {
+		mlock.Mlock(nil)
+	}
 	fmt.Println("Version: " + "1.25")
 	envPtr := flag.String("env", "dev", "Environment to get seed data for.")
 
