59a1d6d79e52b752f104890ea654fd5cb22e947e
Meghan Bailey <mbailey@mbailey-us-ll01.am.trimblecorp.net>
2022-08-15 09:18:32 -0700

    Working spiral and subspiral

--- trcgorillaz/trcdatavisualizer/ttdirender/element.go
+++ trcgorillaz/trcdatavisualizer/ttdirender/element.go
@@ -1,7 +1,6 @@
 package ttdirender
 
 import (
-	//"fmt"
 	"log"
 
 	"github.com/g3n/engine/core"
@@ -14,7 +13,6 @@ import (
 	g3ndpalette "github.com/mrjrieke/nute/g3nd/palette"
 	"github.com/mrjrieke/nute/g3nd/worldg3n/g3nrender"
 	"github.com/mrjrieke/nute/mashupsdk"
-	//"github.com/mrjrieke/nute/mashupsdk"
 )
 
 type ClickedG3nDetailElement struct {
@@ -30,7 +28,7 @@ type ElementRenderer struct {
 	//subCounter    float64
 	//locnCounter   *math32.Vector3
 	totalElements int
-	activeSet     map[int64]*math32.Vector3
+	locationCache map[int64]*math32.Vector3
 	//compoundMesh  *CompoundMesh
 
 	clickedElements []*ClickedG3nDetailElement //ClickedG3nDetailElement // Stack containing clicked spiral (sub as well) g3n elements.
@@ -63,8 +61,6 @@ func (er *ElementRenderer) Top() *ClickedG3nDetailElement {
 	return er.clickedElements[er.Length()-1]
 }
 
-// Add into different group based on g3n element
-// Sort group using mashupdetailedelement.Alias
 func (er *ElementRenderer) NewSolidAtPosition(g3n *g3nmash.G3nDetailedElement, vpos *math32.Vector3) core.INode {
 	sphereGeom := geometry.NewSphere(.1, 100, 100)
 	color := g3ndpalette.DARK_BLUE
@@ -72,7 +68,6 @@ func (er *ElementRenderer) NewSolidAtPosition(g3n *g3nmash.G3nDetailedElement, v
 	sphereMesh := graphic.NewMesh(sphereGeom, mat)
 	sphereMesh.SetLoaderID(g3n.GetDisplayName())
 	sphereMesh.SetPositionVec(vpos)
-	//g3n.SetNamedMesh(g3n.GetDisplayName(), sphereMesh)
 	return sphereMesh
 }
 
@@ -80,176 +75,150 @@ func (er *ElementRenderer) NewInternalMeshAtPosition(g3n *g3nmash.G3nDetailedEle
 	return nil
 }
 
-// Add multiple next coordinate methods?? Base it on grouping? How to edit groups --> need it based on argosy struct --> use alias
 func (er *ElementRenderer) NextCoordinate(g3n *g3nmash.G3nDetailedElement, totalElements int) (*g3nmash.G3nDetailedElement, *math32.Vector3) {
-	er.totalElements = totalElements //totalElements parameter increases as it is rendered?
-	if er.iOffset == 0 {
-		er.iOffset = 1
-		er.counter = 0.0
-		er.Push(g3n, math32.NewVector3(float32(0.0), float32(0.0), float32(0.0)))
-		return g3n, math32.NewVector3(float32(0.0), float32(0.0), float32(0.0))
-	} else if er.iOffset == 1 {
-		er.iOffset = 2
-		er.counter = 0.0
-		return g3n, math32.NewVector3(er.Top().center.X, er.Top().center.Y, er.Top().center.Z)
-	} else {
-		er.counter = er.counter - 0.1
-		complex := binetFormula(er.counter)
-		return g3n, math32.NewVector3(float32(-real(complex))+er.Top().center.X, float32(imag(complex))+er.Top().center.Y, float32(-er.counter)+er.Top().center.Z)
+	er.totalElements = totalElements // Not sure if this is necessary
+	if er.locationCache != nil {
+		id := g3n.GetDisplayId()
+		return g3n, er.locationCache[id]
+	} else { // This is because locn cache is not set until clicked element :(
+		if er.iOffset == 0 {
+			er.iOffset = 1
+			er.counter = 0.0
+			return g3n, math32.NewVector3(float32(0.0), float32(0.0), float32(0.0))
+		} else {
+			er.counter = er.counter - 0.1
+			complex := binetFormula(er.counter)
+			return g3n, math32.NewVector3(float32(-real(complex)), float32(imag(complex)), float32(-er.counter))
+		}
 	}
 }
 
 func (er *ElementRenderer) Layout(worldApp *g3nworld.WorldApp,
 	g3nRenderableElements []*g3nmash.G3nDetailedElement) {
-	er.LayoutBase(worldApp, er, g3nRenderableElements) //Doesn't accept new mesh type in call to layoutbase
+	er.LayoutBase(worldApp, er, g3nRenderableElements)
 }
 
 func (er *ElementRenderer) InitRenderLoop(worldApp *g3nworld.WorldApp) bool {
 	// TODO: noop
+	//Initialize location cache
+	if er.locationCache == nil {
+		er.locationCache = make(map[int64]*math32.Vector3)
+		for _, mainElement := range worldApp.ConcreteElements {
+			if mainElement.GetNamedMesh(mainElement.GetDisplayName()) != nil && mainElement.GetDetailedElement().Renderer == "Element" {
+				location := mainElement.GetNamedMesh(mainElement.GetDisplayName()).Position()
+				id := mainElement.GetDetailedElement().GetId()
+				er.locationCache[id] = &location
+				counter := float32(0.0)
+				for _, childID := range mainElement.GetChildElementIds() {
+					er.locationCache[childID] = er.calculateLocation(&location, counter)
+					counter = counter - 0.1
+				}
+			}
+		}
+	}
+
+	// Hide the elements that should be hidden when a different element is clicked
+	if !er.IsEmpty() {
+		prevElement := er.Pop()
+		if !er.isChildElement(worldApp, prevElement.clickedElement) && prevElement != nil && prevElement.clickedElement.GetDetailedElement().Genre != "Solid" {
+			//Case 1: Main spiral element: have to remove children nodes
+			for _, childID := range prevElement.clickedElement.GetChildElementIds() {
+				if !er.isChildElement(worldApp, prevElement.clickedElement) {
+					worldApp.ConcreteElements[childID].ApplyState(mashupsdk.Hidden, true)
+					er.RemoveAll(worldApp, childID)
+				}
+			}
+			//Case 2: Sub Spiral element and main spiral clicked: have to remove children nodes of parent node
+			for _, parentID := range prevElement.clickedElement.GetParentElementIds() {
+				parentElement := worldApp.ConcreteElements[parentID]
+				for _, childID := range parentElement.GetChildElementIds() {
+					if !er.isChildElement(worldApp, parentElement) {
+						worldApp.ConcreteElements[childID].ApplyState(mashupsdk.Hidden, true)
+						er.RemoveAll(worldApp, childID)
+					}
+				}
+			}
+		}
+	}
+
+	// Handle state change here and then in render element just render all the elements that are not yet showing but are hidden
+	clickedElement := worldApp.ClickedElements[len(worldApp.ClickedElements)-1]
+	if clickedElement.GetDetailedElement().Genre != "Solid" && clickedElement.GetDetailedElement().Genre != "Space" {
+		center := clickedElement.GetNamedMesh(clickedElement.GetDisplayName()).Position()
+		er.Push(clickedElement, &center)
+		for _, childID := range clickedElement.GetChildElementIds() {
+			childElement := worldApp.ConcreteElements[childID]
+			if childElement.GetDetailedElement().Genre != "Solid" {
+				childElement.ApplyState(mashupsdk.Hidden, false)
+				childElement.ApplyState(mashupsdk.Clicked, true)
+			}
+		}
+	}
+
 	return true
 }
 
+func (er *ElementRenderer) isChildElement(worldApp *g3nworld.WorldApp, prevElement *g3nmash.G3nDetailedElement) bool {
+	clickedElement := worldApp.ClickedElements[len(worldApp.ClickedElements)-1]
+	for _, childID := range prevElement.GetChildElementIds() {
+		if clickedElement == worldApp.ConcreteElements[childID] {
+			return true
+		}
+	}
+	return false
+}
+
+func (er *ElementRenderer) calculateLocation(center *math32.Vector3, counter float32) *math32.Vector3 {
+	complex := binetFormula(float64(counter))
+	return math32.NewVector3(float32(-real(complex))+center.X, float32(imag(complex))+center.Y, float32(-counter)+center.Z)
+}
+
+// Change color and fade other elements?
 func (er *ElementRenderer) RenderElement(worldApp *g3nworld.WorldApp, g3n *g3nmash.G3nDetailedElement) bool {
-	//Look at clicked elements and apply state changes to child Ids
-	//Keep this method ignorant (only changes look and feel of objects and state)
 	if g3n == worldApp.ClickedElements[len(worldApp.ClickedElements)-1] {
 		if g3n.GetNamedMesh(g3n.GetDisplayName()) != nil {
-			location := g3n.GetNamedMesh(g3n.GetDisplayName()).Position()
-			er.Push(g3n, &location)
-			er.iOffset = 1
-		}
-		for _, childId := range g3n.GetChildElementIds() {
-			//for _, child := range worldApp.ConcreteElements[childId].GetChildElementIds() {
-			element := worldApp.ConcreteElements[childId]
-			element.ApplyState(mashupsdk.Hidden, false)
-			element.ApplyState(mashupsdk.Clicked, true)
-			_, nextPos := er.NextCoordinate(element, er.totalElements)
-			solidMesh := er.NewSolidAtPosition(g3n, nextPos)
-			if solidMesh != nil {
-				log.Printf("Adding %s\n", solidMesh.GetNode().LoaderID())
-				//if !g3nRenderableElement.IsStateSet(mashupsdk.Hidden) {\
-				//graphic.Mesh(solidMesh).SetPositionVec(math32.NewVector3(float32(1), 1.0, 1.0))
-				//solidMesh.Position().X = float32(1.0)
-
-				//math32.NewVector3(float32(1), 1.0, 1.0))
-				solidMesh.GetNode().SetPositionVec(math32.NewVector3(1.0, 1.0, 1.0))
-				worldApp.AddToScene(solidMesh)
-				//}
-				g3n.SetNamedMesh(g3n.GetDisplayName(), solidMesh)
+			g3n.SetColor(math32.NewColor("darkred"), 1.0)
+			for _, childId := range g3n.GetChildElementIds() {
+				element := worldApp.ConcreteElements[childId]
+				if element.GetDetailedElement().Genre != "Solid" {
+					if element.GetNamedMesh(element.GetDisplayName()) == nil {
+						_, nextPos := er.NextCoordinate(element, er.totalElements)
+						solidMesh := er.NewSolidAtPosition(element, nextPos)
+						if solidMesh != nil {
+							log.Printf("Adding %s\n", solidMesh.GetNode().LoaderID())
+							worldApp.UpsertToScene(solidMesh)
+							element.SetNamedMesh(element.GetDisplayName(), solidMesh)
+						}
+					} else {
+						worldApp.UpsertToScene(element.GetNamedMesh(element.GetDisplayName()))
+					}
+				}
 			}
-			//worldApp.AddToScene(element.GetNamedMesh(element.GetDisplayName()))
-			//}
 		}
+
 		return true
+	} else {
+		color := g3ndpalette.DARK_BLUE
+		g3n.SetColor(color.Set(0, 0.349, 0.643), 1.0)
 	}
 	return true
-	// if g3nDetailedElement.GetDetailedElement().Alias == "Argosy" {
-	// 	return er.HandleArgosyStateChange(worldApp, g3nDetailedElement)
-	// } else if g3nDetailedElement.GetDetailedElement().Alias == "DataFlowGroup" {
-	// 	return er.HandleDFGStateChange(worldApp, g3nDetailedElement)
-	// } else {
-	// 	return true
-	// }
 }
 
-func (er *ElementRenderer) HelpStateChange() {}
-
-func (er *ElementRenderer) HandleArgosyStateChange(worldApp *g3nworld.WorldApp, g3nDetailedElement *g3nmash.G3nDetailedElement) bool {
-	var g3nColor *math32.Color
-
-	if g3nDetailedElement.IsItemActive() {
-		//er.Push(g3nDetailedElement, g3nDetailedElement.Position())
-		// for _, childId := range g3nDetailedElement.GetChildElementIds() {
-		// 	e := worldApp.ConcreteElements[int64(childId)]
-		// 	for _, childId := range e.GetChildElementIds() {
-		// 		for _, subChildID := range worldApp.ConcreteElements[childId].GetChildElementIds() {
-		// 			element := worldApp.ConcreteElements[subChildID]
-		// 			name := element.GetDisplayName()
-		// 			compoundMesh := element.GetNamedMesh(name)
-		// 			if compoundMesh != nil {
-		// 				element.ApplyState(mashupsdk.Init, false)
-		// 				element.ApplyState(mashupsdk.Hidden, false)
-		// 				element.ApplyState(mashupsdk.Clicked, true)
-		// 			}
-		// 		}
-		// 	}
-		// }
-		g3nColor = math32.NewColor("darkred")
-		mesh := g3nDetailedElement.GetNamedMesh(g3nDetailedElement.GetDisplayName())
-		if er.activeSet == nil {
-			er.activeSet = map[int64]*math32.Vector3{}
+func (er *ElementRenderer) RemoveAll(worldApp *g3nworld.WorldApp, childId int64) {
+	if child, childOk := worldApp.ConcreteElements[childId]; childOk {
+		if !child.IsAbstract() && child.GetDetailedElement().Genre != "Solid" {
+			if childMesh := child.GetNamedMesh(child.GetDisplayName()); childMesh != nil {
+				log.Printf("Child Item removed %s: %v", child.GetDisplayName(), worldApp.RemoveFromScene(childMesh))
+			}
 		}
-		activePosition := mesh.(*graphic.Mesh).GetGraphic().Position()
-		er.activeSet[g3nDetailedElement.GetDetailedElement().GetId()] = &activePosition
-		return g3nDetailedElement.SetColor(g3nColor, 1.0)
-		//fmt.Printf("Active element centered at %v\n", activePosition)
-	} else {
-		if g3nDetailedElement.IsBackgroundElement() {
-			// Axial circle
-			g3nColor = g3ndpalette.GREY
-		} else {
-			if !worldApp.Sticky {
-				g3nColor = g3ndpalette.DARK_BLUE
-			} else {
-				g3nColor = g3nDetailedElement.GetColor()
-				if g3nColor == nil {
-					g3nColor = g3ndpalette.DARK_BLUE
-				}
+
+		if len(child.GetChildElementIds()) > 0 {
+			for _, cId := range child.GetChildElementIds() {
+				er.RemoveAll(worldApp, cId)
 			}
 		}
 	}
 
-	return g3nDetailedElement.SetColor(g3nColor, 1.0)
-	//return true
-}
-
-func (er *ElementRenderer) HandleDFGStateChange(worldApp *g3nworld.WorldApp, g3nDetailedElement *g3nmash.G3nDetailedElement) bool {
-	var g3nColor *math32.Color
-
-	if g3nDetailedElement.IsItemActive() {
-		//er.Push(g3nDetailedElement)
-		// for _, childId := range g3nDetailedElement.GetChildElementIds() {
-		// 	e := worldApp.ConcreteElements[int64(childId)]
-		// 	for _, childId := range e.GetChildElementIds() {
-		// 		for _, subChildID := range worldApp.ConcreteElements[childId].GetChildElementIds() {
-		// 			element := worldApp.ConcreteElements[subChildID]
-		// 			name := element.GetDisplayName()
-		// 			compoundMesh := element.GetNamedMesh(name)
-		// 			if compoundMesh != nil {
-		// 				element.ApplyState(mashupsdk.Init, false)
-		// 				element.ApplyState(mashupsdk.Hidden, false)
-		// 				element.ApplyState(mashupsdk.Clicked, true)
-		// 			}
-		// 		}
-		// 	}
-		// }
-		g3nColor = math32.NewColor("darkred")
-		mesh := g3nDetailedElement.GetNamedMesh(g3nDetailedElement.GetDisplayName())
-		if er.activeSet == nil {
-			er.activeSet = map[int64]*math32.Vector3{}
-		}
-		activePosition := mesh.(*graphic.Mesh).GetGraphic().Position()
-		er.activeSet[g3nDetailedElement.GetDetailedElement().GetId()] = &activePosition
-		return g3nDetailedElement.SetColor(g3nColor, 1.0)
-		//fmt.Printf("Active element centered at %v\n", activePosition)
-	} else {
-		if g3nDetailedElement.IsBackgroundElement() {
-			// Axial circle
-			g3nColor = g3ndpalette.GREY
-		} else {
-			if !worldApp.Sticky {
-				g3nColor = g3ndpalette.DARK_BLUE
-			} else {
-				g3nColor = g3nDetailedElement.GetColor()
-				if g3nColor == nil {
-					g3nColor = g3ndpalette.DARK_BLUE
-				}
-			}
-		}
-	}
-
-	return g3nDetailedElement.SetColor(g3nColor, 1.0)
-	//return true
 }
 
 func (er *ElementRenderer) LayoutBase(worldApp *g3nworld.WorldApp,
@@ -278,16 +247,14 @@ func (er *ElementRenderer) LayoutBase(worldApp *g3nworld.WorldApp,
 			solidMesh := g3Renderer.NewSolidAtPosition(concreteG3nRenderableElement, nextPos)
 			if solidMesh != nil {
 				log.Printf("Adding %s\n", solidMesh.GetNode().LoaderID())
-				//if !g3nRenderableElement.IsStateSet(mashupsdk.Hidden) {
-				worldApp.AddToScene(solidMesh)
-				//}
+				worldApp.UpsertToScene(solidMesh)
 				concreteG3nRenderableElement.SetNamedMesh(concreteG3nRenderableElement.GetDisplayName(), solidMesh)
 			}
 
 			for _, relatedG3n := range worldApp.GetG3nDetailedChildElementsByGenre(concreteG3nRenderableElement, "Related") {
 				relatedMesh := g3Renderer.NewRelatedMeshAtPosition(concreteG3nRenderableElement, nextPos, prevSolidPos)
 				if relatedMesh != nil {
-					worldApp.AddToScene(relatedMesh)
+					worldApp.UpsertToScene(relatedMesh)
 					concreteG3nRenderableElement.SetNamedMesh(relatedG3n.GetDisplayName(), relatedMesh)
 				}
 			}
@@ -295,7 +262,7 @@ func (er *ElementRenderer) LayoutBase(worldApp *g3nworld.WorldApp,
 			for _, innerG3n := range worldApp.GetG3nDetailedChildElementsByGenre(concreteG3nRenderableElement, "Space") {
 				negativeMesh := g3Renderer.NewInternalMeshAtPosition(innerG3n, nextPos)
 				if negativeMesh != nil {
-					worldApp.AddToScene(negativeMesh)
+					worldApp.UpsertToScene(negativeMesh)
 					innerG3n.SetNamedMesh(innerG3n.GetDisplayName(), negativeMesh)
 				}
 			}
