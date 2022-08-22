fe7500d6d050bd1d5fdaabf055f5609656d3e755
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-08-22 14:09:00 -0700

    Tierceron Flow Controller (#205)
    * Initializing skeleton for controller flow
    
    * Adding tierceron controller
    
    * Adding IndexedPathExt
    
    * Adding new process cycle for flow init
    
    * Adding import channel to alert when table is done initting
    
    * Adding import channel to alert when table is done initting
    
    * New template
    
    * Removing template
    
    * Adding support for controller database
    
    * Fixing init order for controller database
    
    * Merging from develop
    
    * Adding final writeback upon sync cycle cancelation
    
    * Adding state alerts & ability to stop/start
    
    * Adding support for ability to stop/start flows
    
    * Adding state update support
    
    * Adding registration flow to controller
    
    * Cleaning up interval & message
    
    * Fixing spelling mistake
    
    * Congregated controller db name
    
    * Renmaing template
    
    * Renaming template
    
    * Adding nil checks
    
    * Adding controller DB
    
    * Adding controller interface configs
    
    * Adding controller config fields
    
    * Adding nil & ok checks
    
    * Adding cast checks
    
    * Fixing field names & adding support for it
    
    * Fixing field names & adding support for it
    
    * Adding nil check
    
    * Fixing possible crash

--- trcflow/core/context.go
+++ trcflow/core/context.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"context"
 	"database/sql"
 	"io"
 	"os"
@@ -242,7 +243,8 @@ func (tfmContext *TrcFlowMachineContext) seedVaultCycle(tfContext *TrcFlowContex
 	vaultIndexColumnName string,
 	vaultSecondIndexColumnName string,
 	getIndexedPathExt func(engine interface{}, rowDataMap map[string]interface{}, vaultIndexColumnName string, databaseName string, tableName string, dbCallBack func(interface{}, string) (string, []string, [][]interface{}, error)) (string, error),
-	flowPushRemote func(map[string]interface{}, map[string]interface{}) error) {
+	flowPushRemote func(map[string]interface{}, map[string]interface{}) error,
+	ctx context.Context) {
 
 	mysqlPushEnabled := mysql.IsMysqlPushEnabled()
 	flowChangedChannel := channelMap[tfContext.Flow]
@@ -261,6 +263,16 @@ func (tfmContext *TrcFlowMachineContext) seedVaultCycle(tfContext *TrcFlowContex
 				mysqlPushEnabled,
 				getIndexedPathExt,
 				flowPushRemote)
+		case <-ctx.Done():
+			tfmContext.vaultPersistPushRemoteChanges(
+				tfContext,
+				identityColumnName,
+				vaultIndexColumnName,
+				vaultSecondIndexColumnName,
+				mysqlPushEnabled,
+				getIndexedPathExt,
+				flowPushRemote)
+			return
 		}
 	}
 }
@@ -302,6 +314,9 @@ func (tfmContext *TrcFlowMachineContext) seedTrcDbCycle(tfContext *TrcFlowContex
 			triggerLock.Unlock()
 		}
 		seedInitCompleteChan <- true
+		if importChan, ok := tfContext.RemoteDataSource["vaultImportChannel"].(chan bool); ok {
+			importChan <- true
+		}
 	}
 
 	// Check vault hourly for changes to sync with mysql
@@ -345,7 +360,8 @@ func (tfmContext *TrcFlowMachineContext) SyncTableCycle(tfContext *TrcFlowContex
 	vaultIndexColumnName string,
 	vaultSecondIndexColumnName string,
 	getIndexedPathExt func(engine interface{}, rowDataMap map[string]interface{}, vaultIndexColumnName string, databaseName string, tableName string, dbCallBack func(interface{}, string) (string, []string, [][]interface{}, error)) (string, error),
-	flowPushRemote func(map[string]interface{}, map[string]interface{}) error) {
+	flowPushRemote func(map[string]interface{}, map[string]interface{}) error,
+	ctx context.Context) {
 
 	var seedInitComplete chan bool = make(chan bool, 1)
 	if vaultSecondIndexColumnName == "" {
@@ -354,7 +370,7 @@ func (tfmContext *TrcFlowMachineContext) SyncTableCycle(tfContext *TrcFlowContex
 		seedInitComplete <- true
 	}
 	<-seedInitComplete
-	go tfmContext.seedVaultCycle(tfContext, identityColumnName, vaultIndexColumnName, vaultSecondIndexColumnName, getIndexedPathExt, flowPushRemote)
+	go tfmContext.seedVaultCycle(tfContext, identityColumnName, vaultIndexColumnName, vaultSecondIndexColumnName, getIndexedPathExt, flowPushRemote, ctx)
 }
 
 func (tfmContext *TrcFlowMachineContext) SelectFlowChannel(tfContext *TrcFlowContext) <-chan bool {
