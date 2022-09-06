7fbf8a06ee64e02930332f30b3e3ae6fc860b571
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-09-06 16:59:05 -0700

    Merging Main into Legacy (#249)
    * Adding SQL flow control to TierceronFlow (#233)
    
    * Adding support for syncMode & improved state updates
    
    * Adding support for syncMode & improved state updates
    
    * Removing usage of SQL flags
    
    * Adding alert channel for updates to table
    
    * Some clean up
    
    * Adding syncFilter
    
    * Adding syncFilter
    
    * Adding syncFilter column
    
    * Adding params to concurrent processes
    
    * Adding updates for lastModified
    
    * Adding defaults for table & adding N/A syncFilters
    
    * Fixing data races
    
    * Fixing lock locations
    
    * Attempting to fix data races
    
    * Attempting to fix data races
    
    * Adding type check
    
    * Fixing reversed locks
    
    * Getting back to working state
    
    * Getting back to working state
    
    * Getting back to working state
    
    * Getting back to working state
    
    * Enable memlock.
    
    * Sql Flow Data Races (#234)
    
    * Fixing more races
    
    * Fixing more races
    
    * Change startup to make controller more quickly available.
    
    * Refactor to allow marking table initted even if flow is off.
    
    * Fixing double done.
    
    * Delay db startup until tables are loaded.
    
    * Renaming WG
    
    * Adding locks & support for init WG
    
    * Fixing more data races (#235)
    
    * Handle init wg for non running state.
    
    * Modifiying change channels to support multiple tfmContexts
    
    * Modifiying change channels to support multiple tfmContexts (#238)
    
    * Modifiying change channels to support multiple tfmContexts (#238) (#239)
    
    * Adding decode/encode and cast fix.
    
    * Actually, here it needs to remain a string to go into vault correctly.
    
    * Merging Develop into Main (#245)
    
    * Add custom Tierceron encoding taging mechnanism.
    
    * Push/Pull Once (#244)
    
    * Adding syncMode to stateUpdates
    
    * Changing numerical SyncMode to string
    
    * Adding comment for FlowLock
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    * Merging Develop into Main (#245) (#246)
    
    * Add custom Tierceron encoding taging mechnanism.
    
    * Push/Pull Once (#244)
    
    * Adding syncMode to stateUpdates
    
    * Changing numerical SyncMode to string
    
    * Adding comment for FlowLock
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    * Merging Develop into Main (#247)
    
    * Add custom Tierceron encoding taging mechnanism.
    
    * Push/Pull Once (#244)
    
    * Adding syncMode to stateUpdates
    
    * Changing numerical SyncMode to string
    
    * Adding comment for FlowLock
    
    * Prevent going into backup folders.
    
    * Deal with previously unencoded data.
    
    * Fixing indexed init path
    
    * Adding check to avoid duplicate subsection path
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>

--- trcflow/core/flowcorehelper/flow.go
+++ trcflow/core/flowcorehelper/flow.go
@@ -8,17 +8,20 @@ const TierceronFlowDB = "FlumeDatabase"
 type FlowStateUpdate struct {
 	FlowName    string
 	StateUpdate string
+	SyncFilter  string
+	SyncMode    string
 }
 
 type CurrentFlowState struct {
-	State    int64
-	SyncMode string
+	State      int64
+	SyncMode   string
+	SyncFilter string
 }
 
 func GetFlowDBName() string {
 	return TierceronFlowDB
 }
 
-func UpdateTierceronFlowState(flowName string, newState string) string {
-	return "update " + TierceronFlowDB + "." + TierceronFlowConfigurationTableName + " set state=" + newState + " where flowName='" + flowName + "'"
+func UpdateTierceronFlowState(flowName string, newState string, syncFilter string, syncMode string) string {
+	return "update " + TierceronFlowDB + "." + TierceronFlowConfigurationTableName + " set lastModified=current_timestamp(), syncMode='" + syncMode + "', state=" + newState + ", SyncFilter='" + syncFilter + "' where flowName='" + flowName + "'"
 }
