d1481cb4a166589e122b71ea058121e8b80f8273
Joel Rieke <joel_rieke@trimble.com>
2022-09-21 16:55:22 -0700

    Adding logging to listener.

--- trcflow/flumen/trcdb_interface_listener.go
+++ trcflow/flumen/trcdb_interface_listener.go
@@ -1,6 +1,7 @@
 package flumen
 
 import (
+	"log"
 	"strings"
 	"sync"
 	flowcore "tierceron/trcflow/core"
@@ -9,7 +10,9 @@ import (
 
 var changeLock sync.Mutex
 
-type TrcDBServerEventListener struct{}
+type TrcDBServerEventListener struct {
+	Log *log.Logger
+}
 
 func (t *TrcDBServerEventListener) ClientConnected() {}
 
@@ -18,6 +21,7 @@ func (tl *TrcDBServerEventListener) ClientDisconnected() {}
 func (tl *TrcDBServerEventListener) QueryStarted() {}
 
 func (tl *TrcDBServerEventListener) QueryCompleted(query string, success bool, duration time.Duration) {
+	tl.Log.Printf("Query completed: %s %v\n", query, success)
 	if success && (strings.HasPrefix(strings.ToLower(query), "insert") || strings.HasPrefix(strings.ToLower(query), "update")) {
 		changeLock.Lock()
 		flowcore.TriggerAllChangeChannel()
