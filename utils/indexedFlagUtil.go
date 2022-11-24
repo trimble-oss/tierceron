618adca0376e88ef20deabed494a979309912974
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2022-11-23 16:30:04 -0800

    Merging Main to Plugin (#394)
    * Merging Develop into Main (#368) (#370)
    
    * Fixing changes being blocked by full channel
    
    * Add support for direct seed initialization.
    
    * Fixing regresion in trcx
    
    * Fixing regression with subIndexes
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    
    * Merging Develop into Main (#387)
    
    * Fixing trcx tool
    
    * Adding release of gomod.
    
    * Merge
    
    * Implement modifier cache so trcx and config don't hammer vault.
    
    * Use raw env.
    
    * Improve logging.
    
    * Get rid of troubleshooting help.
    
    * Fix dump for everything but dfs and role.
    
    * Speed up inserts a tiny bit (#385)
    
    * Revert accidental change.
    
    * Flip logic to positive logic.
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    * Merging Develop into Main (#387) (#389)
    
    * Fixing trcx tool
    
    * Adding release of gomod.
    
    * Merge
    
    * Implement modifier cache so trcx and config don't hammer vault.
    
    * Use raw env.
    
    * Improve logging.
    
    * Get rid of troubleshooting help.
    
    * Fix dump for everything but dfs and role.
    
    * Speed up inserts a tiny bit (#385)
    
    * Revert accidental change.
    
    * Flip logic to positive logic.
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    * Merging Develop into Main (#390)
    
    * Fixing trcx tool
    
    * Adding release of gomod.
    
    * Merge
    
    * Implement modifier cache so trcx and config don't hammer vault.
    
    * Use raw env.
    
    * Improve logging.
    
    * Get rid of troubleshooting help.
    
    * Fix dump for everything but dfs and role.
    
    * Speed up inserts a tiny bit (#385)
    
    * Revert accidental change.
    
    * Flip logic to positive logic.
    
    * Adding continue to row loading errors
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    * Merging Develop into Main (#393)
    
    * Fixing trcx tool
    
    * Adding release of gomod.
    
    * Merge
    
    * Implement modifier cache so trcx and config don't hammer vault.
    
    * Use raw env.
    
    * Improve logging.
    
    * Get rid of troubleshooting help.
    
    * Fix dump for everything but dfs and role.
    
    * Speed up inserts a tiny bit (#385)
    
    * Revert accidental change.
    
    * Flip logic to positive logic.
    
    * Adding continue to row loading errors
    
    * Renaming some variables for clarity.
    
    * Reorder struct.
    
    * Adding ability for controller to invalidate certain states
    
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    
    Co-authored-by: joel-rieke <53945260+joel-rieke@users.noreply.github.com>
    Co-authored-by: Joel Rieke <joel_rieke@trimble.com>

--- utils/indexedFlagUtil.go
+++ utils/indexedFlagUtil.go
@@ -7,11 +7,11 @@ import (
 	"strings"
 )
 
-var IndexServiceExtFilterPtr = flag.String("serviceExtFilter", "", "Specifies which nested services (or tables) to filter") //offset or database
-var IndexServiceFilterPtr = flag.String("serviceFilter", "", "Specifies which services (or tables) to filter")              // Table names
-var IndexNameFilterPtr = flag.String("indexFilter", "", "Specifies which index names to filter")                            // column index, table to filter.
-var IndexValueFilterPtr = flag.String("indexValueFilter", "", "Specifies which index values to filter")                     // column index value to filter on.
-var IndexedPtr = flag.String("indexed", "", "Specifies which projects are indexed")                                         // Indicates indexed projects...
+var ServiceNameFilterPtr = flag.String("serviceExtFilter", "", "Specifies which nested services (or tables) to filter") //offset or database
+var ServiceFilterPtr = flag.String("serviceFilter", "", "Specifies which services (or tables) to filter")               // Table names
+var IndexNameFilterPtr = flag.String("indexFilter", "", "Specifies which index names to filter")                        // column index, table to filter.
+var IndexValueFilterPtr = flag.String("indexValueFilter", "", "Specifies which index values to filter")                 // column index value to filter on.
+var IndexedPtr = flag.String("indexed", "", "Specifies which projects are indexed")                                     // Indicates indexed projects...
 var RestrictedPtr = flag.String("restricted", "", "Specifies which projects have restricted access.")
 var ProtectedPtr = flag.String("protected", "", "Specifies which projects have protected access.")
 var BasePtr = flag.Bool("base", false, "Specifies whether the base env seed file will be seeded")
@@ -36,7 +36,7 @@ func checkInitFlagHelper() {
 func CheckInitFlags() {
 	filtered := false
 	//Cannot specify a pathed indexed/restricted seed file while specifying a restricted/indexed section.
-	if len(*IndexNameFilterPtr) > 0 || len(*IndexServiceFilterPtr) > 0 || len(*IndexValueFilterPtr) > 0 || len(*IndexServiceExtFilterPtr) > 0 {
+	if len(*IndexNameFilterPtr) > 0 || len(*ServiceNameFilterPtr) > 0 || len(*IndexValueFilterPtr) > 0 || len(*ServiceNameFilterPtr) > 0 {
 		filtered = true
 	}
 	if len(*RestrictedPtr) > 0 && len(*IndexedPtr) > 0 && filtered {
@@ -61,7 +61,7 @@ func CheckInitFlags() {
 	}
 
 	if len(*RestrictedPtr) > 0 || len(*IndexedPtr) > 0 {
-		if len(*IndexServiceFilterPtr) > 0 {
+		if len(*ServiceNameFilterPtr) > 0 {
 			checkInitFlagHelper()
 		} else if len(*IndexValueFilterPtr) > 0 {
 			checkInitFlagHelper()
@@ -76,8 +76,8 @@ func CheckInitFlags() {
 	}
 
 	//These two filters are used differently between x and init so this is modifying incoming params to what is expected inside shared helpers.
-	if len(*IndexValueFilterPtr) > 0 && len(*IndexServiceFilterPtr) == 0 && len(*IndexServiceExtFilterPtr) == 0 {
-		*IndexServiceFilterPtr = *IndexValueFilterPtr
+	if len(*IndexValueFilterPtr) > 0 && len(*ServiceNameFilterPtr) == 0 && len(*ServiceNameFilterPtr) == 0 {
+		*ServiceNameFilterPtr = *IndexValueFilterPtr
 		*IndexValueFilterPtr = ""
 	}
 
