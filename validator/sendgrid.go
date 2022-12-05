e00ca6dcd2922db23a1e4d509a9f113deb6b143f
Joel Rieke <joel_rieke@trimble.com>
2022-12-05 07:38:51 -0800

    Setting timeout for sendgrid calls during validation.

--- validator/sendgrid.go
+++ validator/sendgrid.go
@@ -1,19 +1,23 @@
 package validator
 
 import (
+	"context"
 	"errors"
 	"strconv"
+	"time"
 
 	sendgrid "github.com/sendgrid/sendgrid-go"
 )
 
-//ValidateSendGrid validates the sendgrid API key.
+// ValidateSendGrid validates the sendgrid API key.
 func ValidateSendGrid(apiKey string) (bool, error) {
 
 	host := "https://api.sendgrid.com"
 	request := sendgrid.GetRequest(apiKey, "/v3/scopes", host)
 	request.Method = "GET"
-	response, err := sendgrid.API(request)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	response, err := sendgrid.MakeRequestWithContext(ctx, request)
 
 	if err != nil {
 		return false, err
