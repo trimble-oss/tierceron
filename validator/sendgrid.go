cbaa36db987422ebb8014242e01c3cbdd9710442
Joel Rieke <joel_rieke@trimble.com>
2022-12-05 08:49:01 -0800

    Adding a dial timeout instead

--- validator/sendgrid.go
+++ validator/sendgrid.go
@@ -1,11 +1,13 @@
 package validator
 
 import (
-	"context"
 	"errors"
+	"net"
+	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/sendgrid/rest"
 	sendgrid "github.com/sendgrid/sendgrid-go"
 )
 
@@ -15,9 +17,24 @@ func ValidateSendGrid(apiKey string) (bool, error) {
 	host := "https://api.sendgrid.com"
 	request := sendgrid.GetRequest(apiKey, "/v3/scopes", host)
 	request.Method = "GET"
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	response, err := sendgrid.MakeRequestWithContext(ctx, request)
+
+	sendgrid.DefaultClient = &rest.Client{
+		HTTPClient: &http.Client{
+			Transport: &http.Transport{
+				DialContext: (&net.Dialer{
+					Timeout:   4 * time.Second, // Dial timeout
+					KeepAlive: 30 * time.Second,
+					DualStack: true,
+				}).DialContext,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+				DisableKeepAlives:     false,
+				MaxConnsPerHost:       10,
+			},
+		},
+	}
+	response, err := sendgrid.MakeRequest(request)
 
 	if err != nil {
 		return false, err
