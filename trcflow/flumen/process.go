d5782b794c3cb18c611fd2d7f6d7c3265dd4b192
Joel Rieke <joel_rieke@trimble.com>
2022-12-06 03:31:28 -0800

    Adding some nil checks.

--- trcflow/flumen/process.go
+++ trcflow/flumen/process.go
@@ -214,7 +214,9 @@ func ProcessFlows(pluginConfig map[string]interface{}, logger *log.Logger) error
 	// 2. Get json by Api call.
 	extensionAuthComponents := buildopts.GetExtensionAuthComponents(trcIdentityConfig)
 	httpClient, err := helperkv.CreateHTTPClient(false, extensionAuthComponents["authDomain"].(string), pluginConfig["env"].(string), false)
-	defer httpClient.CloseIdleConnections()
+	if httpClient != nil {
+		defer httpClient.CloseIdleConnections()
+	}
 	if err != nil {
 		eUtils.LogErrorObject(config, err, false)
 		return err
