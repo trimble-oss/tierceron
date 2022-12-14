12c9111c2d9d8eab2e06321e2e4600f8a33ea89e
Joel Rieke <joel_rieke@trimble.com>
2022-12-14 12:57:42 -0800

    Adding tierceron ctl.

---
+++ trcctl/init.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	trcinitbase "tierceron/trcinitbase"
+	"tierceron/trcvault/opts/memonly"
+	"tierceron/trcx/xutil"
+	"tierceron/trcxbase"
+	"tierceron/utils/mlock"
+)
+
+// This is a controller program that can act as any command line utility.
+// The swiss army knife of tierceron if you will.
+func main() {
+	if memonly.IsMemonly() {
+		mlock.Mlock(nil)
+	}
+	fmt.Println("Version: " + "1.34")
+	envPtr := flag.String("env", "dev", "Environment to be seeded")
+	envCtxPtr := flag.String("context", "dev", "Context to define.")
+
+	if ctl := os.Args[1]; ctl != "" {
+		switch ctl {
+		case "pub":
+
+		case "init":
+			trcinitbase.CommonMain(envPtr, nil)
+		case "config":
+
+		case "x":
+			trcxbase.CommonMain(nil, xutil.GenerateSeedsFromVault, envPtr, nil, nil)
+		}
+	}
+
+}
