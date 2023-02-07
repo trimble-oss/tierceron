b858bc8a12b72dd7dbfe21f6b93bf443d7461460
Joel Rieke <joel_rieke@trimble.com>
2023-02-06 16:39:21 -0800

    Very beginnings of cap auth.

--- trcvault/carrierfactory/capauth/capauth.go
+++ trcvault/carrierfactory/capauth/capauth.go
@@ -2,10 +2,20 @@ package capauth
 
 import (
 	"github.com/trimble-oss/tierceron-hat/cap"
+	"github.com/trimble-oss/tierceron/vaulthelper/kv"
 )
 
-func Init(mod Mod) {
+func Init(mod *kv.Modifier) error {
 
-	cap.Tap("ExePath", "Exesha256")
+	certifyMap, err := mod.ReadData("super-secrets/Index/TrcVault/trcplugin/trcagentctl/Certify")
+	if err != nil {
+		return err
+	}
+	// TODO: Get patch to agent...
+
+	if _, ok := certifyMap["trcsha256"]; ok {
+		cap.Tap("ExePath", "Exesha256")
+	}
 	// github.com/trimble-oss/tierceron-hat
+	return nil
 }
