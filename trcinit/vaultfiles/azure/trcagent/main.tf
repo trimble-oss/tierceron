2b071acf43b8e90b4cd912861efd6d538e71a0dc
Joel Rieke <joel_rieke@trimble.com>
2023-02-10 13:29:51 -0800

    Updating to ubuntu 20.04

--- trcinit/vaultfiles/azure/trcagent/main.tf
+++ trcinit/vaultfiles/azure/trcagent/main.tf
@@ -302,7 +302,7 @@ resource "azurerm_linux_virtual_machine" "az-vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 
