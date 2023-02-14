63c12ef628080d3b91b7122fb47f0d2f6deab4b6
Joel Rieke <joel_rieke@trimble.com>
2023-02-14 06:30:26 -0800

    Initial implementation of Trc shell -- Pronounced 'Tres shell' or 'Trc shell'.

--- trcvault/carrierfactory/capauth/capauth.go
+++ trcvault/carrierfactory/capauth/capauth.go
@@ -1,18 +1,23 @@
 package capauth
 
 import (
+	"crypto/tls"
 	"log"
 	"sync"
 
+	"github.com/trimble-oss/tierceron/trcsh/trcshauth"
+
 	"github.com/trimble-oss/tierceron-hat/cap"
 	"github.com/trimble-oss/tierceron/vaulthelper/kv"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var onceMemo sync.Once
 
 func Init(mod *kv.Modifier, pluginConfig map[string]interface{}, logger *log.Logger) error {
 
-	certifyMap, err := mod.ReadData("super-secrets/Index/TrcVault/trcplugin/trcagentctl/Certify")
+	certifyMap, err := mod.ReadData("super-secrets/Index/TrcVault/trcplugin/trcsh/Certify")
 	if err != nil {
 		return err
 	}
@@ -20,8 +25,8 @@ func Init(mod *kv.Modifier, pluginConfig map[string]interface{}, logger *log.Log
 	if _, ok := certifyMap["trcsha256"]; ok {
 		logger.Println("Registering cap auth.")
 		go func() {
-			//			err := cap.Tap("/home/jrieke/workspace/Github/tierceron/trcagentctl/__debug_bin", certifyMap["trcsha256"].(string))
-			err := cap.Tap("/home/azuredeploy/bin/trcagentctl", certifyMap["trcsha256"].(string))
+			err := cap.Tap("/home/jrieke/workspace/Github/tierceron/trcsh/__debug_bin", certifyMap["trcsha256"].(string))
+			//		err := cap.Tap("/home/azuredeploy/bin/trcsh", certifyMap["trcsha256"].(string))
 			if err != nil {
 				logger.Println("Cap tap failed with error: " + err.Error())
 			}
@@ -33,7 +38,7 @@ func Init(mod *kv.Modifier, pluginConfig map[string]interface{}, logger *log.Log
 }
 
 // Things to make available to trusted agent.
-func MemorizeAndStart(memorizeFields map[string]interface{}, logger *log.Logger) {
+func MemorizeAndStart(memorizeFields map[string]interface{}, logger *log.Logger) error {
 	for key, value := range memorizeFields {
 		switch key {
 		case "vaddress", "pubrole", "configrole", "kubeconfig":
@@ -43,7 +48,26 @@ func MemorizeAndStart(memorizeFields map[string]interface{}, logger *log.Logger)
 			logger.Println("Skipping key: " + key)
 		}
 	}
+	mashupCertBytes, err := trcshauth.MashupCert.ReadFile("tls/mashup.crt")
+	if err != nil {
+		log.Printf("Couldn't load cert: %v\n", err)
+		return err
+	}
+
+	mashupKeyBytes, err := trcshauth.MashupKey.ReadFile("tls/mashup.key")
+	if err != nil {
+		log.Printf("Couldn't load key: %v\n", err)
+		return err
+	}
+
+	cert, err := tls.X509KeyPair(mashupCertBytes, mashupKeyBytes)
+	if err != nil {
+		log.Printf("Couldn't load cert: %v\n", err)
+		return err
+	}
+	creds := credentials.NewServerTLSFromCert(&cert)
 
 	// TODO: make port configured and stored in vault.
-	cap.TapServer("127.0.0.1:12384")
+	cap.TapServer("127.0.0.1:12384", grpc.Creds(creds))
+	return nil
 }
