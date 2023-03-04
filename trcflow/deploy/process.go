412bc284572d506704ece63735a4e7be31359b2f
Karnveer Gill <karnveer_gill@trimble.com>
2023-03-03 16:15:20 -0800

    Adding bypass for deployed plugins

--- trcflow/deploy/process.go
+++ trcflow/deploy/process.go
@@ -104,6 +104,10 @@ func PluginDeployFlow(pluginConfig map[string]interface{}, logger *log.Logger) e
 	if ptcErr != nil {
 		eUtils.LogErrorMessage(config, "PluginDeployFlow failure: plugin load failure: "+ptcErr.Error(), false)
 	}
+	if vaultPluginSignature["deployed"].(bool) && vaultPluginSignature["copied"].(bool) {
+		eUtils.LogErrorMessage(config, "Plugin has already been deployed and copied: "+vaultPluginSignature["trcplugin"].(string), false)
+		return nil
+	}
 
 	if _, ok := vaultPluginSignature["trcplugin"]; !ok {
 		// TODO: maybe delete plugin if it exists since there was no entry in vault...
