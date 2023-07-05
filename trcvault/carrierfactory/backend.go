c31b73b14cd1523945ce20c59f617b8c9ed6d128
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2023-07-05 13:50:26 -0700

    Merging Develop into Main (#647)

--- trcvault/carrierfactory/backend.go
+++ trcvault/carrierfactory/backend.go
@@ -35,7 +35,7 @@ func Init(processFlowConfig trcvutils.ProcessFlowConfig, processFlowInit trcvuti
 
 	// Set up a table process runner.
 	//	go initVaultHostBootstrap()
-	//	<-vaultHostInitialized
+	<-vaultHostInitialized
 
 	var configCompleteChan chan bool = nil
 	if !headless {
@@ -215,10 +215,18 @@ func initVaultHostRemoteBootstrap(vaddr string) {
 			logger.Println("TrcCarrierUpdate stage 1.1")
 			vaultHost = vaddr
 			vaultPort = vaultUrl.Port()
+			vaultHostInitialized <- true
+			vaultInitialized <- true
 		} else {
 			logger.Println("Bad address: " + vaddr)
 		}
+	} else {
+		go func() { //Is this always true if vaultHost && port is not empty
+			vaultHostInitialized <- true
+			vaultInitialized <- true
+		}()
 	}
+
 }
 
 func parseToken(e *logical.StorageEntry) (map[string]interface{}, error) {
