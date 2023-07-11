c45f5e6fd0a0f61bbebe741340c9fe313de06316
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2023-07-11 08:51:51 -0700

    Main (#655)
    * Update to address some staging/prod issues. (#654)
    
    * Adding some code changes needed to support trcshell in staging and prod.  Stab at fixing carrier wedges.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>
    Co-authored-by: Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
    Co-authored-by: meghan-bailey <107890930+meghan-bailey@users.noreply.github.com>

--- trcvault/plugins/carrier/main.go
+++ trcvault/plugins/carrier/main.go
@@ -54,8 +54,9 @@ func main() {
 		logger.Println("Running in developer mode with self signed certs.")
 		args = append(args, "--tls-skip-verify=true")
 	} else {
-		// TODO: this may not be needed...
-		//	args = append(args, fmt.Sprintf("--ca-cert=", caPEM))
+		logger.Println("Running plugin with cert validation...")
+		args = append(args, fmt.Sprintf("--client-cert=%s", "/etc/opt/vault/certs/serv_cert.pem"))
+		args = append(args, fmt.Sprintf("--client-key=%s", "/etc/opt/vault/certs/serv_key.pem"))
 	}
 
 	argErr := flags.Parse(args[1:])
