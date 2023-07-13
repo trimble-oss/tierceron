2e2a387cc214d354ea7f973692856e9749491f2c
Joel Rieke <joel_rieke@trimble.com>
2023-07-12 19:51:48 -0700

    Cleanup and simplify.

--- trcvault/deploy/deployupdate.sh
+++ trcvault/deploy/deployupdate.sh
@@ -41,14 +41,8 @@ else
     read PRE_CERTIFY
 fi
 
-if [ "$VAULT_ENV" = "prod" ] || [ "$VAULT_ENV" = "staging" ]; then
-    if [ "$PRE_CERTIFY" = "Y" ] || [ "$PRE_CERTIFY" = "yes" ]; then
-    trcplgtool -env=$VAULT_ENV -certify -addr=$VAULT_ADDR -token=$VAULT_TOKEN -insecure -pluginName=$TRC_PLUGIN_NAME-prod -sha256=$(cat target/$TRC_PLUGIN_NAME-prod.sha256)
-    fi
-else
-    if [ "$PRE_CERTIFY" = "Y" ] || [ "$PRE_CERTIFY" = "yes" ]; then
+if [ "$PRE_CERTIFY" = "Y" ] || [ "$PRE_CERTIFY" = "yes" ]; then
     trcplgtool -env=$VAULT_ENV -certify -addr=$VAULT_ADDR -token=$VAULT_TOKEN -insecure -pluginName=$TRC_PLUGIN_NAME -sha256=$(cat target/$TRC_PLUGIN_NAME.sha256)
-    fi
 fi
 
 if [ "$VAULT_PLUGIN_DIR" ]
@@ -56,17 +50,10 @@ then
     echo "Local plugin registration skipping certification."
 else
     echo "Checking plugin deploy status."
-    if [ "$VAULT_ENV" = "prod" ] || [ "$VAULT_ENV" = "staging" ]; then
-    echo "Certifying plugin for env $VAULT_ENV."
-    trcplgtool -env=$VAULT_ENV -checkDeployed -addr=$VAULT_ADDR -token=$VAULT_TOKEN -insecure -pluginName=$TRC_PLUGIN_NAME-prod -sha256=$(cat target/$TRC_PLUGIN_NAME-prod.sha256)
-    status=$?
-    echo "Plugin certified with result $status."
-    else
     echo "Certifying plugin for env $VAULT_ENV."
     trcplgtool -env=$VAULT_ENV -checkDeployed -addr=$VAULT_ADDR -token=$VAULT_TOKEN -insecure -pluginName=$TRC_PLUGIN_NAME -sha256=$(cat target/$TRC_PLUGIN_NAME.sha256)
     status=$?
     echo "Plugin certified with result $status."
-    fi 
 
     if [ $status -eq 0 ]; then       
     echo "This version of the plugin has already been deployed - enabling for environment $VAULT_ENV."
@@ -88,70 +75,40 @@ export VAULT_ADDR
 export VAULT_TOKEN
 export VAULT_API_ADDR
 
-if [ "$VAULT_ENV" = "prod" ] || [ "$VAULT_ENV" = "staging" ]; then
-    # Just writing to vault will trigger the carrier plugin...
+if [ "$VAULT_PLUGIN_DIR" ]
+then
+    chmod 700 target/$TRC_PLUGIN_NAME
+    cp target/$TRC_PLUGIN_NAME $VAULT_PLUGIN_DIR
+    SHAVAL=$( cat target/$TRC_PLUGIN_NAME.sha256 )
+    sudo setcap cap_ipc_lock=+ep $VAULT_PLUGIN_DIR/$TRC_PLUGIN_NAME
+else
+    # Just writing to vault will trigger a copy...
     # First we set Copied to false...
     # This should also trigger the copy process...
     # It should return sha256 of copied plugin on success.
-    SHA256BUNDLE=$(vault write vaultcarrier/$VAULT_ENV token=$VAULT_CARRIER_DEPLOY_TOKEN plugin=$TRC_PLUGIN_NAME-prod vaddress=$VAULT_ADDR)
+    SHA256BUNDLE=$(vault write vaultcarrier/$VAULT_ENV token=$VAULT_CARRIER_DEPLOY_TOKEN plugin=$TRC_PLUGIN_NAME vaddress=$VAULT_ADDR)
     SHAVAL=$(echo $SHA256BUNDLE | awk '{print $6}')
-    
-    if [ "$SHAVAL" != "$FILESHAVAL" ]; then
-    echo "Carrier failed to deploy plugin.  Please try again."
-    exit -1
+
+    if [ "$SHAVAL" = "Failure" ]; then
+        echo "Failed to obtain sha256 for indicated plugin.   Refusing to continue."
+        exit -1
     fi
-
-    if [ "$SHAVAL" = "" ]; then
-    echo "Failed to obtain sha256 for indicated plugin.   Refusing to continue."
-    exit -1
-    fi
-
-    echo "Registering new plugin."
-    vault plugin register \
-            -command=$TRC_PLUGIN_NAME-prod \
-            -sha256=$(echo $SHAVAL) \
-            -args=`backendUUID=789` \
-            $TRC_PLUGIN_NAME-prod
-    echo "Enabling new plugin."
-    vault plugin reload \
-            -plugin $TRC_PLUGIN_NAME-prod
-else
-    if [ "$VAULT_PLUGIN_DIR" ]
-    then
-        chmod 700 target/$TRC_PLUGIN_NAME
-        cp target/$TRC_PLUGIN_NAME $VAULT_PLUGIN_DIR
-        SHAVAL=$( cat target/$TRC_PLUGIN_NAME.sha256 )
-        sudo setcap cap_ipc_lock=+ep $VAULT_PLUGIN_DIR/$TRC_PLUGIN_NAME
-    else
-        # Just writing to vault will trigger a copy...
-        # First we set Copied to false...
-        # This should also trigger the copy process...
-        # It should return sha256 of copied plugin on success.
-        SHA256BUNDLE=$(vault write vaultcarrier/$VAULT_ENV token=$VAULT_CARRIER_DEPLOY_TOKEN plugin=$TRC_PLUGIN_NAME vaddress=$VAULT_ADDR)
-        SHAVAL=$(echo $SHA256BUNDLE | awk '{print $6}')
-
-        if [ "$SHAVAL" = "Failure" ]; then
-            echo "Failed to obtain sha256 for indicated plugin.   Refusing to continue."
-            exit -1
-        fi
-    fi
-
-    if [ "$SHAVAL" != "$FILESHAVAL" ]; then
-    echo "Carrier failed to deploy plugin.  Please try again."
-    exit -1
-    fi
-
-    echo "Registering new plugin."
-    vault plugin register \
-            -command=$TRC_PLUGIN_NAME \
-            -sha256=$(echo $SHAVAL) \
-            -args=`backendUUID=789` \
-            $TRC_PLUGIN_NAME
-    echo "Enabling new plugin."
-    vault plugin reload \
-            -plugin $TRC_PLUGIN_NAME
 fi
 
+if [ "$SHAVAL" != "$FILESHAVAL" ]; then
+    echo "Carrier failed to deploy plugin.  Please try again."
+    exit -1
+fi
+
+echo "Registering new plugin."
+vault plugin register \
+        -command=$TRC_PLUGIN_NAME \
+        -sha256=$(echo $SHAVAL) \
+        -args=`backendUUID=789` \
+        $TRC_PLUGIN_NAME
+echo "Enabling new plugin."
+vault plugin reload \
+        -plugin $TRC_PLUGIN_NAME
 
 #Activates/starts the deployed plugin.
 # Note: plugin should update deployed flag for itself.
