363da36514d4c0ff0b0df38c33086aff8d7ac990
Joel Rieke <joel_rieke@trimble.com>
2023-07-14 11:26:03 -0700

    Fixing prod deploy.

--- trcvault/deploy/refreshtoken.sh
+++ trcvault/deploy/refreshtoken.sh
@@ -3,16 +3,21 @@
 echo "Enter plugin name: "
 read TRC_PLUGIN_NAME
 
-echo "Enter vault host base url: "
+if [ "$TRC_PLUGIN_NAME" = 'trc-vault-carrier-plugin' ] ; then
+    echo "Use refreshcarriertoken to refresh carrier tokens."
+    exit 1
+fi
+
+echo "Enter organization vault host base url including port: "
 read VAULT_ADDR
 
-echo "Enter root token: "
+echo "Enter organization vault root token: "
 read VAULT_TOKEN
 
 echo "Enter environment: "
 read VAULT_ENV
 
-echo "Enter trc plugin runtime environment token with write permissions unrestricted: "
+echo "Enter organization vault unrestricted environment token with write permissions: "
 read VAULT_ENV_TOKEN
 
 VAULT_API_ADDR=VAULT_ADDR
