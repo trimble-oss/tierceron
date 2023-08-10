1752470d725317688971b2e84b3d52f5bb23e18c
Karnveer Gill <karnveer_gill@trimble.com>
2023-08-10 13:12:46 -0700

    Merge fix

--- trcvault/util/repository/azrcr.go
+++ trcvault/util/repository/azrcr.go
@@ -22,7 +22,6 @@ import (
 func getImageSHA(config *eUtils.DriverConfig, svc *azidentity.ClientSecretCredential, pluginToolConfig map[string]interface{}) error {
 	client, err := azcontainerregistry.NewClient(
 		pluginToolConfig["acrrepository"].(string),
-		pluginToolConfig["acrrepository"].(string),
 		svc, nil)
 	if err != nil {
 		config.Log.Printf("failed to create client: %v", err)
@@ -45,7 +44,6 @@ func getImageSHA(config *eUtils.DriverConfig, svc *azidentity.ClientSecretCreden
 	}
 
 	// Get manifest
-	manifestRes, err := client.GetManifest(ctx, pluginToolConfig["trcplugin"].(string), latestTag, &azcontainerregistry.ClientGetManifestOptions{Accept: to.Ptr(string(azcontainerregistry.ContentTypeApplicationVndDockerDistributionManifestV2JSON))})
 	manifestRes, err := client.GetManifest(ctx, pluginToolConfig["trcplugin"].(string), latestTag, &azcontainerregistry.ClientGetManifestOptions{Accept: to.Ptr(string(azcontainerregistry.ContentTypeApplicationVndDockerDistributionManifestV2JSON))})
 	if err != nil {
 		config.Log.Printf("failed to get manifest: %v", err)
@@ -82,9 +80,7 @@ func getImageSHA(config *eUtils.DriverConfig, svc *azidentity.ClientSecretCreden
 
 // Return url to the image to be used for download.
 func GetImageAndShaFromDownload(config *eUtils.DriverConfig, pluginToolConfig map[string]interface{}) error {
-func GetImageAndShaFromDownload(config *eUtils.DriverConfig, pluginToolConfig map[string]interface{}) error {
 	svc, err := azidentity.NewClientSecretCredential(
-		pluginToolConfig["azureTenantId"].(string),
 		pluginToolConfig["azureTenantId"].(string),
 		pluginToolConfig["azureClientId"].(string),
 		pluginToolConfig["azureClientSecret"].(string),
@@ -110,13 +106,11 @@ func GetImageAndShaFromDownload(config *eUtils.DriverConfig, pluginToolConfig ma
 		log.Fatalf("failed to create validation reader: %v", readErr)
 	}
 
-
 	layerData, configErr := io.ReadAll(reader)
 	if configErr != nil {
 		log.Fatalf("failed to read config data: %v", configErr)
 	}
 
-	pluginTarredData, gUnZipError := gUnZipData(layerData)
 	pluginTarredData, gUnZipError := gUnZipData(layerData)
 	if gUnZipError != nil {
 		return errors.New("Gunzip failed.")
