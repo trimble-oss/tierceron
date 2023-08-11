1bd4549a1eb2c985f10aaf9613f356b3bb16be5a
Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
2023-08-11 14:06:41 -0700

    Merging Main to Plugin (#721)

--- trcconfig/trcconfig.go
+++ trcconfig/trcconfig.go
@@ -22,8 +22,9 @@ func main() {
 	addrPtr := flag.String("addr", "", "API endpoint for the vault")
 	tokenPtr := flag.String("token", "", "Vault access token")
 	secretIDPtr := flag.String("secretID", "", "Secret app role ID")
+	regionPtr := flag.String("region", "", "Region to be processed") //If this is blank -> use context otherwise override context.
 	appRoleIDPtr := flag.String("appRoleID", "", "Public app role ID")
 	tokenNamePtr := flag.String("tokenName", "", "Token name used by this"+coreopts.GetFolderPrefix(nil)+"config to access the vault")
 
-	trcconfigbase.CommonMain(envPtr, addrPtr, tokenPtr, nil, secretIDPtr, appRoleIDPtr, tokenNamePtr, nil)
+	trcconfigbase.CommonMain(envPtr, addrPtr, tokenPtr, nil, secretIDPtr, appRoleIDPtr, tokenNamePtr, regionPtr, nil)
 }
