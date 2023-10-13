3c15c41849bcb823bf67912c4f7417a4b51697c0
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2023-10-13 16:56:28 -0700

    Feature/trcshellenhanced (#745)
    * Initial refactorings to support trcshell enhanced.
    
    * Temporary checkin.
    
    * Cleanup and refactor for trcshagent service support.
    
    * Small cleanup.
    
    * Adding missing file.
    
    * Rolling back override
    
    * Adding back caddr & ctoken
    
    * Checking in progress.
    
    * Checking in progress for trcsh.
    
    * Checking in progress.
    
    * One more checkin.
    
    * Adding CDS to update deploy status
    
    * Added cds support to update deploy status
    
    * Adding ability to define a service.
    
    * Make pluginName requried.
    
    * Add ability for sha256sum to be interpretted as an actual location to a file instead.
    
    * Update for writing out deploy status to support cds
    
    * Adding fields to template
    
    * Improvements to certify.
    
    * Restructure and make ctoken available to trcsh.
    
    * Adding nil check.
    
    * Add trcsh certification (not definition) for plugins in trcsh.
    
    * Added support for read status with cds
    
    * Minor tweaks to trcsh
    
    * Adding region support through cds for certify
    
    * Renaming variable
    
    * A little cleanup.
    
    * removing hostname
    
    * Disabling region certification for non vault type
    
    * A little simplification.
    
    * One more cleanup.
    
    * Minor cleanup.
    
    * Adding support for initialization of regioned and non-regioned data through certify
    
    ---------
    
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>

--- trcsh/trcsh.go
+++ trcsh/trcsh.go
@@ -7,12 +7,15 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/exec"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/trimble-oss/tierceron-hat/cap"
 	"github.com/trimble-oss/tierceron/buildopts/coreopts"
 	"github.com/trimble-oss/tierceron/buildopts/memprotectopts"
 	"github.com/trimble-oss/tierceron/trcconfigbase"
@@ -21,10 +24,14 @@ import (
 	"github.com/trimble-oss/tierceron/trcsh/trcshauth"
 	"github.com/trimble-oss/tierceron/trcvault/carrierfactory/capauth"
 	"github.com/trimble-oss/tierceron/trcvault/opts/memonly"
+	"github.com/trimble-oss/tierceron/trcvault/trcplgtoolbase"
 	eUtils "github.com/trimble-oss/tierceron/utils"
 
 	helperkv "github.com/trimble-oss/tierceron/vaulthelper/kv"
 )
+
+var gAgentConfig *trcshauth.AgentConfigs = nil
+var gTrcshConfig *trcshauth.TrcShConfig
 
 // This is a controller program that can act as any command line utility.
 // The Tierceron Shell runs tierceron and kubectl commands in a secure shell.
@@ -33,41 +40,226 @@ func main() {
 		memprotectopts.MemProtectInit(nil)
 	}
 	eUtils.InitHeadless(true)
-	fmt.Println("trcsh Version: " + "1.11")
+	fmt.Println("trcsh Version: " + "1.12")
+	var envPtr, regionPtr, trcPathPtr, appRoleIDPtr, secretIDPtr *string
 
-	if os.Geteuid() == 0 {
-		fmt.Println("Trcsh cannot be run as root.")
-		os.Exit(-1)
+	if runtime.GOOS != "windows" {
+		if os.Geteuid() == 0 {
+			fmt.Println("Trcsh cannot be run as root.")
+			os.Exit(-1)
+		} else {
+			capauth.CheckNotSudo()
+		}
+		if len(os.Args) > 1 {
+			if strings.Contains(os.Args[1], "trc") && !strings.Contains(os.Args[1], "-c") {
+				// Running as shell.
+				os.Args[1] = "-c=" + os.Args[1]
+			}
+		}
+		envPtr = flag.String("env", "", "Environment to be processed")   //If this is blank -> use context otherwise override context.
+		regionPtr = flag.String("region", "", "Region to be processed")  //If this is blank -> use context otherwise override context.
+		trcPathPtr = flag.String("c", "", "Optional script to execute.") //If this is blank -> use context otherwise override context.
+		appRoleIDPtr = flag.String("appRoleID", "", "Public app role ID")
+		secretIDPtr = flag.String("secretID", "", "App role secret")
+		flag.Parse()
+
+		if len(*appRoleIDPtr) == 0 {
+			*appRoleIDPtr = os.Getenv("DEPLOY_ROLE")
+		}
+
+		if len(*secretIDPtr) == 0 {
+			*secretIDPtr = os.Getenv("DEPLOY_SECRET")
+		}
+		memprotectopts.MemProtect(nil, secretIDPtr)
+		memprotectopts.MemProtect(nil, appRoleIDPtr)
+
+		//Open deploy script and parse it.
+		ProcessDeploy(*envPtr, *regionPtr, "", *trcPathPtr, secretIDPtr, appRoleIDPtr, true)
 	} else {
-		capauth.CheckNotSudo()
+		gAgentConfig = &trcshauth.AgentConfigs{}
+		agentName := os.Getenv("AGENT_NAME")
+		agentToken := os.Getenv("AGENT_TOKEN")
+		agentEnv := os.Getenv("AGENT_ENV")
+		address := os.Getenv("VAULT_ADDR")
+
+		if len(agentToken) == 0 {
+			fmt.Println("trcsh on windows requires agent token.")
+			os.Exit(-1)
+		}
+
+		if len(address) == 0 {
+			fmt.Println("trcsh on windows requires VAULT address.")
+			os.Exit(-1)
+		}
+
+		memprotectopts.MemProtect(nil, &agentToken)
+		memprotectopts.MemProtect(nil, &address)
+		shutdown := make(chan bool)
+
+		// Preload agent synchronization configs
+		gAgentConfig.LoadConfigs(address, agentToken, agentName, agentEnv)
+		for {
+			if featherMode, featherErr := cap.FeatherCtlEmit(*gAgentConfig.EncryptPass,
+				*gAgentConfig.EncryptSalt,
+				*gAgentConfig.CarrierCtlHostPort,
+				*gAgentConfig.DeployRoleID,
+				cap.MODE_GLIDE, agentName+"."+*gAgentConfig.Env); featherErr == nil && featherMode == cap.MODE_FEATHER {
+				ProcessDeploy(*envPtr, *regionPtr, "", *trcPathPtr, secretIDPtr, appRoleIDPtr, false)
+			}
+		}
+		<-shutdown
 	}
-	if len(os.Args) > 1 {
-		if strings.Contains(os.Args[1], "trc") && !strings.Contains(os.Args[1], "-c") {
-			// Running as shell.
-			os.Args[1] = "-c=" + os.Args[1]
+
+}
+
+func configCmd(env string,
+	trcshConfig *trcshauth.TrcShConfig,
+	region string,
+	config *eUtils.DriverConfig,
+	agentToken bool,
+	token string,
+	argsOrig []string,
+	deployArgLines []string,
+	configCount *int) {
+	*configCount -= 1
+	if *configCount != 0 { //This is to keep result channel open - closes on the final config call of the script.
+		config.EndDir = "deploy"
+	}
+	config.AppRoleConfig = "config.yml"
+	config.FileFilter = nil
+	config.EnvRaw = env
+	config.WantCerts = false
+	config.IsShellSubProcess = true
+
+	configRoleSlice := strings.Split(*trcshConfig.ConfigRole, ":")
+	tokenName := "config_token_" + env
+	tokenConfig := ""
+	configEnv := env
+
+	trcconfigbase.CommonMain(&configEnv, &config.VaultAddress, &tokenConfig, &trcshConfig.EnvContext, &configRoleSlice[1], &configRoleSlice[0], &tokenName, &region, config)
+	ResetModifier(config)                                            //Resetting modifier cache to avoid token conflicts.
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) //Reset flag parse to allow more toolset calls.
+
+	if !agentToken {
+		token = ""
+		config.Token = token
+	}
+}
+
+func processPluginCmds(trcKubeDeploymentConfig *kube.TrcKubeConfig,
+	onceKubeInit *sync.Once,
+	PipeOS billy.File,
+	env string,
+	trcshConfig *trcshauth.TrcShConfig,
+	region string,
+	config *eUtils.DriverConfig,
+	control string,
+	agentToken bool,
+	token string,
+	argsOrig []string,
+	deployArgLines []string,
+	configCount *int,
+	logger *log.Logger) {
+	switch control {
+	case "trcpub":
+		config.AppRoleConfig = "configpub.yml"
+		config.EnvRaw = env
+		config.IsShellSubProcess = true
+
+		pubRoleSlice := strings.Split(*gTrcshConfig.PubRole, ":")
+		tokenName := "vault_pub_token_" + env
+		tokenPub := ""
+		pubEnv := env
+
+		trcpubbase.CommonMain(&pubEnv, &config.VaultAddress, &tokenPub, &trcshConfig.EnvContext, &pubRoleSlice[1], &pubRoleSlice[0], &tokenName, config)
+		ResetModifier(config)                                            //Resetting modifier cache to avoid token conflicts.
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) //Reset flag parse to allow more toolset calls.
+		if !agentToken {
+			token = ""
+			config.Token = token
+		}
+	case "trcconfig":
+		configCmd(env, trcshConfig, region, config, agentToken, token, argsOrig, deployArgLines, configCount)
+	case "trcplgtool":
+		config.AppRoleConfig = "configpub.yml"
+		config.EnvRaw = env
+		config.IsShellSubProcess = true
+
+		trcplgtoolbase.CommonMain(&env, &config.VaultAddress, trcshConfig.CToken, &region, config)
+		ResetModifier(config)                                            //Resetting modifier cache to avoid token conflicts.
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) //Reset flag parse to allow more toolset calls.
+		if !agentToken {
+			token = ""
+			config.Token = token
+		}
+
+	case "kubectl":
+		onceKubeInit.Do(func() {
+			var kubeInitErr error
+			trcKubeDeploymentConfig, kubeInitErr = kube.InitTrcKubeConfig(trcshConfig, config)
+			if kubeInitErr != nil {
+				fmt.Println(kubeInitErr)
+				return
+			}
+		})
+		trcKubeDeploymentConfig.PipeOS = PipeOS
+
+		kubectlErrChan := make(chan error, 1)
+
+		go func() {
+			kubectlErrChan <- kube.KubeCtl(trcKubeDeploymentConfig, config)
+		}()
+
+		select {
+		case <-time.After(15 * time.Second):
+			fmt.Println("Agent is not yet ready..")
+			logger.Println("Timed out waiting for KubeCtl.")
+			os.Exit(-1)
+		case kubeErr := <-kubectlErrChan:
+			if kubeErr != nil {
+				logger.Println(kubeErr)
+				os.Exit(-1)
+			}
 		}
 	}
-	envPtr := flag.String("env", "", "Environment to be processed")   //If this is blank -> use context otherwise override context.
-	regionPtr := flag.String("region", "", "Region to be processed")  //If this is blank -> use context otherwise override context.
-	trcPathPtr := flag.String("c", "", "Optional script to execute.") //If this is blank -> use context otherwise override context.
-	appRoleIDPtr := flag.String("appRoleID", "", "Public app role ID")
-	secretIDPtr := flag.String("secretID", "", "App role secret")
+}
 
-	flag.Parse()
-
-	if len(*appRoleIDPtr) == 0 {
-		*appRoleIDPtr = os.Getenv("DEPLOY_ROLE")
+func processWindowsCmds(trcKubeDeploymentConfig *kube.TrcKubeConfig,
+	onceKubeInit *sync.Once,
+	PipeOS billy.File,
+	env string,
+	trcshConfig *trcshauth.TrcShConfig,
+	region string,
+	config *eUtils.DriverConfig,
+	control string,
+	agentToken bool,
+	token string,
+	argsOrig []string,
+	deployArgLines []string,
+	configCount *int,
+	logger *log.Logger) {
+	switch control {
+	case "trcwinservicestart":
+		cmd := exec.Command("sc", "start", "TODO")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	case "trcwinservicestop":
+		cmd := exec.Command("sc", "stop", "TODO")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	case "trcwinservicedeploy":
+		// Bring it on Carrier....
+	case "trcplgtool":
+		//
+	case "trcconfig":
+		configCmd(env, trcshConfig, region, config, agentToken, token, argsOrig, deployArgLines, configCount)
 	}
-
-	if len(*secretIDPtr) == 0 {
-		*secretIDPtr = os.Getenv("DEPLOY_SECRET")
-	}
-
-	memprotectopts.MemProtect(nil, secretIDPtr)
-	memprotectopts.MemProtect(nil, appRoleIDPtr)
-
-	//Open deploy script and parse it.
-	ProcessDeploy(*envPtr, *regionPtr, "", *trcPathPtr, secretIDPtr, appRoleIDPtr)
 }
 
 // ProcessDeploy
@@ -83,7 +275,7 @@ func main() {
 // Returns:
 //
 //	Nothing.
-func ProcessDeploy(env string, region string, token string, trcPath string, secretId *string, approleId *string) {
+func ProcessDeploy(env string, region string, token string, trcPath string, secretId *string, approleId *string, outputMemCache bool) {
 	var err error
 	agentToken := false
 	if token != "" {
@@ -128,7 +320,7 @@ func ProcessDeploy(env string, region string, token string, trcPath string, secr
 		Log:               logger,
 		IsShell:           true,
 		IsShellSubProcess: false,
-		OutputMemCache:    true,
+		OutputMemCache:    outputMemCache,
 		MemFs:             memfs.New(),
 		Regions:           regions,
 		ExitOnFailure:     true}
@@ -139,20 +331,31 @@ func ProcessDeploy(env string, region string, token string, trcPath string, secr
 	fmt.Println("Logging initialized.")
 	logger.Printf("Logging initialized for env:%s\n", env)
 
-	// 	trcshConfig := &trcshauth.TrcShConfig{Env: "",
-	// 		EnvContext: "",
-	// 		ConfigRole: "",
-	// 		PubRole:    "",
-	// 	}
-	trcshConfig, err := trcshauth.TrcshAuth(config)
-	if err != nil {
-		fmt.Println("Tierceron bootstrap failure.")
-		logger.Println(err)
-		os.Exit(-1)
+	// Chewbacca: scrub before checkin
+	// This data is generated by TrcshAuth
+	// cToken := ""
+	// configRole := ""
+	// pubRole := ""
+	// gTrcshConfig = &trcshauth.TrcShConfig{Env: "dev",
+	// 	EnvContext: "dev",
+	// 	CToken:     &cToken,
+	// 	ConfigRole: &configRole,
+	// 	PubRole:    &pubRole,
+	// }
+	// config.VaultAddress = ""
+	//	config.Token =
+	// Chewbacca: end scrub
+	if gTrcshConfig == nil {
+		gTrcshConfig, err = trcshauth.TrcshAuth(gAgentConfig, config)
+		if err != nil {
+			fmt.Println("Tierceron bootstrap failure.")
+			logger.Println(err)
+			os.Exit(-1)
+		}
+		fmt.Println("Auth loaded" + env)
 	}
-	fmt.Println("Auth loaded" + env)
 
-	// Begin dbg comment
+	// Chewbacca: Begin dbg comment
 	var auth string
 	authTokenName := "vault_token_azuredeploy"
 	authTokenEnv := "azuredeploy"
@@ -168,13 +371,13 @@ func ProcessDeploy(env string, region string, token string, trcPath string, secr
 		// Generate .trc code...
 		trcPathParts := strings.Split(trcPath, "/")
 		config.FileFilter = []string{trcPathParts[len(trcPathParts)-1]}
-		configRoleSlice := strings.Split(trcshConfig.ConfigRole, ":")
+		configRoleSlice := strings.Split(*gTrcshConfig.ConfigRole, ":")
 		tokenName := "config_token_" + env
 		configEnv := env
 		config.EnvRaw = env
 		config.EndDir = "deploy"
 		config.OutputMemCache = true
-		trcconfigbase.CommonMain(&configEnv, &config.VaultAddress, &token, &trcshConfig.EnvContext, &configRoleSlice[1], &configRoleSlice[0], &tokenName, &region, config)
+		trcconfigbase.CommonMain(&configEnv, &config.VaultAddress, &token, &gTrcshConfig.EnvContext, &configRoleSlice[1], &configRoleSlice[0], &tokenName, &region, config)
 		ResetModifier(config) //Resetting modifier cache to avoid token conflicts.
 
 		var memFile billy.File
@@ -225,7 +428,7 @@ func ProcessDeploy(env string, region string, token string, trcPath string, secr
 
 	for _, deployPipeline := range deployArgLines {
 		deployPipeline = strings.TrimLeft(deployPipeline, " ")
-		if strings.HasPrefix(deployPipeline, "#") {
+		if strings.HasPrefix(deployPipeline, "#") || deployPipeline == "" {
 			continue
 		}
 		// Print current process line.
@@ -276,78 +479,38 @@ func ProcessDeploy(env string, region string, token string, trcPath string, secr
 					os.Args = deployArgs
 				}
 			}
-
-			switch control {
-			case "trcpub":
-				config.AppRoleConfig = "configpub.yml"
-				config.EnvRaw = env
-				config.IsShellSubProcess = true
-
-				pubRoleSlice := strings.Split(trcshConfig.PubRole, ":")
-				tokenName := "vault_pub_token_" + env
-				tokenPub := ""
-				pubEnv := env
-
-				trcpubbase.CommonMain(&pubEnv, &config.VaultAddress, &tokenPub, &trcshConfig.EnvContext, &pubRoleSlice[1], &pubRoleSlice[0], &tokenName, config)
-				ResetModifier(config)                                            //Resetting modifier cache to avoid token conflicts.
-				flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) //Reset flag parse to allow more toolset calls.
-				if !agentToken {
-					token = ""
-					config.Token = token
-				}
-			case "trcconfig":
-				configCount -= 1
-				if configCount != 0 { //This is to keep result channel open - closes on the final config call of the script.
-					config.EndDir = "deploy"
-				}
-				config.AppRoleConfig = "config.yml"
-				config.FileFilter = nil
-				config.EnvRaw = env
-				config.WantCerts = false
-				config.IsShellSubProcess = true
-
-				configRoleSlice := strings.Split(trcshConfig.ConfigRole, ":")
-				tokenName := "config_token_" + env
-				tokenConfig := ""
-				configEnv := env
-
-				trcconfigbase.CommonMain(&configEnv, &config.VaultAddress, &tokenConfig, &trcshConfig.EnvContext, &configRoleSlice[1], &configRoleSlice[0], &tokenName, &region, config)
-				ResetModifier(config)                                            //Resetting modifier cache to avoid token conflicts.
-				flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) //Reset flag parse to allow more toolset calls.
-
-				if !agentToken {
-					token = ""
-					config.Token = token
-				}
-			case "trcpluginctl":
-			case "kubectl":
-				onceKubeInit.Do(func() {
-					var kubeInitErr error
-					trcKubeDeploymentConfig, kubeInitErr = kube.InitTrcKubeConfig(trcshConfig, config)
-					if kubeInitErr != nil {
-						fmt.Println(kubeInitErr)
-						return
-					}
-				})
-				trcKubeDeploymentConfig.PipeOS = PipeOS
-
-				kubectlErrChan := make(chan error, 1)
-
-				go func() {
-					kubectlErrChan <- kube.KubeCtl(trcKubeDeploymentConfig, config)
-				}()
-
-				select {
-				case <-time.After(15 * time.Second):
-					fmt.Println("Agent is not yet ready..")
-					logger.Println("Timed out waiting for KubeCtl.")
-					os.Exit(-1)
-				case kubeErr := <-kubectlErrChan:
-					if kubeErr != nil {
-						logger.Println(kubeErr)
-						os.Exit(-1)
-					}
-				}
+			if runtime.GOOS == "windows" {
+				processWindowsCmds(
+					trcKubeDeploymentConfig,
+					&onceKubeInit,
+					PipeOS,
+					env,
+					gTrcshConfig,
+					region,
+					config,
+					control,
+					agentToken,
+					token,
+					argsOrig,
+					deployArgLines,
+					&configCount,
+					logger)
+			} else {
+				processPluginCmds(
+					trcKubeDeploymentConfig,
+					&onceKubeInit,
+					PipeOS,
+					env,
+					gTrcshConfig,
+					region,
+					config,
+					control,
+					agentToken,
+					token,
+					argsOrig,
+					deployArgLines,
+					&configCount,
+					logger)
 			}
 		}
 	}
--- trcsh/trcshauth/auth.go
+++ trcsh/trcshauth/auth.go
@@ -11,6 +11,7 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
 	"regexp"
@@ -22,6 +23,7 @@ import (
 	eUtils "github.com/trimble-oss/tierceron/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 //go:embed tls/mashup.crt
@@ -62,11 +64,13 @@ func randomString(n int) string {
 }
 
 type TrcShConfig struct {
-	Env        string
-	EnvContext string // Current env context...
-	ConfigRole string
-	PubRole    string
-	KubeConfig string
+	Env          string
+	EnvContext   string // Current env context...
+	VaultAddress *string
+	CToken       *string
+	ConfigRole   *string
+	PubRole      *string
+	KubeConfig   *string
 }
 
 const configDir = "/.tierceron/config.yml"
@@ -131,11 +135,10 @@ func GetSetEnvAddrContext(env string, envContext string, addrPort string) (strin
 }
 
 // Helper function for obtaining auth components.
-func TrcshAuth(config *eUtils.DriverConfig) (*TrcShConfig, error) {
+func TrcshAuth(agentConfig *AgentConfigs, config *eUtils.DriverConfig) (*TrcShConfig, error) {
 	trcshConfig := &TrcShConfig{}
 	var err error
 
-	fmt.Println("Auth phase 1")
 	if config.EnvRaw == "staging" || config.EnvRaw == "prod" || len(config.TrcShellRaw) > 0 {
 		dir, err := os.UserHomeDir()
 		if err != nil {
@@ -147,61 +150,87 @@ func TrcshAuth(config *eUtils.DriverConfig) (*TrcShConfig, error) {
 			fmt.Println("No local kube config found...")
 			os.Exit(1)
 		}
-		trcshConfig.KubeConfig = base64.StdEncoding.EncodeToString(fileBytes)
+		kc := base64.StdEncoding.EncodeToString(fileBytes)
+		trcshConfig.KubeConfig = &kc
 
 		if len(config.TrcShellRaw) > 0 {
 			return trcshConfig, nil
 		}
 	} else {
-		trcshConfig.KubeConfig, err = PenseQuery("kubeconfig")
+		if agentConfig == nil {
+			fmt.Println("Auth phase 1")
+			trcshConfig.KubeConfig, err = PenseQuery("kubeconfig")
+		}
 	}
 
 	if err != nil {
 		return trcshConfig, err
 	}
-	memprotectopts.MemProtect(nil, &trcshConfig.KubeConfig)
+	if trcshConfig.KubeConfig != nil {
+		memprotectopts.MemProtect(nil, trcshConfig.KubeConfig)
+	}
 
-	fmt.Println("Auth phase 2")
-	addr, vAddressErr := PenseQuery("vaddress")
-	if vAddressErr != nil {
+	if agentConfig != nil {
+		trcshConfig.VaultAddress, err = PenseFeatherQuery(agentConfig, "vaddress")
+	} else {
+		fmt.Println("Auth phase 2")
+		trcshConfig.VaultAddress, err = PenseQuery("vaddress")
+	}
+	memprotectopts.MemProtect(nil, trcshConfig.VaultAddress)
+
+	if err != nil {
 		var addrPort string
 		var env, envContext string
 
-		fmt.Println(vAddressErr)
+		fmt.Println(err)
 		//Env should come from command line - not context here. but addr port is needed.
 		trcshConfig.Env, trcshConfig.EnvContext, addrPort, err = GetSetEnvAddrContext(env, envContext, addrPort)
 		if err != nil {
 			fmt.Println(err)
 			return trcshConfig, err
 		}
-		addr = "https://127.0.0.1:" + addrPort
+		vAddr := "https://127.0.0.1:" + addrPort
+		trcshConfig.VaultAddress = &vAddr
 
 		config.Env = env
 		config.EnvRaw = env
 	}
 
-	config.VaultAddress = addr
+	config.VaultAddress = *trcshConfig.VaultAddress
 	memprotectopts.MemProtect(nil, &config.VaultAddress)
 
-	fmt.Println("Auth phase 3")
-	trcshConfig.ConfigRole, err = PenseQuery("configrole")
-	if err != nil {
-		return trcshConfig, err
+	if agentConfig != nil {
+		trcshConfig.ConfigRole, err = PenseFeatherQuery(agentConfig, "configrole")
+	} else {
+		fmt.Println("Auth phase 3")
+		trcshConfig.ConfigRole, err = PenseQuery("configrole")
 	}
-	memprotectopts.MemProtect(nil, &trcshConfig.ConfigRole)
+	memprotectopts.MemProtect(nil, trcshConfig.ConfigRole)
 
-	fmt.Println("Auth phase 4")
-	trcshConfig.PubRole, err = PenseQuery("pubrole")
-	if err != nil {
-		return trcshConfig, err
+	if agentConfig == nil {
+		fmt.Println("Auth phase 4")
+		trcshConfig.ConfigRole, err = PenseQuery("pubrole")
+		if err != nil {
+			return trcshConfig, err
+		}
+		memprotectopts.MemProtect(nil, trcshConfig.PubRole)
 	}
-	memprotectopts.MemProtect(nil, &trcshConfig.PubRole)
+
+	if agentConfig == nil {
+		fmt.Println("Auth phase 5")
+		trcshConfig.CToken, err = PenseQuery("ctoken")
+		if err != nil {
+			return trcshConfig, err
+		}
+		memprotectopts.MemProtect(nil, trcshConfig.CToken)
+	}
+
 	fmt.Println("Auth complete.")
 
 	return trcshConfig, err
 }
 
-func PenseQuery(pense string) (string, error) {
+func PenseQuery(pense string) (*string, error) {
 	penseCode := randomString(7 + rand.Intn(7))
 	penseArray := sha256.Sum256([]byte(penseCode))
 	penseSum := hex.EncodeToString(penseArray[:])
@@ -211,12 +240,12 @@ func PenseQuery(pense string) (string, error) {
 		fmt.Println("Code 54 failure...")
 		// 2023-06-30T01:29:21.7020686Z read unix @->/tmp/trccarrier/trcsnap.sock: read: connection reset by peer
 		os.Exit(-1) // restarting carrier will rebuild necessary resources...
-		return "", errors.Join(errors.New("Tap writer error"), capWriteErr)
+		return new(string), errors.Join(errors.New("Tap writer error"), capWriteErr)
 	}
 
 	conn, err := grpc.Dial("127.0.0.1:12384", grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{ServerName: "", RootCAs: mashupCertPool, InsecureSkipVerify: true})))
 	if err != nil {
-		return "", err
+		return new(string), err
 	}
 	defer conn.Close()
 	c := cap.NewCapClient(conn)
@@ -227,8 +256,49 @@ func PenseQuery(pense string) (string, error) {
 
 	r, penseErr := c.Pense(ctx, &cap.PenseRequest{Pense: penseCode, PenseIndex: pense})
 	if penseErr != nil {
-		return "", errors.Join(errors.New("pense error"), penseErr)
+		return new(string), errors.Join(errors.New("pense error"), penseErr)
+	}
+	var penseProtect *string
+	rPense := r.GetPense()
+	penseProtect = &rPense
+	memprotectopts.MemProtect(nil, penseProtect)
+
+	return penseProtect, nil
+}
+
+func PenseFeatherQuery(agentconfig *AgentConfigs, pense string) (*string, error) {
+	penseCode := randomString(7 + rand.Intn(7))
+	penseArray := sha256.Sum256([]byte(penseCode))
+	penseSum := hex.EncodeToString(penseArray[:])
+
+	_, featherErr := cap.FeatherWriter(*agentconfig.EncryptPass,
+		*agentconfig.EncryptSalt,
+		*agentconfig.CarrierCtlHostPort,
+		*agentconfig.DeployRoleID,
+		penseSum)
+	if featherErr != nil {
+		return nil, featherErr
 	}
 
-	return r.GetPense(), nil
+	conn, err := grpc.Dial(*agentconfig.CarrierHostPort, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := cap.NewCapClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Pense(ctx, &cap.PenseRequest{Pense: penseCode, PenseIndex: pense})
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	var penseProtect *string
+	rPense := r.GetPense()
+	penseProtect = &rPense
+	memprotectopts.MemProtect(nil, penseProtect)
+
+	return penseProtect, nil
 }
