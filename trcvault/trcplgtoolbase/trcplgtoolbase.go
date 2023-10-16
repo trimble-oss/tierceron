bcff174df73e319c007d61408c4bfb84390eea30
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2023-10-16 09:14:02 -0700

    Prepare for release (#747)
    * Rolling back override
    
    * Adding back caddr & ctoken
    
    * Checking in progress.
    
    * Checking in progress for trcsh.
    
    * Checking in progress.
    
    * One more checkin.
    
    * Adding CDS to update deploy status
    
    * Added cds support to update deploy status
    
    * Adding ability to define a service.
    
    * Make pluginName requried.
    
    * Add ability for sha256sum to be interpretted as an actual location to a file instead.
    
    * Update for writing out deploy status to support cds
    
    * Adding fields to template
    
    * Improvements to certify.
    
    * Restructure and make ctoken available to trcsh.
    
    * Adding nil check.
    
    * Add trcsh certification (not definition) for plugins in trcsh.
    
    * Added support for read status with cds
    
    * Minor tweaks to trcsh
    
    * Adding region support through cds for certify
    
    * Renaming variable
    
    * A little cleanup.
    
    * removing hostname
    
    * Disabling region certification for non vault type
    
    * A little simplification.
    
    * One more cleanup.
    
    * Minor cleanup.
    
    * Adding support for initialization of regioned and non-regioned data through certify
    
    ---------
    
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>
    
    * Adding cds support
    
    * Cleanup feathering.
    
    * Disable feathering in prod for now.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>
    Co-authored-by: meghan-bailey <107890930+meghan-bailey@users.noreply.github.com>
    Co-authored-by: Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Karnveer Gill <karnveer_gill@trimble.com>
    Co-authored-by: Karnveer Gill <101755160+KarnveerSGill@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: meghan-bailey <107890930+meghan-bailey@users.noreply.github.com>

--- trcvault/trcplgtoolbase/trcplgtoolbase.go
+++ trcvault/trcplgtoolbase/trcplgtoolbase.go
@@ -1,49 +1,77 @@
 package trcplgtoolbase
 
 import (
+	"bufio"
+	"crypto/sha256"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 
+	"github.com/trimble-oss/tierceron/buildopts"
 	"github.com/trimble-oss/tierceron/buildopts/coreopts"
 	trcvutils "github.com/trimble-oss/tierceron/trcvault/util"
 	"github.com/trimble-oss/tierceron/trcvault/util/repository"
 	eUtils "github.com/trimble-oss/tierceron/utils"
-	helperkv "github.com/trimble-oss/tierceron/vaulthelper/kv"
 )
 
-func PluginMain() {
-	addrPtr := flag.String("addr", "", "API endpoint for the vault")
-	tokenPtr := flag.String("token", "", "Vault access token")
-	envPtr := flag.String("env", "dev", "Environement in vault")
+func CommonMain(envPtr *string,
+	addrPtr *string,
+	tokenPtr *string,
+	regionPtr *string,
+	c *eUtils.DriverConfig) {
+
+	// Main functions are as follows:
+	defineServicePtr := flag.Bool("defineService", false, "Service is defined.")
+	certifyImagePtr := flag.Bool("certify", false, "Used to certifies vault plugin.")
+	// These functions only valid for pluginType trcshservice
+	winservicestopPtr := flag.Bool("winservicestop", false, "To stop a windows service for a particular plugin.")
+	winservicestartPtr := flag.Bool("winservicestart", false, "To start a windows service for a particular plugin.")
+	codebundledeployPtr := flag.Bool("codebundledeploy", false, "To deploy a code bundle.")
+
+	// Common flags...
 	startDirPtr := flag.String("startDir", coreopts.GetFolderPrefix(nil)+"_templates", "Template directory")
 	insecurePtr := flag.Bool("insecure", false, "By default, every ssl connection is secure.  Allows to continue with server connections considered insecure.")
 	logFilePtr := flag.String("log", "./"+coreopts.GetFolderPrefix(nil)+"plgtool.log", "Output path for log files")
-	certifyImagePtr := flag.Bool("certify", false, "Used to certifies vault plugin.")
+
+	// defineService flags...
+	deployrootPtr := flag.String("deployroot", "", "Optional path for deploying services to.")
+	serviceNamePtr := flag.String("serviceName", "", "Optional name of service to use in managing service.")
+	codeBundlePtr := flag.String("codeBundle", "", "Code bundle to deploy.")
+
+	// Common plugin flags...
 	pluginNamePtr := flag.String("pluginName", "", "Used to certify vault plugin")
 	pluginTypePtr := flag.String("pluginType", "vault", "Used to indicate type of plugin.  Default is vault.")
-	pluginPathPtr := flag.String("pluginPathPtr", "", "Optional path for deploying services to.")
+
+	// Certify flags...
 	sha256Ptr := flag.String("sha256", "", "Used to certify vault plugin") //This has to match the image that is pulled -> then we write the vault.
 	checkDeployedPtr := flag.Bool("checkDeployed", false, "Used to check if plugin has been copied, deployed, & certified")
 	checkCopiedPtr := flag.Bool("checkCopied", false, "Used to check if plugin has been copied & certified")
-	hostNamePtr := flag.String("hostName", "", "Used to find overrides for plugin certification.") //This is used for plugin copied/deploy status.
+
 	certifyInit := false
 
-	args := os.Args[1:]
-	for i := 0; i < len(args); i++ {
-		s := args[i]
-		if s[0] != '-' {
-			fmt.Println("Wrong flag syntax: ", s)
+	if c == nil || !c.IsShellSubProcess {
+		args := os.Args[1:]
+		for i := 0; i < len(args); i++ {
+			s := args[i]
+			if s[0] != '-' {
+				fmt.Println("Wrong flag syntax: ", s)
+				os.Exit(1)
+			}
+		}
+		flag.Parse()
+
+		// Prints usage if no flags are specified
+		if flag.NFlag() == 0 {
+			flag.Usage()
 			os.Exit(1)
 		}
-	}
-	flag.Parse()
-
-	// Prints usage if no flags are specified
-	if flag.NFlag() == 0 {
-		flag.Usage()
-		os.Exit(1)
+	} else {
+		flag.CommandLine.Parse(os.Args)
+		flag.Parse()
 	}
 
 	if *certifyImagePtr && (len(*pluginNamePtr) == 0 || len(*sha256Ptr) == 0) {
@@ -56,54 +84,235 @@ func PluginMain() {
 		os.Exit(1)
 	}
 
+	if *defineServicePtr && (len(*pluginNamePtr) == 0) {
+		fmt.Println("Must use -pluginName flag to use -defineService flag")
+		os.Exit(1)
+	}
+
+	if strings.Contains(*pluginNamePtr, ".") {
+		fmt.Println("-pluginName cannot contain reserved character '.'")
+		os.Exit(1)
+	}
+
 	switch *pluginTypePtr {
-	case "vault":
-	case "agent":
+	case "vault": // A vault plugin
+		if c != nil {
+			// TODO: do we want to support Deployment certifications in the pipeline at some point?
+			// If so this is a config check to remove.
+			fmt.Println("Plugin type not supported in trcsh.")
+			os.Exit(-1)
+		}
+	case "agent": // A deployment agent tool.
+		if c != nil {
+			// TODO: do we want to support Deployment certifications in the pipeline at some point?
+			// If so this is a config check to remove.
+			fmt.Println("Plugin type not supported in trcsh.")
+			os.Exit(-1)
+		}
+	case "trcshservice": // A trcshservice managed microservice
 	default:
 		fmt.Println("Unsupported plugin type: " + *pluginTypePtr)
 		os.Exit(1)
 	}
 
-	// If logging production directory does not exist and is selected log to local directory
-	if _, err := os.Stat("/var/log/"); os.IsNotExist(err) && *logFilePtr == "/var/log/"+coreopts.GetFolderPrefix(nil)+"plgtool.log" {
-		*logFilePtr = "./" + coreopts.GetFolderPrefix(nil) + "plgtool.log"
+	if *pluginTypePtr != "vault" {
+		*regionPtr = ""
 	}
-	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 
-	logger := log.New(f, "[INIT]", log.LstdFlags)
-	config := &eUtils.DriverConfig{Insecure: *insecurePtr, Log: logger, ExitOnFailure: true, StartDir: []string{*startDirPtr}, SubSectionValue: *pluginNamePtr}
+	var configBase *eUtils.DriverConfig
+	var logger *log.Logger
+	if c != nil {
+		configBase = c
+		configBase.SubSectionValue = *pluginNamePtr
+		*insecurePtr = configBase.Insecure
+	} else {
+		// If logging production directory does not exist and is selected log to local directory
+		if _, err := os.Stat("/var/log/"); os.IsNotExist(err) && *logFilePtr == "/var/log/"+coreopts.GetFolderPrefix(nil)+"plgtool.log" {
+			*logFilePtr = "./" + coreopts.GetFolderPrefix(nil) + "plgtool.log"
+		}
+		f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		logger = log.New(f, "[INIT]", log.LstdFlags)
 
+		configBase = &eUtils.DriverConfig{Insecure: *insecurePtr, Log: logger, ExitOnFailure: true, StartDir: []string{*startDirPtr}, SubSectionValue: *pluginNamePtr}
+		eUtils.CheckError(configBase, err, true)
+	}
+
+	regions := []string{}
+
+	pluginConfig := map[string]interface{}{}
+	pluginConfig = buildopts.ProcessPluginEnvConfig(pluginConfig) //contains logNamespace for InitVaultMod
+	if pluginConfig == nil {
+		fmt.Println("Error: Could not find plugin config")
+		os.Exit(1)
+	}
+	pluginConfig["env"] = *envPtr
+	pluginConfig["vaddress"] = *addrPtr
+	pluginConfig["token"] = *tokenPtr
+	pluginConfig["ExitOnFailure"] = true
+	pluginConfig["regions"] = []string{*regionPtr}
+	config, mod, vault, err := eUtils.InitVaultModForPlugin(pluginConfig, logger)
+	if err != nil {
+		logger.Println("Error: " + err.Error() + " - 1")
+		logger.Println("Failed to init mod for deploy update")
+		os.Exit(1)
+	}
+	config.StartDir = []string{*startDirPtr}
+	config.SubSectionValue = *pluginNamePtr
+	mod.Env = *envPtr
 	eUtils.CheckError(config, err, true)
 
-	//Grabbing configs
-	mod, err := helperkv.NewModifier(*insecurePtr, *tokenPtr, *addrPtr, *envPtr, nil, true, logger)
-	if mod != nil {
-		defer mod.Release()
+	if strings.HasPrefix(*envPtr, "staging") || strings.HasPrefix(*envPtr, "prod") || strings.HasPrefix(*envPtr, "dev") {
+		supportedRegions := eUtils.GetSupportedProdRegions()
+		if *regionPtr != "" {
+			for _, supportedRegion := range supportedRegions {
+				if *regionPtr == supportedRegion {
+					regions = append(regions, *regionPtr)
+					break
+				}
+			}
+			if len(regions) == 0 {
+				fmt.Println("Unsupported region: " + *regionPtr)
+				os.Exit(1)
+			}
+		}
+		configBase.Regions = regions
 	}
-	if err != nil {
-		eUtils.CheckError(config, err, true)
-	}
-	mod.Env = *envPtr
+
 	// Get existing configs if they exist...
-	pluginToolConfig, plcErr := trcvutils.GetPluginToolConfig(config, mod, coreopts.ProcessDeployPluginEnvConfig(map[string]interface{}{}), *hostNamePtr)
+	pluginToolConfig, plcErr := trcvutils.GetPluginToolConfig(configBase, mod, coreopts.ProcessDeployPluginEnvConfig(map[string]interface{}{}))
 	if plcErr != nil {
 		fmt.Println(plcErr.Error())
 		os.Exit(1)
 	}
 
+	if len(*sha256Ptr) > 0 {
+		fileInfo, statErr := os.Stat(*sha256Ptr)
+		if statErr == nil {
+			if fileInfo.Mode().IsRegular() {
+				file, fileOpenErr := os.Open(*sha256Ptr)
+				if fileOpenErr != nil {
+					fmt.Println(fileOpenErr)
+					return
+				}
+
+				// Close the file when we're done.
+				defer file.Close()
+
+				// Create a reader to the file.
+				reader := bufio.NewReader(file)
+
+				pluginImage, imageErr := io.ReadAll(reader)
+				if imageErr != nil {
+					fmt.Println("Failed to read image:" + imageErr.Error())
+					return
+				}
+				sha256Bytes := sha256.Sum256(pluginImage)
+				*sha256Ptr = fmt.Sprintf("%x", sha256Bytes)
+			}
+		}
+	}
+
 	pluginToolConfig["trcsha256"] = *sha256Ptr
 	pluginToolConfig["pluginNamePtr"] = *pluginNamePtr
+	pluginToolConfig["deployrootPtr"] = *deployrootPtr
+	pluginToolConfig["serviceNamePtr"] = *serviceNamePtr
+	pluginToolConfig["codeBundlePtr"] = *codeBundlePtr
 
 	if _, ok := pluginToolConfig["trcplugin"].(string); !ok {
 		pluginToolConfig["trcplugin"] = pluginToolConfig["pluginNamePtr"].(string)
 		if *certifyImagePtr {
 			certifyInit = true
 		}
+
+		if *defineServicePtr &&
+			!*winservicestopPtr &&
+			!*winservicestartPtr &&
+			!*codebundledeployPtr &&
+			!*certifyImagePtr {
+
+			if c != nil {
+				fmt.Println("Service definition not supported in trcsh.")
+				os.Exit(-1)
+			}
+			if _, ok := pluginToolConfig["deployrootPtr"].(string); ok {
+				pluginToolConfig["trcdeployroot"] = pluginToolConfig["deployrootPtr"].(string)
+			}
+
+			if _, ok := pluginToolConfig["serviceNamePtr"].(string); ok {
+				pluginToolConfig["trcservicename"] = pluginToolConfig["serviceNamePtr"].(string)
+			}
+			if _, ok := pluginToolConfig["codeBundlePtr"].(string); ok {
+				pluginToolConfig["trccodebundle"] = pluginToolConfig["codeBundlePtr"].(string)
+			}
+		}
 	}
-	//Certify Image
-	if *certifyImagePtr {
+
+	//Define Service Image
+	if *defineServicePtr {
+		fmt.Printf("Connecting to vault @ %s\n", *addrPtr)
+		writeMap := make(map[string]interface{})
+		writeMap["trcplugin"] = *pluginNamePtr
+		writeMap["trctype"] = *pluginTypePtr
+
+		if _, ok := pluginToolConfig["trcdeployroot"]; ok {
+			writeMap["trcdeployroot"] = pluginToolConfig["trcdeployroot"]
+		}
+		if _, ok := pluginToolConfig["trcservicename"]; ok {
+			writeMap["trcservicename"] = pluginToolConfig["trcservicename"]
+		}
+		if _, ok := pluginToolConfig["trccodebundle"]; ok {
+			writeMap["trccodebundle"] = pluginToolConfig["trccodebundle"]
+		}
+		_, err = mod.Write(pluginToolConfig["pluginpath"].(string), writeMap, configBase.Log)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Deployment definition applied to vault and is ready for deployments.")
+	} else if *winservicestopPtr {
+		fmt.Printf("Stopping service %s\n", pluginToolConfig["trcservicename"].(string))
+		cmd := exec.Command("sc", "stop", pluginToolConfig["trcservicename"].(string))
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Service stopped: %s\n", pluginToolConfig["trcservicename"].(string))
+
+	} else if *winservicestartPtr {
+		fmt.Printf("Starting service %s\n", pluginToolConfig["trcservicename"].(string))
+		cmd := exec.Command("sc", "start", pluginToolConfig["trcservicename"].(string))
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Service started: %s\n", pluginToolConfig["trcservicename"].(string))
+	} else if *codebundledeployPtr {
+		err := repository.GetImageAndShaFromDownload(configBase, pluginToolConfig)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if pluginToolConfig["trcsha256"].(string) == pluginToolConfig["imagesha256"].(string) {
+			// Write the image to the destination...
+			deployPath := fmt.Sprintf("%s\\%s", pluginToolConfig["trcdeployroot"].(string), pluginToolConfig["trccodebundle"].(string))
+			fmt.Printf("Deploying image to: %s\n", deployPath)
+
+			err = os.WriteFile(deployPath, pluginToolConfig["rawImageFile"].([]byte), 0644)
+			if err != nil {
+				fmt.Println(err.Error())
+				os.Exit(1)
+			}
+			fmt.Println("Image deployed.")
+		} else {
+			fmt.Printf("Image not certified.  Cannot deploy image for %s\n", pluginToolConfig["trcplugin"])
+		}
+	} else if *certifyImagePtr {
+		//Certify Image
 		if !certifyInit {
-			err := repository.GetImageAndShaFromDownload(config, pluginToolConfig)
+			err := repository.GetImageAndShaFromDownload(configBase, pluginToolConfig)
 			if err != nil {
 				fmt.Println(err.Error())
 				os.Exit(1)
@@ -114,32 +323,41 @@ func PluginMain() {
 			fmt.Println("Valid image found.")
 			//SHA MATCHES
 			fmt.Printf("Connecting to vault @ %s\n", *addrPtr)
-			writeMap := make(map[string]interface{})
-			writeMap["trcplugin"] = pluginToolConfig["trcplugin"].(string)
-			writeMap["trcpluginpath"] = *pluginPathPtr
-			writeMap["trctype"] = *pluginTypePtr
-			writeMap["trcsha256"] = pluginToolConfig["trcsha256"].(string)
-			if pluginToolConfig["instances"] == nil {
-				pluginToolConfig["instances"] = "0"
-			}
-			writeMap["instances"] = pluginToolConfig["instances"].(string)
+			logger.Println("TrcCarrierUpdate getting plugin settings for env: " + mod.Env)
+			// The following confirms that this version of carrier has been certified to run...
+			// It will bail if it hasn't.
+			if *regionPtr != "" { //If region is set
+				mod.SectionName = "trcplugin"
+				mod.SectionKey = "/Index/"
+				mod.SubSectionValue = pluginToolConfig["trcplugin"].(string)
 
-			_, err = mod.Write(pluginToolConfig["pluginpath"].(string), writeMap, config.Log)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			writeMap = make(map[string]interface{})
-			writeMap["copied"] = false
-			writeMap["deployed"] = false
-			_, err = mod.Write(pluginToolConfig["overridepath"].(string), writeMap, config.Log)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
+				properties, err := trcvutils.NewProperties(config, vault, mod, mod.Env, "TrcVault", "Certify")
+				if err != nil {
+					fmt.Println("Couldn't create properties for regioned certify:" + err.Error())
+					os.Exit(1)
+				}
 
-			fmt.Println("Image certified in vault and is ready for release.")
+				writeMap, replacedFields := properties.GetPluginData(*regionPtr, "Certify", "config", logger)
 
+				writeErr := properties.WritePluginData(WriteMapUpdate(writeMap, pluginToolConfig, *defineServicePtr, *pluginTypePtr), replacedFields, mod, config.Log, *regionPtr, pluginToolConfig["trcplugin"].(string))
+				if writeErr != nil {
+					fmt.Println(writeErr)
+					os.Exit(1)
+				}
+			} else { //Non region certify
+				writeMap, readErr := mod.ReadData(pluginToolConfig["pluginpath"].(string))
+				if readErr != nil {
+					fmt.Println(readErr)
+					os.Exit(1)
+				}
+
+				_, err = mod.Write(pluginToolConfig["pluginpath"].(string), WriteMapUpdate(writeMap, pluginToolConfig, *defineServicePtr, *pluginTypePtr), configBase.Log)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				fmt.Println("Image certified in vault and is ready for release.")
+			}
 		} else {
 			fmt.Println("Invalid or nonexistent image.")
 			os.Exit(1)
@@ -155,7 +373,7 @@ func PluginMain() {
 			os.Exit(0)
 		}
 
-		err := repository.GetImageAndShaFromDownload(config, pluginToolConfig)
+		err := repository.GetImageAndShaFromDownload(configBase, pluginToolConfig)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
@@ -178,7 +396,7 @@ func PluginMain() {
 			os.Exit(0)
 		}
 
-		err := repository.GetImageAndShaFromDownload(config, pluginToolConfig)
+		err := repository.GetImageAndShaFromDownload(configBase, pluginToolConfig)
 		if err != nil {
 			fmt.Println(err.Error())
 			os.Exit(1)
@@ -194,4 +412,28 @@ func PluginMain() {
 		fmt.Println("Plugin has not been copied or deployed.")
 		os.Exit(2)
 	}
+}
+
+func WriteMapUpdate(writeMap map[string]interface{}, pluginToolConfig map[string]interface{}, defineServicePtr bool, pluginTypePtr string) map[string]interface{} {
+	if pluginTypePtr != "trcshservice" {
+		writeMap["trcplugin"] = pluginToolConfig["trcplugin"].(string)
+		writeMap["trctype"] = pluginTypePtr
+		if pluginToolConfig["instances"] == nil {
+			pluginToolConfig["instances"] = "0"
+		}
+		writeMap["instances"] = pluginToolConfig["instances"].(string)
+	}
+	if defineServicePtr {
+		writeMap["trccodebundle"] = pluginToolConfig["trccodebundle"].(string)
+		writeMap["trcservicename"] = pluginToolConfig["trcservicename"].(string)
+		writeMap["trcdeployroot"] = pluginToolConfig["trcdeployroot"].(string)
+	} else {
+		writeMap["trccodebundle"] = ""
+		writeMap["trcservicename"] = ""
+		writeMap["trcdeployroot"] = ""
+	}
+	writeMap["trcsha256"] = pluginToolConfig["trcsha256"].(string)
+	writeMap["copied"] = false
+	writeMap["deployed"] = false
+	return writeMap
 }
