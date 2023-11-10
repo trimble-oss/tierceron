a55ad568ece1037108772e886f2ec042f1b99f19
Joel Rieke <joel_rieke@trimble.com>
2023-11-10 14:54:12 -0800

    Certify exit with error code.

--- trcvault/trcplgtool/trcplgtool.go
+++ trcvault/trcplgtool/trcplgtool.go
@@ -3,18 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	plgtbase "github.com/trimble-oss/tierceron/trcvault/trcplgtoolbase"
 )
 
 // This executable automates the cerification of a plugin docker image.
 func main() {
-	fmt.Println("Version: " + "1.01")
+	fmt.Println("Version: " + "1.02")
 
 	envPtr := flag.String("env", "dev", "Environment to configure")
 	addrPtr := flag.String("addr", "", "API endpoint for the vault")
 	tokenPtr := flag.String("token", "", "Vault access token")
 	regionPtr := flag.String("region", "", "Region to be processed") //If this is blank -> use context otherwise override context.
 
-	plgtbase.CommonMain(envPtr, addrPtr, tokenPtr, regionPtr, nil)
+	err := plgtbase.CommonMain(envPtr, addrPtr, tokenPtr, regionPtr, nil)
+	if err != nil {
+		os.Exit(1)
+	}
+
 }
