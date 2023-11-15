f8d64e091ae06164a35eae755369546f6988751d
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2023-11-14 19:12:38 -0800

    Prepare for release (#810)
    * Update to make safe for trcshell. (#811)
    
    * Increase timeout.
    * Adding some retries.
    * Make re-entrant safe.
    
    ---------
    Signed-off-by: dependabot[bot] <support@github.com>

--- trcconfigbase/trcconfig.go
+++ trcconfigbase/trcconfig.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/trimble-oss/tierceron/buildopts/coreopts"
 	"github.com/trimble-oss/tierceron/buildopts/memprotectopts"
@@ -20,34 +21,21 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-type ResultData struct {
-	inData *string
-	inPath string
-}
-
-var resultMap = make(map[string]*string)
-var envDiffSlice = make([]string, 0)
-var resultChannel = make(chan *ResultData, 5)
-var fileSysIndex = -1
-var envLength int
-var wg sync.WaitGroup
-var mutex = &sync.Mutex{}
-
-func messenger(inData *string, inPath string) {
-	var data ResultData
-	data.inData = inData
-	data.inPath = inPath
+func messenger(configCtx *utils.ConfigContext, inData *string, inPath string) {
+	var data utils.ResultData
+	data.InData = inData
+	data.InPath = inPath
 	inPathSplit := strings.Split(inPath, "||.")
-	mutex.Lock()
-	_, present := resultMap["filesys||."+inPathSplit[1]]
-	mutex.Unlock()
+	configCtx.Mutex.Lock()
+	_, present := configCtx.ResultMap["filesys||."+inPathSplit[1]]
+	configCtx.Mutex.Unlock()
 	//If data is filesys - skip fileSys loop
 	if strings.Contains(inPath, "filesys") {
 		goto skipSwitch
 	}
 
 	//Read file from filesys once per new file
-	if fileSysIndex != -1 && !present {
+	if configCtx.FileSysIndex != -1 && !present {
 		path, err := os.Getwd()
 		fileData, err1 := os.ReadFile(filepath.FromSlash(path + inPathSplit[1]))
 		if err != nil || err1 != nil {
@@ -55,24 +43,29 @@ func messenger(inData *string, inPath string) {
 			return
 		}
 		dataStr := string(fileData)
-		messenger(&dataStr, "filesys||."+inPathSplit[1])
+		messenger(configCtx, &dataStr, "filesys||."+inPathSplit[1])
 	}
 
 skipSwitch:
-	resultChannel <- &data
+	configCtx.ResultChannel <- &data
 }
 
-func receiver() {
+func receiver(configCtx *utils.ConfigContext) {
 
-	for data := range resultChannel {
-		if data != nil && data.inData != nil && data.inPath != "" {
-			mutex.Lock()
-			resultMap[data.inPath] = data.inData
-			mutex.Unlock()
+	for data := range configCtx.ResultChannel {
+		if data != nil && data.InData != nil && data.InPath != "" {
+			configCtx.Mutex.Lock()
+			configCtx.ResultMap[data.InPath] = data.InData
+			configCtx.Mutex.Unlock()
 		}
 	}
 
 }
+
+var (
+	ENDDIR_DEFAULT   = "."
+	STARTDIR_DEFAULT = coreopts.GetFolderPrefix(nil) + "_templates"
+)
 
 func CommonMain(envPtr *string,
 	addrPtr *string,
@@ -82,26 +75,48 @@ func CommonMain(envPtr *string,
 	appRoleIDPtr *string,
 	tokenNamePtr *string,
 	regionPtr *string,
+	flagset *flag.FlagSet,
+	argLines []string,
 	c *eUtils.DriverConfig) error {
 	if memonly.IsMemonly() {
 		memprotectopts.MemProtectInit(nil)
 	}
 
-	startDirPtr := flag.String("startDir", coreopts.GetFolderPrefix(nil)+"_templates", "Template directory")
-	endDirPtr := flag.String("endDir", ".", "Directory to put configured templates into")
-	secretMode := flag.Bool("secretMode", true, "Only override secret values in templates?")
-	servicesWanted := flag.String("servicesWanted", "", "Services to pull template values for, in the form 'service1,service2' (defaults to all services)")
-	wantCertsPtr := flag.Bool("certs", false, "Pull certificates into directory specified by endDirPtr")
-	keyStorePtr := flag.String("keystore", "", "Put certificates into this keystore file.")
-	logFilePtr := flag.String("log", "./"+coreopts.GetFolderPrefix(nil)+"config.log", "Output path for log file")
-	pingPtr := flag.Bool("ping", false, "Ping vault.")
-	zcPtr := flag.Bool("zc", false, "Zero config (no configuration option).")
-	diffPtr := flag.Bool("diff", false, "Diff files")
-	fileFilterPtr := flag.String("filter", "", "Filter files for diff")
-	templateInfoPtr := flag.Bool("templateInfo", false, "Version information about templates")
-	versionInfoPtr := flag.Bool("versions", false, "Version information about values")
-	insecurePtr := flag.Bool("insecure", false, "By default, every ssl connection is secure.  Allows to continue with server connections considered insecure.")
-	noVaultPtr := flag.Bool("novault", false, "Don't pull configuration data from vault.")
+	configCtx := &utils.ConfigContext{
+		ResultMap:     make(map[string]*string),
+		EnvSlice:      make([]string, 0),
+		ResultChannel: make(chan *utils.ResultData, 5),
+		FileSysIndex:  -1,
+		ConfigWg:      sync.WaitGroup{},
+		Mutex:         &sync.Mutex{},
+	}
+
+	if flagset == nil {
+		flagset = flag.NewFlagSet(argLines[0], flag.ExitOnError)
+		flagset.Usage = flag.Usage
+		flagset.String("env", "dev", "Environment to configure")
+		flagset.String("addr", "", "API endpoint for the vault")
+		flagset.String("token", "", "Vault access token")
+		flagset.String("secretID", "", "Secret app role ID")
+		flagset.String("region", "", "Region to be processed") //If this is blank -> use context otherwise override context.
+		flagset.String("appRoleID", "", "Public app role ID")
+		flagset.String("tokenName", "", "Token name used by this"+coreopts.GetFolderPrefix(nil)+"config to access the vault")
+	}
+	startDirPtr := flagset.String("startDir", STARTDIR_DEFAULT, "Template directory")
+	endDirPtr := flagset.String("endDir", ENDDIR_DEFAULT, "Directory to put configured templates into")
+	secretMode := flagset.Bool("secretMode", true, "Only override secret values in templates?")
+	servicesWanted := flagset.String("servicesWanted", "", "Services to pull template values for, in the form 'service1,service2' (defaults to all services)")
+	wantCertsPtr := flagset.Bool("certs", false, "Pull certificates into directory specified by endDirPtr")
+	keyStorePtr := flagset.String("keystore", "", "Put certificates into this keystore file.")
+	logFilePtr := flagset.String("log", "./"+coreopts.GetFolderPrefix(nil)+"config.log", "Output path for log file")
+	pingPtr := flagset.Bool("ping", false, "Ping vault.")
+	zcPtr := flagset.Bool("zc", false, "Zero config (no configuration option).")
+	diffPtr := flagset.Bool("diff", false, "Diff files")
+	fileFilterPtr := flagset.String("filter", "", "Filter files for diff")
+	templateInfoPtr := flagset.Bool("templateInfo", false, "Version information about templates")
+	versionInfoPtr := flagset.Bool("versions", false, "Version information about values")
+	insecurePtr := flagset.Bool("insecure", false, "By default, every ssl connection is secure.  Allows to continue with server connections considered insecure.")
+	noVaultPtr := flagset.Bool("novault", false, "Don't pull configuration data from vault.")
 	isShell := false
 
 	if c != nil {
@@ -109,7 +124,7 @@ func CommonMain(envPtr *string,
 	}
 
 	if c == nil || !isShell {
-		args := os.Args[1:]
+		args := argLines[1:]
 		for i := 0; i < len(args); i++ {
 			s := args[i]
 			if s[0] != '-' {
@@ -117,10 +132,10 @@ func CommonMain(envPtr *string,
 				return fmt.Errorf("wrong flag syntax: %s", s)
 			}
 		}
-		flag.Parse()
+		flagset.Parse(argLines[1:])
 	} else {
 		// TODO: rework to support standard arg parsing...
-		for _, args := range os.Args {
+		for _, args := range argLines {
 			if args == "-certs" {
 				c.WantCerts = true
 			} else if strings.HasPrefix(args, "-keystore") {
@@ -135,7 +150,7 @@ func CommonMain(envPtr *string,
 				}
 			}
 		}
-		flag.CommandLine.Parse(nil)
+		flagset.Parse(nil)
 		if c.WantCerts {
 			*wantCertsPtr = true
 		}
@@ -160,11 +175,11 @@ func CommonMain(envPtr *string,
 	var configBase *eUtils.DriverConfig
 	if c != nil {
 		configBase = c
-		if len(configBase.EndDir) == 0 || len(*endDirPtr) > len(configBase.EndDir) {
+		if len(configBase.EndDir) == 0 || *endDirPtr != ENDDIR_DEFAULT {
 			// Honor inputs if provided...
 			configBase.EndDir = *endDirPtr
 		}
-		if len(configBase.StartDir) == 0 || len(configBase.StartDir[0]) == 0 || (len(*startDirPtr) > len(configBase.StartDir[0])) {
+		if len(configBase.StartDir) == 0 || len(configBase.StartDir[0]) == 0 || *startDirPtr != STARTDIR_DEFAULT {
 			// Bad inputs... use default.
 			configBase.StartDir = append([]string{}, *startDirPtr)
 		}
@@ -199,25 +214,25 @@ func CommonMain(envPtr *string,
 	} else if *diffPtr {
 		if strings.ContainsAny(*envPtr, ",") { //Multiple environments
 			*envPtr = strings.ReplaceAll(*envPtr, "latest", "0")
-			envDiffSlice = strings.Split(*envPtr, ",")
-			envLength = len(envDiffSlice)
-			if len(envDiffSlice) > 4 {
+			configCtx.EnvSlice = strings.Split(*envPtr, ",")
+			configCtx.EnvLength = len(configCtx.EnvSlice)
+			if len(configCtx.EnvSlice) > 4 {
 				fmt.Println("Unsupported number of environments - Maximum: 4")
 				return errors.New("unsupported number of environments - Maximum: 4")
 			}
-			for i, env := range envDiffSlice {
+			for i, env := range configCtx.EnvSlice {
 				if env == "local" {
 					fmt.Println("Unsupported env: local not available with diff flag")
 					return errors.New("unsupported env: local not available with diff flag")
 				}
 				if !strings.Contains(env, "_") && env != "filesys" {
-					envDiffSlice[i] = env + "_0"
+					configCtx.EnvSlice[i] = env + "_0"
 				}
 			}
-			for i, env := range envDiffSlice {
+			for i, env := range configCtx.EnvSlice {
 				if env == "filesys" {
-					fileSysIndex = i
-					envDiffSlice = append(envDiffSlice[:i], envDiffSlice[i+1:]...)
+					configCtx.FileSysIndex = i
+					configCtx.EnvSlice = append(configCtx.EnvSlice[:i], configCtx.EnvSlice[i+1:]...)
 				}
 			}
 		} else {
@@ -274,9 +289,9 @@ func CommonMain(envPtr *string,
 		}
 	}
 
-	if len(envDiffSlice) > 1 {
-		removeDuplicateValuesSlice := eUtils.RemoveDuplicateValues(envDiffSlice)
-		if !cmp.Equal(envDiffSlice, removeDuplicateValuesSlice) {
+	if len(configCtx.EnvSlice) > 1 {
+		removeDuplicateValuesSlice := eUtils.RemoveDuplicateValues(configCtx.EnvSlice)
+		if !cmp.Equal(configCtx.EnvSlice, removeDuplicateValuesSlice) {
 			fmt.Println("There is a duplicate environment in the -env flag")
 			return errors.New("there is a duplicate environment in the -env flag")
 		}
@@ -328,11 +343,11 @@ func CommonMain(envPtr *string,
 	}
 
 	//channel receiver
-	go receiver()
+	go receiver(configCtx)
 	var diffFileCount int
 	if *diffPtr {
-		configSlice := make([]eUtils.DriverConfig, 0, len(envDiffSlice)-1)
-		for _, env := range envDiffSlice {
+		configSlice := make([]eUtils.DriverConfig, 0, len(configCtx.EnvSlice)-1)
+		for _, env := range configCtx.EnvSlice {
 			envVersion := eUtils.SplitEnv(env)
 			*envPtr = envVersion[0]
 			*tokenPtr = ""
@@ -388,10 +403,10 @@ func CommonMain(envPtr *string,
 				FileFilter:        fileFilterSlice,
 			}
 			configSlice = append(configSlice, config)
-			wg.Add(1)
+			configCtx.ConfigWg.Add(1)
 			go func() {
-				defer wg.Done()
-				eUtils.ConfigControl(nil, &configSlice[len(configSlice)-1], vcutils.GenerateConfigsFromVault)
+				defer configCtx.ConfigWg.Done()
+				eUtils.ConfigControl(nil, configCtx, &configSlice[len(configSlice)-1], vcutils.GenerateConfigsFromVault)
 				if diffFileCount < configSlice[len(configSlice)-1].DiffCounter { //Without this, resultMap may be missing data when diffing.
 					diffFileCount = configSlice[len(configSlice)-1].DiffCounter //This counter helps the diff wait for results
 				}
@@ -443,29 +458,37 @@ func CommonMain(envPtr *string,
 		if len(configBase.DeploymentConfig) > 0 {
 			config.DeploymentConfig = configBase.DeploymentConfig
 		}
-		wg.Add(1)
+		configCtx.ConfigWg.Add(1)
 		go func(c *eUtils.DriverConfig) {
-			defer wg.Done()
-			eUtils.ConfigControl(nil, c, vcutils.GenerateConfigsFromVault)
+			defer configCtx.ConfigWg.Done()
+			eUtils.ConfigControl(nil, configCtx, c, vcutils.GenerateConfigsFromVault)
 		}(&config)
 	}
-	wg.Wait() //Wait for templates
+	configCtx.ConfigWg.Wait() //Wait for templates
 	if c == nil {
-		close(resultChannel)
+		close(configCtx.ResultChannel)
 	} else if c.IsShellConfigComplete {
-		close(resultChannel)
+		select {
+		case _, ok := <-configCtx.ResultChannel:
+			if ok {
+				close(configCtx.ResultChannel)
+			}
+		case <-time.NewTicker(200 * time.Millisecond).C:
+			close(configCtx.ResultChannel)
+			break
+		}
 	}
 	if *diffPtr { //Diff if needed
-		if fileSysIndex != -1 {
-			envDiffSlice = append(envDiffSlice, "filesys")
-			envLength = len(envDiffSlice)
+		if configCtx.FileSysIndex != -1 {
+			configCtx.EnvSlice = append(configCtx.EnvSlice, "filesys")
+			configCtx.EnvLength = len(configCtx.EnvSlice)
 		}
-		wg.Add(1)
+		configCtx.ConfigWg.Add(1)
 		go func() {
-			defer wg.Done()
-			eUtils.DiffHelper(resultMap, envLength, envDiffSlice, fileSysIndex, true, mutex, diffFileCount)
+			defer configCtx.ConfigWg.Done()
+			eUtils.DiffHelper(configCtx, true, diffFileCount)
 		}()
 	}
-	wg.Wait() //Wait for diff
+	configCtx.ConfigWg.Wait() //Wait for diff
 	return nil
 }
