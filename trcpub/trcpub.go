b46ca5099acf78aa55bce4c1b2faa3410a7f904f
Joel Rieke <joel_rieke@trimble.com>
2023-11-15 07:09:01 -0800

    Fixing flags and usage.

--- trcpub/trcpub.go
+++ trcpub/trcpub.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/trimble-oss/tierceron/buildopts/coreopts"
 	"github.com/trimble-oss/tierceron/buildopts/memprotectopts"
@@ -21,12 +22,17 @@ func main() {
 		memprotectopts.MemProtectInit(nil)
 	}
 	fmt.Println("Version: " + "1.26")
-	envPtr := flag.String("env", "dev", "Environment to configure")
-	addrPtr := flag.String("addr", "", "API endpoint for the vault")
-	tokenPtr := flag.String("token", "", "Vault access token")
-	secretIDPtr := flag.String("secretID", "", "Public app role ID")
-	appRoleIDPtr := flag.String("appRoleID", "", "Secret app role ID")
-	tokenNamePtr := flag.String("tokenName", "", "Token name used by this "+coreopts.GetFolderPrefix(nil)+"pub to access the vault")
+	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flagset.Usage = func() {
+		fmt.Fprintf(flagset.Output(), "Usage of %s:\n", os.Args[0])
+		flagset.PrintDefaults()
+	}
+	envPtr := flagset.String("env", "dev", "Environment to configure")
+	addrPtr := flagset.String("addr", "", "API endpoint for the vault")
+	tokenPtr := flagset.String("token", "", "Vault access token")
+	secretIDPtr := flagset.String("secretID", "", "Public app role ID")
+	appRoleIDPtr := flagset.String("appRoleID", "", "Secret app role ID")
+	tokenNamePtr := flagset.String("tokenName", "", "Token name used by this "+coreopts.GetFolderPrefix(nil)+"pub to access the vault")
 
-	trcpubbase.CommonMain(envPtr, addrPtr, tokenPtr, nil, secretIDPtr, appRoleIDPtr, tokenNamePtr, nil)
+	trcpubbase.CommonMain(envPtr, addrPtr, tokenPtr, nil, secretIDPtr, appRoleIDPtr, tokenNamePtr, flagset, os.Args, nil)
 }
