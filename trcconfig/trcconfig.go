167ac2be8b5ffed366e206615cf89af93cc98bb4
Joel Rieke <joel_rieke@trimble.com>
2023-11-14 18:38:43 -0800

    Make re-entrant safe.

--- trcconfig/trcconfig.go
+++ trcconfig/trcconfig.go
@@ -19,15 +19,17 @@ func main() {
 		memprotectopts.MemProtectInit(nil)
 	}
 	fmt.Println("Version: " + "1.27")
-	envPtr := flag.String("env", "dev", "Environment to configure")
-	addrPtr := flag.String("addr", "", "API endpoint for the vault")
-	tokenPtr := flag.String("token", "", "Vault access token")
-	secretIDPtr := flag.String("secretID", "", "Secret app role ID")
-	regionPtr := flag.String("region", "", "Region to be processed") //If this is blank -> use context otherwise override context.
-	appRoleIDPtr := flag.String("appRoleID", "", "Public app role ID")
-	tokenNamePtr := flag.String("tokenName", "", "Token name used by this"+coreopts.GetFolderPrefix(nil)+"config to access the vault")
+	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flagset.Usage = flag.Usage
+	envPtr := flagset.String("env", "dev", "Environment to configure")
+	addrPtr := flagset.String("addr", "", "API endpoint for the vault")
+	tokenPtr := flagset.String("token", "", "Vault access token")
+	secretIDPtr := flagset.String("secretID", "", "Secret app role ID")
+	regionPtr := flagset.String("region", "", "Region to be processed") //If this is blank -> use context otherwise override context.
+	appRoleIDPtr := flagset.String("appRoleID", "", "Public app role ID")
+	tokenNamePtr := flagset.String("tokenName", "", "Token name used by this"+coreopts.GetFolderPrefix(nil)+"config to access the vault")
 
-	err := trcconfigbase.CommonMain(envPtr, addrPtr, tokenPtr, nil, secretIDPtr, appRoleIDPtr, tokenNamePtr, regionPtr, nil)
+	err := trcconfigbase.CommonMain(envPtr, addrPtr, tokenPtr, nil, secretIDPtr, appRoleIDPtr, tokenNamePtr, regionPtr, flagset, os.Args, nil)
 	if err != nil {
 		os.Exit(1)
 	}
