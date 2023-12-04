79c6436b85bf1ed7243dc704562e54f6e271ab41
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2023-12-03 17:05:31 -0800

    Prepare for release. (#843)
    * Prepare for release (#842)
    * Fixing break on windows.
    * Add more robust support for servicesWanted and filter.
    * Deployments should match a deployment run.  Always load deployment during a run.
    
    ---------
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ trcconfigbase/utils/filter.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"strings"
+)
+
+// FilterPaths -- filters based on provided fileFilter
+func FilterPaths(templatePaths []string, endPaths []string, fileFilter []string, prefix bool) ([]string, []string) {
+	fileFilterIndex := make([]int, len(templatePaths))
+	fileFilterCounter := 0
+
+	if len(fileFilter) != 0 && fileFilter[0] != "" {
+		for _, FileFilter := range fileFilter {
+			for i, templatePath := range templatePaths {
+				if !prefix {
+					ii := -1
+					if strings.Contains(templatePath, FileFilter) {
+						ii = i
+					}
+					if len(fileFilterIndex) > fileFilterCounter {
+						fileFilterIndex[fileFilterCounter] = ii
+						fileFilterCounter++
+					} else {
+						break
+					}
+				} else {
+					if strings.HasPrefix(templatePath, FileFilter) {
+						if len(fileFilterIndex) > fileFilterCounter {
+							fileFilterIndex[fileFilterCounter] = i
+							fileFilterCounter++
+						} else {
+							break
+						}
+					}
+
+				}
+			}
+		}
+
+		fileTemplatePaths := []string{}
+		fileEndPaths := []string{}
+		for _, index := range fileFilterIndex {
+			if index > 0 {
+				fileTemplatePaths = append(fileTemplatePaths, templatePaths[index])
+				fileEndPaths = append(fileEndPaths, endPaths[index])
+			}
+		}
+
+		templatePaths = fileTemplatePaths
+		endPaths = fileEndPaths
+	}
+	return templatePaths, endPaths
+}
