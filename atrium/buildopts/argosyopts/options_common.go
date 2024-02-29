394189b188ac5ef35d3e8fb8d725d692dc7f990d
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2024-02-29 10:09:19 -0800

    Improve documentation and provide default implementation for askflume. (#979)

--- atrium/buildopts/argosyopts/options_common.go
+++ atrium/buildopts/argosyopts/options_common.go
@@ -15,10 +15,16 @@ func GetStubbedDataFlowStatistics() ([]string, map[string][]float64) {
 	return nil, nil
 }
 
+// BuildFleet loads a set of TTDINodes utilizing the modifier.
+// TTDINodes returned are used to build the data spiral.
+// * TTDINodes are defined recursively, with each node containing a list of child nodes.
+// * this enabled the data to be rendered 3-dimensionally.
+// The modifier is used to access the secret provider.
 func BuildFleet(mod *kv.Modifier, logger *log.Logger) (*flowcore.TTDINode, error) {
 	return &flowcore.TTDINode{}, nil
 }
 
+// Unused function - candidate for future deletion
 func GetDataFlowGroups(mod *kv.Modifier, argosy *flowcore.TTDINode) []flowcore.TTDINode {
 	return nil
 }
