b7e886e89bfc547d451c7643bdcc8d473e71d09c
Joel Rieke <joel_rieke@trimble.com>
2024-07-01 13:41:01 -0700

    Fixing typo.

--- installation/trclocal/trchelloworld/deploy/servicedefine.sh
+++ installation/trclocal/trchelloworld/deploy/servicedefine.sh
@@ -1,4 +1,4 @@
 #!/usr/bin
 
 # dev
-trcplgtool -addr=$VAULT_ADDR -token=$VAULT_TOKEN -env=dev -defineService -pluginName=trchelloworld -projectservice="HelloProject/HelloService" -pluginType=trcshservice -serviceName="TomcatMain" -codeBundle=trchelloword -deployroot=/usr/local/hello -deploysubpath=bin
+trcplgtool -addr=$VAULT_ADDR -token=$VAULT_TOKEN -env=dev -defineService -pluginName=trchelloworld -projectservice="HelloProject/HelloService" -pluginType=trcshservice -serviceName="TomcatMain" -codeBundle=trchelloworld -deployroot=/usr/local/hello -deploysubpath=bin
