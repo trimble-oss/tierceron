8ce16d11b9598acb85d373e0c402d022de6504d6
Meghan Bailey <meghan_bailey@trimble.com>
2024-08-05 14:10:21 -0700

    added test case

--- pkg/cli/trcconfigbase/utils/configinator_test.go
+++ pkg/cli/trcconfigbase/utils/configinator_test.go
@@ -17,7 +17,7 @@ func TestGeneratePaths_nil(t *testing.T) {
 	}
 }
 
-func TestGeneratePaths(t *testing.T) {
+func TestGeneratePaths_BaseCase(t *testing.T) {
 	// var driverConfig *eUtils.DriverConfig
 
 	driverConfig := &eUtils.DriverConfig{
@@ -39,7 +39,7 @@ func TestGeneratePaths(t *testing.T) {
 		PathParam:         "",
 		SecretMode:        true,
 		StartDir: []string{
-			"",
+			"foo",
 		},
 		EndDir:           ".",
 		OutputMemCache:   false,
@@ -66,7 +66,72 @@ func TestGeneratePaths(t *testing.T) {
 		t.Fatalf("Expected no error, got %v", err)
 	}
 	for i := 0; i < len(templatePaths); i++ {
-		if !strings.Contains(templatePaths[i], "") {
+		if !strings.Contains(templatePaths[i], "foo") {
+			fmt.Printf("Expected different template path, instead got: %s\n", templatePaths[i])
+			t.Fatalf("Expected different template path, instead got: %s\n", templatePaths[i])
+		}
+	}
+
+	for i := 0; i < len(endPaths); i++ {
+		if !strings.Contains(endPaths[i], ".") {
+			fmt.Printf("Expected different end path, instead got: %s\n", endPaths[i])
+			t.Fatalf("Expected different end path, instead got: %s\n", endPaths[i])
+		}
+	}
+}
+
+func TestGeneratePaths_CaseOne(t *testing.T) {
+
+	driverConfig := &eUtils.DriverConfig{
+		CoreConfig: core.CoreConfig{
+			IsShell:           false,
+			Insecure:          false,
+			Token:             "foo",
+			AppRoleConfig:     "foo",
+			VaultAddress:      "foo.bar",
+			EnvBasis:          "foo",
+			Env:               "foo",
+			Regions:           []string{},
+			DynamicPathFilter: "foo",
+			WantCerts:         false,
+			ExitOnFailure:     true,
+		},
+		IsShellSubProcess: false,
+		FileFilter:        []string{""},
+		PathParam:         "",
+		SecretMode:        true,
+		StartDir: []string{
+			"foo",
+			"bar",
+		},
+		DeploymentConfig: make(map[string]interface{}),
+		EndDir:           ".",
+		OutputMemCache:   false,
+		ZeroConfig:       false,
+		GenAuth:          false,
+		TrcShellRaw:      "",
+		Trcxr:            false,
+		Clean:            false,
+		KeystorePassword: "",
+		WantKeystore:     "",
+		Diff:             false,
+		DiffCounter:      0,
+		ServicesWanted:   []string{"Project/Service"},
+		SectionKey:       "",
+		SectionName:      "",
+		SubSectionName:   "",
+		SubSectionValue:  "",
+	}
+	driverConfig.DeploymentConfig["trcprojectservice"] = "Project1/Service1"
+
+	templatePaths, endPaths, err := generatePaths(driverConfig)
+	fmt.Println(templatePaths)
+	if err != nil {
+		fmt.Printf("Expected no error, got %s\n", err)
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	for i := 0; i < len(templatePaths); i++ {
+		if !strings.Contains(templatePaths[i], "foo") || !strings.Contains(templatePaths[i], "bar") {
 			fmt.Printf("Expected different template path, instead got: %s\n", templatePaths[i])
 			t.Fatalf("Expected different template path, instead got: %s\n", templatePaths[i])
 		}
