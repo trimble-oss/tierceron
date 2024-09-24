84b7a8fb2a8754093c19de36b6c5bd6c72a45c7c
Joel Rieke <joel_rieke@trimble.com>
2024-09-24 07:46:21 -0700

    Re-add curator initial configuration

--- buildopts/cursoropts/buildopts.go
+++ buildopts/cursoropts/buildopts.go
@@ -3,6 +3,7 @@ package cursoropts
 type Option func(*OptionsBuilder)
 
 type OptionsBuilder struct {
+	GetCuratorConfig    func(pluginEnvConfig map[string]interface{}) map[string]interface{}
 	TapInit             func()
 	GetCapPath          func() string
 	GetPluginName       func() string
@@ -14,6 +15,7 @@ type OptionsBuilder struct {
 
 func LoadOptions() Option {
 	return func(optionsBuilder *OptionsBuilder) {
+		optionsBuilder.GetCuratorConfig = GetCuratorConfig
 		optionsBuilder.GetPluginName = GetPluginName
 		optionsBuilder.GetCapPath = GetCapPath
 		optionsBuilder.GetLogPath = GetLogPath
--- buildopts/cursoropts/options_common.go
+++ buildopts/cursoropts/options_common.go
@@ -3,6 +3,17 @@
 
 package cursoropts
 
+func GetCuratorConfig(pluginEnvConfig map[string]interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"env":            "dev",
+		"exitOnFailure":  false,
+		"regions":        []string{"west"},
+		"pluginNameList": []string{"trc-vault-plugin", "trcsh-cursor-aw", "trcsh-cursor-ak"},
+		"templatePath":   []string{"trc_templates/TrcVault/Certify/config.yml.tmpl"},
+		"logNamespace":   "trcshcurator",
+	}
+}
+
 func TapInit() {
 }
 func GetCapPath() string {
