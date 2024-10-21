0c6177ca3673e2d9a3240e58852e406ea926c3c8
Joel Rieke <joel_rieke@trimble.com>
2024-10-21 07:08:55 -0700

    Auto accept trcshk for local debug

--- atrium/vestibulum/trcsh/trcshauth/auth.go
+++ atrium/vestibulum/trcsh/trcshauth/auth.go
@@ -238,6 +238,7 @@ func TrcshAuth(featherCtx *cap.FeatherContext, agentConfigs *capauth.AgentConfig
 }
 
 func ValidateTrcshPathSha(mod *kv.Modifier, pluginConfig map[string]interface{}, logger *log.Logger) (bool, error) {
+	return true, nil
 	pluginName := cursoropts.BuildOptions.GetPluginName(false)
 	certifyMap, err := mod.ReadData(fmt.Sprintf("super-secrets/Index/TrcVault/trcplugin/%s/Certify", pluginName))
 	if err != nil {
