1feae148e243a991bec1683d6f25568a46080fe6
Meghan Bailey <meghan_bailey@trimble.com>
2024-12-20 11:05:11 -0800

    add flag parse

--- pkg/cli/trcctlbase/trcctlbase.go
+++ pkg/cli/trcctlbase/trcctlbase.go
@@ -62,6 +62,7 @@ func CommonMain(envDefaultPtr *string,
 		flagset.Bool("pluginInfo", false, "Lists all plugins")
 		flagset.Bool("novault", false, "Don't pull configuration data from vault.")
 	} else {
+		flagset.Parse(argLines[1:])
 		envPtr = envDefaultPtr
 	}
 
