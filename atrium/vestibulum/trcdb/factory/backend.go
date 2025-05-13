c9ac7eedac97dac896d28349e13fe267ce1bfb71
Joel Rieke <joel_rieke@trimble.com>
2025-05-13 16:47:25 -0700

    Switch to tierceron core IsProd

--- atrium/vestibulum/trcdb/factory/backend.go
+++ atrium/vestibulum/trcdb/factory/backend.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/trimble-oss/tierceron/atrium/vestibulum/trcdb/opts/prod"
+	prod "github.com/trimble-oss/tierceron-core/v2/prod"
 	"github.com/trimble-oss/tierceron/pkg/utils/config"
 
 	"github.com/trimble-oss/tierceron/buildopts"
