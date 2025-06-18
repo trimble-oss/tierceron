74cff3537771d2dbb27def4c348d687bba0a13e2
Joel Rieke <joel_rieke@trimble.com>
2025-06-18 08:36:18 -0700

    Builds control which flows are in fact loaded by the controller

--- atrium/trcflow/core/context_helper.go
+++ atrium/trcflow/core/context_helper.go
@@ -532,6 +532,10 @@ func (tfmContext *TrcFlowMachineContext) seedTrcDbFromVault(
 
 	rows := make([][]any, 0)
 	for _, indexValue := range indexValues {
+		if tfContext.GetFlowName() == flowcorehelper.TierceronFlowConfigurationTableName && !coreopts.BuildOptions.IsSupportedFlow(indexValue) {
+			eUtils.LogInfo(tfmContext.DriverConfig.CoreConfig, "Skipping unsupported flow: "+indexValue)
+			continue
+		}
 		if indexValue != "" {
 			tfContext.GoMod.SectionKey = "/Index/"
 			var subSection string
