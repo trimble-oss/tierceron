cb7a608ceb9c426b8d517d8c3232e59b37ea39ce
joel-rieke <53945260+joel-rieke@users.noreply.github.com>
2025-08-18 10:52:06 -0700

    Trcdb talk (#1629)
    * trcshtalk trcdb
    * More fixes for trcdb

--- atrium/vestibulum/hive/plugins/trcdb/go.mod
+++ atrium/vestibulum/hive/plugins/trcdb/go.mod
@@ -4,7 +4,6 @@ go 1.25.0
 
 require (
 	github.com/trimble-oss/tierceron-core/v2 v2.7.8
-	github.com/trimble-oss/tierceron/atrium v0.0.0-20250609162306-04fdcac49140
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -13,7 +12,9 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
 	github.com/trimble-oss/tierceron-nute-core v1.0.3 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
@@ -25,4 +26,4 @@ require (
 
 replace github.com/cespare/xxhash => github.com/joel-rieke/xxhash v1.1.0-patch
 
-replace github.com/trimble-oss/tierceron-core/v2 => ../../../../../../tierceron-core
+//replace github.com/trimble-oss/tierceron-core/v2 => ../../../../../../tierceron-core
--- atrium/vestibulum/hive/plugins/trcdb/hcore/hcore.go
+++ atrium/vestibulum/hive/plugins/trcdb/hcore/hcore.go
@@ -8,11 +8,13 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/trimble-oss/tierceron-core/v2/buildopts/plugincoreopts"
 	"github.com/trimble-oss/tierceron-core/v2/core"
 	"github.com/trimble-oss/tierceron-core/v2/core/coreconfig"
-	"github.com/trimble-oss/tierceron/atrium/vestibulum/trcflow/flows/argossocii"
 
 	tccore "github.com/trimble-oss/tierceron-core/v2/core"
 	flowcore "github.com/trimble-oss/tierceron-core/v2/flow"
@@ -92,6 +94,7 @@ func send_err(err error) {
 		fmt.Println("Failure to send error message, error channel not initialized properly for trcdb.")
 		return
 	}
+	configContext.Log.Println("trcdb sending error message to kernel: ", err)
 	if dfstat != nil {
 		dfsctx, _, err := dfstat.GetDeliverStatCtx()
 		if err != nil {
@@ -107,8 +110,188 @@ func send_err(err error) {
 				configContext.Log.Println(msg, err)
 			})
 		tccore.SendDfStat(configContext, dfsctx, dfstat)
+		configContext.Log.Println("Sent dataflow statistic with error to kernel: ", dfstat.Name)
 	}
 	*configContext.ErrorChan <- err
+}
+
+func GetWantedTests(event *core.ChatMsg) (map[string]bool, string) {
+	var argosId string
+	testsSet := map[string]bool{}
+	if event.ChatId != nil && strings.Contains(*(*event).ChatId, ":") {
+		argosTestsSlice := strings.Split(*(*event).ChatId, ":")
+		// Scrub tenantId to only alphabetic characters
+		origArgosId := argosTestsSlice[0]
+		var scrubbedTenantId strings.Builder
+		for _, r := range origArgosId {
+			if (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') {
+				scrubbedTenantId.WriteRune(r)
+			}
+		}
+		argosId = scrubbedTenantId.String()
+		testsCommaList := argosTestsSlice[1]
+		testsSlice := strings.Split(testsCommaList, ",")
+
+		for _, test := range testsSlice {
+			if len(test) > 0 {
+				testsSet[test] = true
+			}
+		}
+	}
+	if len(testsSet) == 0 {
+		testsSet = nil
+	}
+
+	return testsSet, argosId
+}
+
+func CheckFlowStatusReport(region string, argosId string, flowGroupKey string) (string, error) {
+	trcdbExchange := &core.TrcdbExchange{
+		Flows:     []string{"DataFlowStatistics"},                                                                                                                                                                                // Flows
+		Query:     fmt.Sprintf("SELECT * FROM %s.DataFlowStatistics where flowGroup like '%s-%s-%s' and argosId like '%s' ORDER by flowName, CAST(stateCode as UNSIGNED) asc", "%s", flowGroupKey, region, "%", string(argosId)), // Query
+		Operation: "SELECT",                                                                                                                                                                                                      // query operation
+	}
+	processTrcdb(trcdbExchange)
+
+	if len(trcdbExchange.Response.Rows) > 0 {
+		var report strings.Builder
+		report.WriteString("Report for " + argosId + ":\n")
+		report.WriteString("Pod | Flow | Step # | Result | Execution Date | Time Spent |  Status\n")
+		report.WriteString("-----------------------------------------------------------------------------\n")
+
+		// Get the lastTestedDate from the first row (assumed to be the same for all rows)
+		var baseTimestamp time.Time
+		var err error
+
+		if len(trcdbExchange.Response.Rows) > 0 && len(trcdbExchange.Response.Rows[0]) >= 8 {
+			baseTimestampStr := fmt.Sprint(trcdbExchange.Response.Rows[0][7]) // Index 7 for timestamp
+			baseTimestamp, err = time.Parse(time.RFC3339, baseTimestampStr)
+			if err != nil {
+				// If we can't parse the timestamp, continue but note the error
+				report.WriteString("Warning: Could not parse base timestamp. Execution times may be inaccurate.\n\n")
+			}
+		}
+
+		// Keep track of cumulative time to add to base timestamp
+		var cumulativeTime time.Duration
+		var currentFlowName string
+
+		for _, row := range trcdbExchange.Response.Rows {
+			if len(row) >= 8 {
+				flowName := fmt.Sprint(row[0]) // Start at index 0 for flowName
+
+				// Reset cumulative time when flow name changes
+				if currentFlowName != flowName {
+					currentFlowName = flowName
+					cumulativeTime = 0
+
+					// Reset base timestamp for the new flow
+					baseTimestampStr := fmt.Sprint(row[7])
+					newBaseTimestamp, err := time.Parse(time.RFC3339, baseTimestampStr)
+					if err == nil {
+						baseTimestamp = newBaseTimestamp
+					}
+				}
+
+				stateCode := fmt.Sprint(row[4]) // Start at index 4 for stateCode
+				modeStr := fmt.Sprint(row[3])   // Index 3 for mode
+				flowGroup := fmt.Sprint(row[1]) // Index 1 for flowGroup
+
+				// Extract pod ID from the flowGroup (should be in the format 'prefix-region-podId')
+				podId := "0"
+				if strings.Contains(flowGroup, "-") {
+					parts := strings.Split(flowGroup, "-")
+					// Get only the last segment as the pod ID
+					podId = parts[len(parts)-1]
+				}
+
+				stateName := fmt.Sprint(row[5])    // Index 5 for stateName
+				timeSplitStr := fmt.Sprint(row[6]) // Index 6 for timeSplit, to be displayed unaltered
+
+				var modeText string
+				switch modeStr {
+				case "0":
+					modeText = "Skipped"
+				case "1":
+					modeText = "Success"
+				case "2":
+					modeText = "Failed"
+				default:
+					modeText = "Unknown (" + modeStr + ")"
+				}
+
+				var stepDuration time.Duration
+
+				// Check for millisecond suffix
+				if strings.HasSuffix(timeSplitStr, "ms") {
+					numericPart := strings.TrimSuffix(timeSplitStr, "ms")
+					valueFloat, err := strconv.ParseFloat(numericPart, 64)
+					if err == nil {
+						stepDuration = time.Duration(valueFloat) * time.Millisecond
+					}
+				} else if strings.HasSuffix(timeSplitStr, "s") { // Check for second suffix
+					numericPart := strings.TrimSuffix(timeSplitStr, "s")
+					valueFloat, err := strconv.ParseFloat(numericPart, 64)
+					if err == nil {
+						stepDuration = time.Duration(valueFloat * float64(time.Second))
+					}
+				} else {
+					valueFloat, err := strconv.ParseFloat(timeSplitStr, 64)
+					if err == nil {
+						stepDuration = time.Duration(valueFloat * float64(time.Second))
+					}
+				}
+
+				cumulativeTime += stepDuration
+
+				var executionTimeDisplay string
+				if currentFlowName == flowName && stateCode != "1" {
+					executionTimeDisplay = "                " // Same width as time format
+				} else {
+					executionTime := baseTimestamp.Add(cumulativeTime)
+					executionTimeDisplay = executionTime.Format("2006-01-02 15:04:05.000")
+				}
+
+				// Add to report
+				report.WriteString(fmt.Sprintf("%s | %s - %s. %s » %s %s %s\n",
+					podId, flowName, stateCode, stateName, executionTimeDisplay, timeSplitStr, modeText))
+			}
+		}
+
+		// Add total duration at the end
+		if cumulativeTime > 0 {
+			report.WriteString(fmt.Sprintf("\nTotal Duration: %s\n", cumulativeTime.String()))
+		}
+
+		return report.String(), nil
+	}
+	return "", nil
+}
+
+func TrcdbChatMessageHandler(event *core.ChatMsg) (string, error) {
+	if event.ChatId != nil && strings.Contains(*(*event).ChatId, ":") {
+		testsSet, _ := GetWantedTests(event)
+		approvedProdTests := make(map[string]bool)
+		for test, _ := range testsSet {
+			switch test {
+			case "FlowStatus":
+			case "PluginStatus":
+				approvedProdTests[test] = true
+			}
+		}
+		if IsProd() && len(approvedProdTests) == 0 {
+			return "Test not supported in production", nil
+		} else if IsProd() {
+			testsSet = approvedProdTests
+		}
+
+		if testsSet["FlowStatus"] {
+			return CheckFlowStatusReport(configContext.Region, "flume", "Flows")
+		} else if testsSet["PluginStatus"] {
+			return CheckFlowStatusReport(configContext.Region, "hiveplugin", "System")
+		}
+	}
+	return "", nil
 }
 
 func chat_receiver(chat_receive_chan chan *tccore.ChatMsg) {
@@ -136,6 +319,22 @@ func chat_receiver(chat_receive_chan chan *tccore.ChatMsg) {
 			configContext.Log.Println("Sending all test results back to kernel.")
 
 			*configContext.ChatSenderChan <- event
+		case event.ChatId == nil || (*event).ChatId == nil || (event.ChatId != nil && (*event).ChatId != nil && *event.ChatId != "PIPELINETEST"):
+			GetConfigContext("trcdb").Log.Println("Trcdb received chat message")
+			response, err := TrcdbChatMessageHandler(event)
+			if err != nil {
+				GetConfigContext("trcdb").Log.Printf("Trcdb errored with %s\n", err.Error())
+			} else {
+				if event.Name != nil && *event.Name == "trcdb" && (event.Query == nil || len(*event.Query) == 0) {
+					GetConfigContext("trcdb").Log.Println("Received message from trcdb, not processing to avoid feedback loop")
+					continue
+				}
+				(*event).Response = &response
+				configContext := GetConfigContext("trcdb")
+				go func(csc *chan *core.ChatMsg) {
+					*csc <- event
+				}(configContext.ChatSenderChan)
+			}
 		default:
 			configContext.Log.Println("trcdb received chat message")
 		}
@@ -187,6 +386,9 @@ func start(pluginName string) {
 		*configContext.CmdSenderChan <- tccore.KernelCmd{
 			Command: tccore.PLUGIN_EVENT_START,
 		}
+		configContext.Log.Println("trcdb reported startup.")
+	} else {
+		configContext.Log.Println("No command sender channel available, trcdb cannot send start event.")
 	}
 }
 
@@ -225,22 +427,21 @@ func GetConfigPaths(pluginName string) []string {
 // flow controller if you define any additional flows other than the default flows:
 // 1. DataFlowStatConfigurationsFlow
 func ProcessFlowController(tfmContext flowcore.FlowMachineContext, tfContext flowcore.FlowContext) error {
-	switch tfContext.GetFlowHeader().FlowName() {
-	case flowcore.ArgosSociiFlow.TableName():
-		tfContext.SetFlowLibraryContext(argossocii.GetProcessFlowDefinition())
-	default:
-		return errors.New("Flow not implemented: " + tfContext.GetFlowHeader().FlowName())
-	}
+	// switch tfContext.GetFlowHeader().FlowName() {
+	// case flowcore.ArgosSociiFlow.TableName():
+	// 	tfContext.SetFlowLibraryContext(argossocii.GetProcessFlowDefinition())
+	// default:
+	// 	return errors.New("Flow not implemented: " + tfContext.GetFlowHeader().FlowName())
+	// }
 
 	return flowcore.ProcessTableConfigurations(tfmContext, tfContext)
 }
 
-func GetDbProject() string {
+func GetDatabaseName() string {
 	return "TrcDb"
 }
-
-func IsSupportedFlow(flow string) bool {
-	return flow != "" && (flow == flowcore.ArgosSociiFlow.FlowName() || flow == flowcore.DataFlowStatConfigurationsFlow.FlowName())
+func GetFlowDatabaseName() string {
+	return "FlumeDb"
 }
 
 func GetFlowMachineTemplates() map[string]any {
@@ -256,7 +457,43 @@ func GetFlowMachineTemplates() map[string]any {
 	return flowMachineTemplates
 }
 
+func GetFlowMachineTemplatesHive() map[string]any {
+	flowMachineTemplates := map[string]any{}
+	flowMachineTemplates["templatePath"] = []string{
+		"trc_templates/TrcDb/DataFlowStatistics/DataFlowStatistics.tmpl", // implemented.
+		"trc_templates/TrcDb/ArgosSocii/ArgosSocii.tmpl",                 // implemented.
+	}
+	flowMachineTemplates["flumeTemplatePath"] = []string{
+		"trc_templates/FlumeDatabase/TierceronFlow/TierceronFlow.tmpl", // implemented.
+	}
+
+	return flowMachineTemplates
+}
+
+func IsSupportedFlow(flow string) bool {
+	return flow != "" && (flow == flowcore.ArgosSociiFlow.FlowName() || flow == flowcore.DataFlowStatConfigurationsFlow.FlowName())
+}
+
+func IsHiveSupportedFlow(flow string) bool {
+	return flow != "" && (flow == flowcore.DataFlowStatConfigurationsFlow.FlowName())
+}
+
+func GetFlowMachineTemplatesEditor() map[string]any {
+	flowMachineTemplates := map[string]any{}
+	flowMachineTemplates["templatePath"] = []string{
+		"trc_templates/TrcDb/DataFlowStatistics/DataFlowStatistics.tmpl", // implemented.
+		"trc_templates/TrcDb/ArgosSocii/ArgosSocii.tmpl",                 // implemented.
+	}
+	flowMachineTemplates["flumeTemplatePath"] = []string{
+		"trc_templates/FlumeDatabase/TierceronFlow/TierceronFlow.tmpl", // implemented.
+	}
+
+	return flowMachineTemplates
+}
+
 func GetBusinessFlows() []flowcore.FlowDefinition {
+	// Not implemented for hive infrastructure yet
+	// return tccutil.GetAdditionalFlows()
 	return []flowcore.FlowDefinition{}
 }
 
@@ -273,16 +510,34 @@ func TestFlowController(tfmContext flowcore.FlowMachineContext, tfContext flowco
 }
 
 func GetFlowMachineInitContext(coreConfig *coreconfig.CoreConfig, pluginName string) *flowcore.FlowMachineInitContext {
-	pluginConfig := GetFlowMachineTemplates()
+	var flowMachineTemplatesFunc func() map[string]any
+	var isSupportedFlow func(flowName string) bool
+
+	if coreConfig != nil && coreConfig.IsEditor {
+		flowMachineTemplatesFunc = GetFlowMachineTemplatesEditor
+		isSupportedFlow = IsSupportedFlow
+	} else {
+		flowMachineTemplatesFunc = GetFlowMachineTemplatesHive
+		isSupportedFlow = IsHiveSupportedFlow
+	}
+	flowMachineTemplates := flowMachineTemplatesFunc()
 
 	return &flowcore.FlowMachineInitContext{
-		GetFlowMachineTemplates:     GetFlowMachineTemplates,
+		GetFlowMachineTemplates:     flowMachineTemplatesFunc,
 		FlowMachineInterfaceConfigs: map[string]any{},
-		GetDatabaseName:             nil,
-		IsSupportedFlow:             IsSupportedFlow,
+		GetDatabaseName: func(flumeDbType flowcore.FlumeDbType) string {
+			switch flumeDbType {
+			case flowcore.TrcDb:
+				return GetDatabaseName()
+			case flowcore.TrcFlumeDb:
+				return GetFlowDatabaseName()
+			}
+			return GetDatabaseName()
+		},
+		IsSupportedFlow: isSupportedFlow,
 		GetTableFlows: func() []flowcore.FlowDefinition {
 			tableFlows := []flowcore.FlowDefinition{}
-			for _, template := range pluginConfig["templatePath"].([]string) {
+			for _, template := range flowMachineTemplates["templatePath"].([]string) {
 				flowSource, service, _, tableTemplateName := coreutil.GetProjectService("", "trc_templates", template)
 				tableName := coreutil.GetTemplateFileName(tableTemplateName, service)
 				tableFlows = append(tableFlows, flowcore.FlowDefinition{
@@ -315,8 +570,8 @@ func Init(pluginName string, properties *map[string]any) {
 	configContext, err = tccore.Init(properties,
 		tccore.TRCSHHIVEK_CERT,
 		tccore.TRCSHHIVEK_KEY,
-		"", // No additional config file used/managed by the trcdb plugin itself.
-		"trcdb",
+		"",           // No additional config file used/managed by the trcdb plugin itself.
+		"hiveplugin", // Categorize as hiveplugin
 		start,
 		receiver,
 		chat_receiver,
@@ -326,13 +581,13 @@ func Init(pluginName string, properties *map[string]any) {
 		return
 	}
 	if _, ok := (*properties)[flowcore.HARBINGER_INTERFACE_CONFIG]; !ok {
-		fmt.Println("Missing common config components")
+		configContext.Log.Println("Missing common config components")
 		return
 	}
 	if tfmCtx, ok := (*properties)[tccore.TRCDB_RESOURCE].(flowcore.FlowMachineContext); ok {
 		tfmContext = tfmCtx
 	} else {
-		fmt.Println("No flow context available for trcdb plugin.")
+		configContext.Log.Println("No flow context available for trcdb plugin.")
 		return
 	}
 }
