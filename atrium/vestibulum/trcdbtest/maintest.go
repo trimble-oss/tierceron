32c51327217bc099107e3e5e89fa86ffccfe2c41
Joel Rieke <joel_rieke@trimble.com>
2025-09-24 09:21:54 -0700

    Adding TierceronFlow also into supported flows

--- atrium/vestibulum/trcdbtest/maintest.go
+++ atrium/vestibulum/trcdbtest/maintest.go
@@ -24,20 +24,19 @@ import (
 )
 
 func IsSupportedFlow(flow string) bool {
-	return flow != "" && (flow == flowcore.ArgosSociiFlow.FlowName() || flow == flowcore.DataFlowStatConfigurationsFlow.FlowName())
+	return flow != "" && (flow == flowcore.TierceronControllerFlow.FlowName() || flow == flowcore.ArgosSociiFlow.FlowName() || flow == flowcore.DataFlowStatConfigurationsFlow.FlowName())
 }
 
 // This executable automates the creation of seed files from template file(s).
 // New seed files are written (or overwrite current seed files) to the specified directory.
 func main() {
-
 	// Supported build flags:
 	//    insecure harbinger tc testrunner ( mysql, testflow -- auto registration -- warning do not use!)
 	logFilePtr := flag.String("log", "./trcdbplugin.log", "Output path for log file")
 	tokenPtr := flag.String("token", "", "Vault access Token")
 	flag.Parse()
 
-	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	f, err := os.OpenFile(*logFilePtr, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o644)
 	logger := log.New(f, "[trcdbplugin]", log.LstdFlags)
 	eUtils.CheckError(&coreconfig.CoreConfig{ExitOnFailure: true, Log: logger}, err, true)
 	kernelopts.NewOptionsBuilder(kernelopts.LoadOptions())
