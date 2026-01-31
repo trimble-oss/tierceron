728e01a7772c01ca0a3cdbb2b1d6df39382b5185
Joel Rieke <joel_rieke@trimble.com>
2026-01-31 09:26:49 -0800

    Small tweak

--- atrium/vestibulum/hive/plugins/trctrcsh/scripts/servicedefine.sh
+++ atrium/vestibulum/hive/plugins/trctrcsh/scripts/servicedefine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin
 
 # dev
-trcplgtool -addr=$VAULT_ADDR -token=$VAULT_TOKEN -env=dev -defineService -pluginName=trcsh -trcbootstrap=/deploy/deploy.trc -projectservice="Hive/PluginTrcsh" -pluginType=trcshpluginservice -codeBundle=trcsh.so -deployroot=/usr/local/trcshk -deploysubpath=plugins
+trcplgtool -addr=$VAULT_ADDR -token=$VAULT_TOKEN -env=dev -defineService -pluginName=trcsh -trcbootstrap=/deploy/deploy.trc -projectservice="Hive/PluginTrcsh" -pluginType=trcshcmdtoolplugin -codeBundle=trcsh.so -deployroot=/usr/local/trcshk -deploysubpath=plugins
 
 #QA
-trcplgtool -addr=$VAULT_ADDR -token=$VAULT_TOKEN -env=QA -defineService -pluginName=trcsh -trcbootstrap=/deploy/deploy.trc -projectservice="Hive/PluginTrcsh" -pluginType=trcshpluginservice -codeBundle=trcsh.so -deployroot=/usr/local/trcshk -deploysubpath=plugins
+trcplgtool -addr=$VAULT_ADDR -token=$VAULT_TOKEN -env=QA -defineService -pluginName=trcsh -trcbootstrap=/deploy/deploy.trc -projectservice="Hive/PluginTrcsh" -pluginType=trcshcmdtoolplugin -codeBundle=trcsh.so -deployroot=/usr/local/trcshk -deploysubpath=plugins
