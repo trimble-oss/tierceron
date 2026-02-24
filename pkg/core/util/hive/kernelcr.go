d367c4bc05edd01855a0e32781b6bb01b57409ec
Joel Rieke <joel_rieke@trimble.com>
2026-02-24 10:25:51 -0800

    Add signal plugin ready messaging for flow loading

--- pkg/core/util/hive/kernelcr.go
+++ pkg/core/util/hive/kernelcr.go
@@ -1189,6 +1189,9 @@ func (pluginHandler *PluginHandler) PluginserviceStart(driverConfig *config.Driv
 				}
 				tfmContext.(flow.FlowMachineContext).SetFlowIDs()
 				tfmContext.(flow.FlowMachineContext).WaitAllFlowsLoaded()
+				// Signal that flows are loaded with a specific "-flowsloaded" signal so other plugins can wait for it
+				pluginsync.SignalPluginReady(pluginHandler.Name + "-flowsloaded")
+				driverConfig.CoreConfig.Log.Printf("Plugin %s flows loaded and ready\n", pluginHandler.Name)
 				// kick off reload process from vault
 				go reloadFlows(tfmContext.(flow.FlowMachineContext), mod)
 				serviceConfig[tccore.TRCDB_RESOURCE] = tfmContext
