413f47bd4f9f0351b8ad33a4edc1b9e019618cd6
Joel Rieke <joel_rieke@trimble.com>
2026-03-18 08:11:18 -0700

    Disable for now

--- atrium/vestibulum/trccarrier/carrierfactory/backend.go
+++ atrium/vestibulum/trccarrier/carrierfactory/backend.go
@@ -103,7 +103,8 @@ func Init(processFlowConfig trcvutils.ProcessFlowConfig,
 
 			if configInitOnce, ciOk := pluginEnvConfig["syncOnce"]; ciOk {
 				configInitOnce.(*sync.Once).Do(func() {
-					startDailyServiceTokenRotation(pluginEnvConfig, logger)
+					// Chewbacca: Enable when stars are aligned
+					// startDailyServiceTokenRotation(pluginEnvConfig, logger)
 
 					if bootFlowMachineFunc != nil {
 						tokenCache := cache.NewTokenCache(fmt.Sprintf("config_token_%s_unrestricted", pluginEnvConfig["env"]), eUtils.RefMap(pluginEnvConfig, "tokenptr"), eUtils.RefMap(pluginEnvConfig, "vaddress"))
