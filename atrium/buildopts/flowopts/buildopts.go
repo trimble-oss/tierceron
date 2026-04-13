8769022560e9a45fa4940cd2ed55a3d60f2ecbaf
Joel Rieke <joel_rieke@trimble.com>
2026-04-13 15:41:18 -0700

    Refactor trcdb buildopts into tierceron-core

--- atrium/buildopts/flowopts/buildopts.go
+++ atrium/buildopts/flowopts/buildopts.go
@@ -1,22 +1,15 @@
 package flowopts
 
 import (
-	flowcore "github.com/trimble-oss/tierceron-core/v2/flow"
+	coreflowopts "github.com/trimble-oss/tierceron-core/v2/atrium/buildopts/flowopts"
 	trcflowcore "github.com/trimble-oss/tierceron/atrium/trcflow/core"
 )
 
 type Option func(*OptionsBuilder)
 
 type OptionsBuilder struct {
-	// Flow
-	AllowTrcdbInterfaceOverride func() bool
-	GetAdditionalFlows          func() []flowcore.FlowDefinition
-	GetAdditionalTestFlows      func() []flowcore.FlowDefinition
-	GetAdditionalFlowsByState   func(string) []flowcore.FlowDefinition
-	ProcessTestFlowController   func(tfmContext flowcore.FlowMachineContext, tfContext flowcore.FlowContext) error
-	ProcessFlowController       func(tfmContext flowcore.FlowMachineContext, tfContext flowcore.FlowContext) error
-	GetFlowMachineTemplates     func() map[string]any
-	ProcessAskFlumeEventMapper  func(askFlumeContext *trcflowcore.AskFlumeContext, query *trcflowcore.AskFlumeMessage, tfmContext *trcflowcore.TrcFlowMachineContext, tfContext *trcflowcore.TrcFlowContext) *trcflowcore.AskFlumeMessage
+	*coreflowopts.OptionsBuilder
+	ProcessAskFlumeEventMapper func(askFlumeContext *trcflowcore.AskFlumeContext, query *trcflowcore.AskFlumeMessage, tfmContext *trcflowcore.TrcFlowMachineContext, tfContext *trcflowcore.TrcFlowContext) *trcflowcore.AskFlumeMessage
 }
 
 func LoadOptions() Option {
@@ -35,8 +28,9 @@ func LoadOptions() Option {
 var BuildOptions *OptionsBuilder
 
 func NewOptionsBuilder(opts ...Option) {
-	BuildOptions = &OptionsBuilder{}
+	BuildOptions = &OptionsBuilder{OptionsBuilder: &coreflowopts.OptionsBuilder{}}
 	for _, opt := range opts {
 		opt(BuildOptions)
 	}
+	coreflowopts.BuildOptions = BuildOptions.OptionsBuilder
 }
